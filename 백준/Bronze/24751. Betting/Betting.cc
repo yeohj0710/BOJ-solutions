18e7522de0cdb95a733438f1c1e1a4a8345c5571
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:12:39 +0900

    [Bronze IV] Title: Betting, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    double n; cin >> n;
+    cout << 100/n << "\n" << 100/(100-n);
+}
