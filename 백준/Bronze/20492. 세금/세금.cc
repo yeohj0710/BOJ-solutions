4507ea8a21ad51fd40969da0011e9a4a40aa8398
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:18:16 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+    cout << int(0.78*N) << " " << int(N - 0.2*0.22*N);
+}
