be62af1e6b685cbbb0167e20c775cde7ceb23167
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:05:42 +0900

    [Bronze IV] Title: Andando no tempo, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.cc
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a == b || b == c || c == a || a+b == c || b+c == a || c+a == b) printf("S");
+    else printf("N");
+}
