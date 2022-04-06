ccdd38a1bfbac393785bfe0f8d1e72f48d1ab172
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:47:03 +0900

    [Bronze III] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+    while(T--) {
+        int x, y; cin >> x >> y;
+        cout << x+y << "\n";
+    }
+}
