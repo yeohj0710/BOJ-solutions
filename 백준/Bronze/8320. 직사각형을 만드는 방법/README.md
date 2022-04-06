cc597f68854f4d8dec40a2bc9335e6792b20f403
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:14:35 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 32 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 직사각형을 만드는 방법 - 8320 
+
+[문제 링크](https://www.acmicpc.net/problem/8320) 
+
+### 성능 요약
+
+메모리: 1112 KB, 시간: 32 ms
+
+### 분류
+
+구현(implementation), 수학(math)
+
---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=i; j++) if(i*j <= n) count++;
+    printf("%d", count);
+}
