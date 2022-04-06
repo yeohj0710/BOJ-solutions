ce29b6255746cdb42e10e3588c8cc4b3a021fee4
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:49:30 +0900

    [Bronze III] Title: Hello Judge, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) printf("Hello World, Judge %d!\n", i);
+}
---
+++ 백준/Bronze/9316. Hello Judge/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] Hello Judge - 9316 
+
+[문제 링크](https://www.acmicpc.net/problem/9316) 
+
+### 성능 요약
+
+메모리: 1112 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
