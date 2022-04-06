4ce3cbab01e088c0364c10e70ac7f780a1f006ac
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 02:52:14 +0900

    [Bronze I] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze I] 팰린드롬인지 확인하기 - 10988 
+
+[문제 링크](https://www.acmicpc.net/problem/10988) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation), 문자열(string)
+
---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+    bool check = true;
+    for(int i=0; i<str.length()/2; i++)
+        if(str[i] != str[str.length()-1-i]) check = false;
+    cout << check;
+}
