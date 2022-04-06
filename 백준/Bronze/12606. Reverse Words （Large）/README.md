91e17b7c89cb27ab0e93c5f81723701cdccea670
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:25:37 +0900

    [Bronze II] Title: Reverse Words (Large), Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Bronze/12606. Reverse Words （Large）/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] Reverse Words (Large) - 12606 
+
+[문제 링크](https://www.acmicpc.net/problem/12606) 
+
+### 성능 요약
+
+메모리: 2160 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation), 파싱(parsing), 문자열(string)
+
---
+++ 백준/Bronze/12606. Reverse Words （Large）/Reverse Words （Large）.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+    cin.ignore();
+
+    for(int t=1; t<=T; t++) {
+        cout << "Case #" << t << ": ";
+
+        string str; getline(cin, str);
+        vector<string> List;
+
+        string temp = "";
+        for(int i=0; i<str.length(); i++) {
+            if(str[i] != ' ') temp += str[i];
+            else {
+                List.push_back(temp);
+                temp = "";
+            }
+        }
+        List.push_back(temp);
+
+        for(int i=List.size()-1; i>=0; i--)
+            cout << List[i] << " ";
+
+        cout << "\n";
+    }
+}
