2a30e8ad44591fb9fcc1bc31f82f1cb28df7438d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:02:42 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == b && b == c) printf("%d", 10000 + a * 1000);
+    else if (a == b) printf("%d", 1000 + a * 100);
+    else if (b == c) printf("%d", 1000 + b * 100);
+    else if (c == a) printf("%d", 1000 + c * 100);
+    else {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        printf("%d", max * 100);
+    }
+}
