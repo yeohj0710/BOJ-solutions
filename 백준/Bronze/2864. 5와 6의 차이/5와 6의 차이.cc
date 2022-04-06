3e1abe46584690a33419e7a853d1afc64ba6850f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:02:32 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string num1, num2; cin >> num1 >> num2;
+
+    for(int i=0; i<num1.length(); i++)
+        if(num1[i] == '6') num1[i] = '5';
+    for(int i=0; i<num2.length(); i++)
+        if(num2[i] == '6') num2[i] = '5';
+
+    cout << stoi(num1) + stoi(num2) << " ";
+
+    for(int i=0; i<num1.length(); i++)
+        if(num1[i] == '5') num1[i] = '6';
+    for(int i=0; i<num2.length(); i++)
+        if(num2[i] == '5') num2[i] = '6';
+
+    cout << stoi(num1) + stoi(num2);
+}
