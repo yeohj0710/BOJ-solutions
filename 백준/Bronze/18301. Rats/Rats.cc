46c12e8bfbfd53195a872c4530b3b7e56270ea57
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:18:05 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+    cout << int((a+1)*(b+1)/(c+1) - 1);
+}
