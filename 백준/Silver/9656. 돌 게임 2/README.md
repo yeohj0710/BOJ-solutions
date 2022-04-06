ece46ce4f577e23532f8ea3f28da583a95017399
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 02:35:18 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/README.md
@@ -0,0 +1,12 @@
+# [Silver IV] 돌 게임 2 - 9656 
+
+[문제 링크](https://www.acmicpc.net/problem/9656) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+다이나믹 프로그래밍(dp), 게임 이론(game_theory)
+
---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    if(N%2 == 0) cout << "SK";
+    else cout << "CY";
+}
