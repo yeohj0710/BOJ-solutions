cba0324e201ac9335337e56293c0254d97c585f9
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 01:28:12 +0900

    [Silver II] Title: 최대 힙, Time: 24 ms, Memory: 2008 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N, input;
+    priority_queue<int> pq;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++) {
+        scanf("%d", &input);
+        if(input == 0) {
+            if(!pq.empty()) {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+            else printf("0\n");
+        }
+        else pq.push(input);
+    }
+}
