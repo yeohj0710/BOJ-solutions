50ce69db2dff76da4f6f54eb4e80226e4e5742ca
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:03:00 +0900

    [Bronze II] Title: 8진수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2998. 8진수/8진수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+
+    if(str.length()%3 == 1) str = "00" + str;
+    else if(str.length()%3 == 2) str = "0" + str;
+
+    for(int i=0; i<str.length(); i+=3) {
+        int temp = 0;
+        if(str[i] == '1') temp += 4;
+        if(str[i+1] == '1') temp += 2;
+        if(str[i+2] == '1') temp += 1;
+        cout << temp;
+    }
+}
---
+++ 백준/Bronze/2998. 8진수/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 8진수 - 2998 
+
+[문제 링크](https://www.acmicpc.net/problem/2998) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation), 수학(math), 문자열(string)
+
