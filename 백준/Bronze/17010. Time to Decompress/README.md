58620df846b3b31b059350c6472ef0b7a4aedee6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:59:16 +0900

    [Bronze III] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] Time to Decompress - 17010 
+
+[문제 링크](https://www.acmicpc.net/problem/17010) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, n;
+    char c;
+    cin >> T;
+    while(T--) {
+        cin >> n >> c;
+        while(n--) cout << c;
+        cout << "\n";
+    }
+}
