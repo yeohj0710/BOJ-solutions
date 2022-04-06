a552d4fa58677ebb9d39bfe918db7b72c6c3a7f6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:54:55 +0900

    [Bronze III] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+    while(T--) {
+        string str; cin >> str;
+        cout << str.length() << "\n";
+    }
+}
---
+++ 백준/Bronze/13297. Quick Estimates/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] Quick Estimates - 13297 
+
+[문제 링크](https://www.acmicpc.net/problem/13297) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 0 ms
+
+### 분류
+
+문자열(string)
+
