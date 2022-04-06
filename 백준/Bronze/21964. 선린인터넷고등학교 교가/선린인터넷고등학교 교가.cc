32d6028667c9f9421bf9ddc008bb72c354d8c5e5
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:31:59 +0900

    [Bronze II] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+    string str; cin >> str;
+
+    cout << str.substr(N-5, 5);
+}
