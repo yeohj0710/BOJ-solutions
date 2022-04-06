12fc19089ac2c2d2dbc8c3c7ef4002961b55f03f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:46:32 +0900

    [Bronze III] Title: What is n, Daddy?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6794. What is n， Daddy？/What is n， Daddy？.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n; cin >> n;
+
+    if(n == 1) cout << "1";
+    else if(n == 2) cout << "2";
+    else if(n == 3) cout << "2";
+    else if(n == 4) cout << "3";
+    else if(n == 5) cout << "3";
+    else if(n == 6) cout << "3";
+    else if(n == 7) cout << "2";
+    else if(n == 8) cout << "2";
+    else if(n == 9) cout << "1";
+    else if(n == 10) cout << "1";
+}
