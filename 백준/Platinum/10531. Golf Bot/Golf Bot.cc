254f10b9309f9c42a2c86d7e3b196ce6dc1a61ec
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 00:56:38 +0900

    [Platinum II] Title: Golf Bot, Time: 140 ms, Memory: 27440 KB -BaekjoonHub

--- 백준/Platinum/10531. Golf Bot/Golf Bot.cc
+++ 백준/Platinum/10531. Golf Bot/Golf Bot.cc
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
-#define MAX 200001
 using namespace std;
 
 const double PI = acos(-1);
-typedef long long ll;
 typedef complex<double> cpx;
 
 void FFT(vector<cpx> &f, bool inv) {
     int N = f.size();
-    vector<cpx> roots(N/2);
 
-    int j = 0;
-    for(int i=1; i<N; i++) {
+    for(int i=1, j=0; i<N; i++) {
         int bit = N/2;
 
         while(j >= bit) {
@@ -23,46 +19,39 @@ void FFT(vector<cpx> &f, bool inv) {
         if(i < j) swap(f[i], f[j]);
     }
 
-    double angle;
-    if(!inv) angle = 2 * M_PI / N;
-    else angle = (-1) * 2 * M_PI / N;
+    for(int k=1; k<N; k*=2) {
+        double angle = (inv ? PI/k : -PI/k);
+        cpx dir(cos(angle), sin(angle));
 
-    for(int i=0; i<N/2; i++)
-        roots[i] = cpx(cos(angle * i), sin(angle * i));
+        for(int i=0; i<N; i+=k*2) {
+            cpx unit(1, 0);
 
-    for(int i=2; i<=N; i*=2) {
-        int step = N / i;
+            for(int j=0; j<k; j++) {
+                cpx u = f[i+j];
+                cpx v = f[i+j+k] * unit;
 
-        for(int j=0; j<N; j+=i)
-            for(int k=0; k<i/2; k++) {
-                cpx u = f[j + k];
-                cpx v = f[j + k + i/2] * roots[step * k];
+                f[i+j] = u + v;
+                f[i+j+k] = u - v;
 
-                f[j + k] = u + v;
-                f[j + k + i/2] = u - v;
+                unit *= dir;
             }
+        }
     }
 
     if(inv)
         for(int i=0; i<N; i++) f[i] /= N;
 }
 
-vector<ll> multiply(vector<ll> &a, vector<ll> &b) {
-    vector<cpx> a_(a.begin(), a.end());
-    vector<cpx> b_(b.begin(), b.end());
-
-    int N = 2;
+vector<cpx> multiply(vector<cpx> &a, vector<cpx> &b) {
+    int N = 1;
     while(N < a.size() + b.size()) N *= 2;
 
-    a_.resize(N); FFT(a_, false);
-    b_.resize(N); FFT(b_, false);
+    a.resize(N); FFT(a, false);
+    b.resize(N); FFT(b, false);
 
-    vector<cpx> c_(N);
-    for(int i=0; i<N; i++) c_[i] = a_[i] * b_[i];
-    FFT(c_, true);
-
-    vector<long long> c(N);
-    for(int i=0; i<N; i++) c[i] = (long long)round(c_[i].real());
+    vector<cpx> c(N);
+    for(int i=0; i<N; i++) c[i] = a[i] * b[i];
+    FFT(c, true);
 
     return c;
 }
@@ -71,23 +60,25 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    vector<ll> a(MAX), b(MAX);
+    vector<cpx> a(200001), b(200001);
+    vector<int> dist(200001);
 
     int N; cin >> N;
     while(N--) {
-        int value; cin >> value;
+        int i; cin >> i;
 
-        a[value] = b[value] = 1;
+        a[i] = b[i] = cpx(1, 0);
+        dist[i] = 1;
     }
 
-    vector<ll> c = multiply(a, b);
+    vector<cpx> c = multiply(a, b);
 
     int M; cin >> M;
     int cnt = 0;
     while(M--) {
-        int value; cin >> value;
+        int i; cin >> i;
 
-        if(a[value] > 0 || c[value] > 0) cnt++;
+        if(dist[i] > 0 || round(c[i].real()) > 0) cnt++;
     }
 
     cout << cnt;
--- 백준/Platinum/10531. Golf Bot/README.md
+++ 백준/Platinum/10531. Golf Bot/README.md
@@ -4,27 +4,9 @@
 
 ### 성능 요약
 
-메모리: 34000 KB, 시간: 196 ms
+메모리: 27440 KB, 시간: 140 ms
 
 ### 분류
 
 고속 푸리에 변환(fft), 수학(math)
-
-### 문제 설명
-
-<p><img alt="" src="" style="float:right; height:392px; width:291px">Do you like golf? I hate it. I hate golf so much that I decided to build the ultimate golf robot, a robot that will never miss a shot. I simply place it over the ball, choose the right direction and distance and, flawlessly, it will strike the ball across the air and into the hole. Golf will never be played again.</p>
-
-<p>Unfortunately, it doesn’t work as planned. So, here I am, standing in the green and preparing my first strike when I realize that the distance-selector knob built-in doesn’t have all the distance options! Not everything is lost, as I have 2 shots.</p>
-
-<p>Given my current robot, how many holes will I be able to complete in 2 strokes or less?</p>
-
-### 입력 
-
- <p>The first line has one integer: N, the number of different distances the Golf Bot can shoot. Each of the following N lines has one integer, k<sub>i</sub>, the distance marked in position i of the knob.</p>
-
-<p>Next line has one integer: M, the number of holes in this course. Each of the following M lines has one integer, d<sub>j</sub>, the distance from Golf Bot to hole j.</p>
-
-### 출력 
-
- <p>You should output a single integer, the number of holes Golf Bot will be able to complete. Golf Bot cannot shoot over a hole on purpose and then shoot backwards.</p>
 
