a4876a75ef804d413cb9b86a25055a6f96de692a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:04:56 +0900

    [Bronze II] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 줄 세기 - 4806 
+
+[문제 링크](https://www.acmicpc.net/problem/4806) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 0 ms
+
+### 분류
+
+애드 혹(ad_hoc), 구현(implementation)
+
---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; int cnt = 0;
+    while(getline(cin, str)) cnt++;
+
+    cout << cnt;
+}
