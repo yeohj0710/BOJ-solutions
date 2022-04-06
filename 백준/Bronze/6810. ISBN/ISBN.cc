b2f1728031e1967111f9693be3b347021d9ba1d6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:04:11 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,8 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("The 1-3-sum is %d", 9*1 + 7*3 + 8*1 + 0*3 + 9*1 + 2*3 + 1*1 + 4*3 + 1*1 + 8*3 + a*1 + b*3 + c*1);
+}
