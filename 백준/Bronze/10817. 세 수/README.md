9fcd80a1442073fa6ce14b852540b526d27ba8b6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:51:44 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 세 수 - 10817 
+
+[문제 링크](https://www.acmicpc.net/problem/10817) 
+
+### 성능 요약
+
+메모리: 1112 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a < b) swap(&a, &b);
+    if(b < c) swap(&b, &c);
+    if(a < b) swap(&a, &b);
+    printf("%d", b);
+}
