d367bf3ae6469f1a23e77c5730704894afaa0934
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 00:52:44 +0900

    [Diamond IV] Title: 제곱수의 합 (More Huge), Time: 24 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Diamond/17633. 제곱수의 합 （More Huge）/README.md
@@ -0,0 +1,12 @@
+# [Diamond IV] 제곱수의 합 (More Huge) - 17633 
+
+[문제 링크](https://www.acmicpc.net/problem/17633) 
+
+### 성능 요약
+
+메모리: 2028 KB, 시간: 24 ms
+
+### 분류
+
+수학(math), 밀러–라빈 소수 판별법(miller_rabin), 정수론(number_theory), 폴라드 로(pollard_rho), 소수 판정(primality_test)
+
---
+++ 백준/Diamond/17633. 제곱수의 합 （More Huge）/제곱수의 합 （More Huge）.cc
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+ull Power(__int128 x, __int128 y, __int128 mod) { // ret = (x^y)%mod
+    x %= mod;
+    __int128 ret = 1;
+    while(y > 0) {
+        if(y%2 == 1) ret = (ret*x)%mod;
+        x = (x*x)%mod;
+        y /= 2;
+    }
+    return (ull)ret;
+}
+
+bool checkPrime(ull n, ull a) {
+    if(a%n == 0) return true;
+    ull k = n-1;
+    while(1) {
+        ull temp = Power(a, k, n);
+        if(temp == n-1) return true;
+        if(k%2 == 1) return (temp == 1 || temp == n-1);
+        k /= 2;
+    }
+}
+
+bool isPrime(ull n) {
+    if(n == 2 || n == 3) return true;
+    if(n%2 == 0) return false;
+
+    ull a[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    for(int i=0; i<12; i++)
+        if(!checkPrime(n, a[i])) {
+            return false;
+            break;
+        }
+    return true;
+}
+
+ull GCD(ull a, ull b) {
+    if(a < b) swap(a, b);
+    while(b != 0) {
+        ull r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+ull findDiv(__int128 n) {
+    if(n%2 == 0) return 2;
+    if(isPrime(n)) return n;
+
+    __int128 x = rand()%(n-2) + 2, y = x, c = rand()%10 + 1, g = 1;
+    while(g == 1) {
+        x = (x*x%n + c)%n;
+        y = (y*y%n + c)%n;
+        y = (y*y%n + c)%n;
+        g = GCD(abs(x-y), n);
+        if(g == n) return findDiv(n);
+    }
+    if(isPrime(g)) return g;
+    else return findDiv(g);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    ull N, temp;
+    cin >> N; temp = N;
+
+    vector<ull> List;
+    while(N > 1) {
+        ull div = findDiv(N);
+        List.push_back(div);
+        N /= div;
+    }
+    sort(List.begin(), List.end());
+
+    bool check = true;
+    if(List.size()%2 == 0) {
+        for(int i=0; i<List.size(); i+=2)
+            if(List[i] != List[i+1]) check = false;
+        if(check) { cout << "1"; return 0; }
+    }
+
+    check = true; int exp_cnt = 1;
+    for(int i=1; i<List.size(); i++) {
+        if(List[i] == List[i-1]) exp_cnt++;
+        else {
+            if(List[i-1]%4 == 3 && exp_cnt%2 != 0) check = false;
+            exp_cnt = 1;
+        }
+    }
+    if(List[List.size()-1]%4 == 3 && exp_cnt%2 != 0) check = false;
+    if(check) { cout << "2"; return 0; }
+
+    N = temp;
+    while(N%4 == 0) N /= 4;
+    if(N%8 != 7) cout << "3";
+    else cout << "4";
+}
