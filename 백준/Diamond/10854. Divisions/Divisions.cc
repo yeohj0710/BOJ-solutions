cb8e0b973637532ac3bb49098988a0adc0f9cdbd
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 00:53:31 +0900

    [Diamond V] Title: Divisions, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Diamond/10854. Divisions/Divisions.cc
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+ull Power(__int128 x, __int128 y, __int128 mod) { // ret = (x^y)%mod
+    x %= mod;
+    __int128 ret = 1;
+    while(y > 0) {
+        if(y%2 == 1) ret = (ret*x)%mod;
+        x = (x*x)%mod;
+        y /= 2;
+    }
+    return (ull)ret;
+}
+
+bool checkPrime(ull n, ull a) {
+    if(a%n == 0) return true;
+    ull k = n-1;
+    while(1) {
+        ull temp = Power(a, k, n);
+        if(temp == n-1) return true;
+        if(k%2 == 1) return (temp == 1 || temp == n-1);
+        k /= 2;
+    }
+}
+
+bool isPrime(ull n) {
+    if(n == 2 || n == 3) return true;
+    if(n%2 == 0) return false;
+
+    ull a[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    for(int i=0; i<12; i++)
+        if(!checkPrime(n, a[i])) {
+            return false;
+            break;
+        }
+    return true;
+}
+
+ull GCD(ull a, ull b) {
+    if(a < b) swap(a, b);
+    while(b != 0) {
+        ull r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+ull findDiv(__int128 n) {
+    if(n%2 == 0) return 2;
+    if(isPrime(n)) return n;
+
+    __int128 x = rand()%(n-2) + 2, y = x, c = rand()%10 + 1, g = 1;
+    while(g == 1) {
+        x = (x*x%n + c)%n;
+        y = (y*y%n + c)%n;
+        y = (y*y%n + c)%n;
+        g = GCD(abs(x-y), n);
+        if(g == n) return findDiv(n);
+    }
+    if(isPrime(g)) return g;
+    else return findDiv(g);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    ull N;
+    cin >> N;
+    if(N == 1) { cout << "1"; return 0; }
+
+    vector<ull> ansList;
+    while(N > 1) {
+        ull div = findDiv(N);
+        ansList.push_back(div);
+        N /= div;
+    }
+
+    sort(ansList.begin(), ansList.end());
+    ull ans = 1, mulNum = 2;
+    for(int i=0; i<ansList.size()-1; i++) {
+        if(ansList[i] == ansList[i+1]) mulNum++;
+        else ans *= mulNum, mulNum = 2;
+    }
+    ans *= mulNum;
+    cout << ans;
+}
---
+++ 백준/Diamond/10854. Divisions/README.md
@@ -0,0 +1,12 @@
+# [Diamond V] Divisions - 10854 
+
+[문제 링크](https://www.acmicpc.net/problem/10854) 
+
+### 성능 요약
+
+메모리: 2028 KB, 시간: 16 ms
+
+### 분류
+
+수학(math), 밀러–라빈 소수 판별법(miller_rabin), 정수론(number_theory), 폴라드 로(pollard_rho), 소수 판정(primality_test)
+
