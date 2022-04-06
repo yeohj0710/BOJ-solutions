626283fded31ec7d23b2c96723f77efd94dce28e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 00:58:49 +0900

    [Platinum III] Title: 두부 모판 자르기, Time: 0 ms, Memory: 74828 KB -BaekjoonHub

---
+++ 백준/Platinum/10937. 두부 모판 자르기/README.md
@@ -0,0 +1,12 @@
+# [Platinum III] 두부 모판 자르기 - 10937 
+
+[문제 링크](https://www.acmicpc.net/problem/10937) 
+
+### 성능 요약
+
+메모리: 74828 KB, 시간: 0 ms
+
+### 분류
+
+비트마스킹(bitmask), 다이나믹 프로그래밍(dp), 비트필드를 이용한 다이나믹 프로그래밍(dp_bitfield)
+
---
+++ 백준/Platinum/10937. 두부 모판 자르기/두부 모판 자르기.cc
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 2505
+#define INF 1000000000
+using namespace std;
+
+int N, M, Sour = 2501, Sink = 2502;
+int Capacity[MAX][MAX] = {}, Flow[MAX][MAX] = {}, SinCost[MAX][MAX] = {};
+vector<int> Line[MAX];
+
+char Map[51][51];
+int MapToCost[6][6] = {{100, 70, 40}, {70, 50, 30}, {40, 30, 20}};
+
+void AddLine(int From, int To) {
+    Line[From].push_back(To), Line[To].push_back(From);
+    Capacity[From][To] = 1;
+    int Cost;
+    if(From <= 2500 && To <= 2500) Cost = -MapToCost[Map[(From-1)/M+1][(From-1)%M+1]-'A'][Map[(To-1)/M+1][(To-1)%M+1]-'A'];
+    else Cost = 0;
+    SinCost[From][To] = Cost, SinCost[To][From] = -Cost;
+}
+
+int MCMF() {
+    int CostSum = 0;
+    while(true) {
+        bool isInQueue[MAX] = {};
+        int Parent[MAX] = {};
+        int Cost[MAX];
+        fill(Cost, Cost+MAX, INF);
+        Cost[Sour] = 0;
+
+        queue<int> Queue;
+        Queue.push(Sour);
+        isInQueue[Sour] = true;
+
+        while(!Queue.empty()) {
+            int Curr = Queue.front();
+            Queue.pop();
+            isInQueue[Curr] = false;
+            for(int i=0; i<Line[Curr].size(); i++) {
+                int Next = Line[Curr][i];
+                if(Capacity[Curr][Next] - Flow[Curr][Next] > 0
+                   && Cost[Curr] + SinCost[Curr][Next] < Cost[Next]) {
+                    Cost[Next] = Cost[Curr] + SinCost[Curr][Next];
+                    Parent[Next] = Curr;
+                    if(!isInQueue[Next]) {
+                        Queue.push(Next);
+                        isInQueue[Next] = true;
+                    }
+                }
+            }
+        }
+        if(!Parent[Sink]) break;
+
+        int Amount = INF;
+        for(int i=Sink; i!=Sour; i=Parent[i])
+            Amount = min(Amount, Capacity[Parent[i]][i] - Flow[Parent[i]][i]);
+        for(int i=Sink; i!=Sour; i=Parent[i]) {
+            CostSum += Amount*SinCost[Parent[i]][i];
+            Flow[Parent[i]][i] += Amount;
+            Flow[i][Parent[i]] -= Amount;
+        }
+    }
+    return CostSum;
+}
+
+int main() {
+    scanf("%d\n", &N, &M);
+    M = N;
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=M; j++) Map[i][j] = getchar();
+        getchar();
+    }
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=M; j++) {
+            int Node = (i-1)*M+j;
+            if((i+j)%2 == 0) {
+                AddLine(Sour, Node);
+                if(i > 1) AddLine(Node, Node-M);
+                if(i < N) AddLine(Node, Node+M);
+                if(j > 1) AddLine(Node, Node-1);
+                if(j < M) AddLine(Node, Node+1);
+            }
+            AddLine(Node, Sink);
+        }
+    printf("%d", -MCMF());
+}
