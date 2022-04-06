6ffa13c453195ea9ce558045a9dbbea1c20a25de
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:55:46 +0900

    [Bronze III] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,9 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a+b>=c*2) printf("%d", a+b-c*2);
+    else printf("%d", a+b);
+}
