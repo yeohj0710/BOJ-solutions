0f4b8cbb1b523d050b5fccd65e81a6e8161cef05
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:09:40 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.cc
@@ -0,0 +1,23 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a, b, c, temp;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a < b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if(b < c) {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    if(a < b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    printf("%d", a+b);
+}
