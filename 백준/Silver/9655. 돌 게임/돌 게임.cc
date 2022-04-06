7d2f408648ff0dabd1fb38a2fe42743dcbc0b383
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 02:41:03 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+    if(N % 2 == 1) cout << "SK";
+    else cout << "CY";
+}
