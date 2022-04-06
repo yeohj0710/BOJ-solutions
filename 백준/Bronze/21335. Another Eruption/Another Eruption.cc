ee9de5719e91c6d67fad2a6a61ed4e5ace338844
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:10:51 +0900

    [Bronze IV] Title: Another Eruption, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/21335. Another Eruption/Another Eruption.cc
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double Area;
+    scanf("%lf", &Area);
+    printf("%.7lf", sqrt(Area/3.14159265358979)*2*3.14159265358979);
+}
