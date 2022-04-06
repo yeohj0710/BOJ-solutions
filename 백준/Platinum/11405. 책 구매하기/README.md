48c809bcdb9927f3773c63ae2ed40cc989e764f2
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 01:04:07 +0900

    [Platinum IV] Title: 책 구매하기, Time: 28 ms, Memory: 1728 KB -BaekjoonHub

---
+++ 백준/Platinum/11405. 책 구매하기/README.md
@@ -0,0 +1,12 @@
+# [Platinum IV] 책 구매하기 - 11405 
+
+[문제 링크](https://www.acmicpc.net/problem/11405) 
+
+### 성능 요약
+
+메모리: 1728 KB, 시간: 28 ms
+
+### 분류
+
+최대 유량(flow), 최소 비용 최대 유량(mcmf)
+
---
+++ 백준/Platinum/11405. 책 구매하기/책 구매하기.cc
@@ -0,0 +1,77 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 205
+#define INF 1000000000
+#define SOUR 1
+#define SINK 2
+using namespace std;
+
+int Capacity[MAX][MAX], Flow[MAX][MAX], SinDist[MAX][MAX];
+vector<int> Line[MAX];
+
+int MCMF(int Sour, int Sink) {
+    int CostSum = 0;
+    while(true) {
+        bool isInQueue[MAX] = {};
+        int Parent[MAX] = {}, Dist[MAX];
+        fill(Dist, Dist+MAX, INF);
+        Dist[SOUR] = 0;
+
+        queue<int> Queue;
+        Queue.push(Sour);
+        isInQueue[Sour] = true;
+
+        while(!Queue.empty()) {
+            int Cur = Queue.front();
+            Queue.pop();
+            isInQueue[Cur] = false;
+            for(int i=0; i<Line[Cur].size(); i++) {
+                int Next = Line[Cur][i];
+                if(Capacity[Cur][Next] - Flow[Cur][Next] > 0
+                   && Dist[Cur] + SinDist[Cur][Next] < Dist[Next]) {
+                    Dist[Next] = Dist[Cur] + SinDist[Cur][Next];
+                    Parent[Next] = Cur;
+                    if(!isInQueue[Next]) {
+                        Queue.push(Next);
+                        isInQueue[Next] = true;
+                    }
+                }
+            }
+        }
+        if(!Parent[Sink]) break;
+
+        int Amount = INF;
+        for(int i=Sink; i!=Sour; i=Parent[i])
+            Amount = min(Amount, Capacity[Parent[i]][i] - Flow[Parent[i]][i]);
+        for(int i=Sink; i!=Sour; i=Parent[i]) {
+            CostSum += Amount*SinDist[Parent[i]][i];
+            Flow[Parent[i]][i] += Amount;
+            Flow[i][Parent[i]] -= Amount;
+        }
+    }
+    return CostSum;
+}
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i=M+3; i<M+N+3; i++) {
+        Line[i].push_back(SINK), Line[SINK].push_back(i);
+        scanf("%d", &Capacity[i][SINK]);
+    }
+    for(int i=3; i<M+3; i++) {
+        Line[SOUR].push_back(i), Line[i].push_back(SOUR);
+        scanf("%d", &Capacity[SOUR][i]);
+    }
+    for(int i=3; i<M+3; i++) {
+        for(int j=M+3; j<M+N+3; j++) {
+            Line[i].push_back(j), Line[j].push_back(i);
+            Capacity[i][j] = INF;
+            scanf("%d", &SinDist[i][j]);
+            SinDist[j][i] = -SinDist[i][j];
+        }
+    }
+    printf("%d", MCMF(SOUR, SINK));
+}
