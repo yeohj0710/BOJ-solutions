9e1d3455aee5abe9a6f4689c2ac22d3bfc492715
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 01:04:01 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 1240 ms, Memory: 10136 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/README.md
@@ -0,0 +1,12 @@
+# [Platinum IV] 열혈강호 2 - 11376 
+
+[문제 링크](https://www.acmicpc.net/problem/11376) 
+
+### 성능 요약
+
+메모리: 10136 KB, 시간: 1240 ms
+
+### 분류
+
+이분 매칭(bipartite_matching)
+
---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 2005;
+
+int Left[MAX], Right[MAX];
+vector<int> Adj[MAX];
+bool Visit[MAX];
+
+bool DFS(int from) {
+    Visit[from] = true;
+    for(int i=0; i<Adj[from].size(); i++) {
+        int to = Adj[from][i];
+        if(Right[to] == -1 || (!Visit[Right[to]] && DFS(Right[to]))) {
+            Left[from] = to, Right[to] = from;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+    for(int from=1; from<=N; from++) {
+        int S; cin >> S;
+        while(S--) {
+            int to; cin >> to;
+            Adj[from*2-1].push_back(to);
+            Adj[from*2].push_back(to);
+        }
+    }
+
+    int match = 0;
+    fill(&Left[1], &Left[MAX], -1), fill(&Right[1], &Right[MAX], -1);
+    for(int i=1; i<=N*2; i++)
+        if(Left[i] == -1) {
+            fill(&Visit[1], &Visit[MAX], false);
+            if(DFS(i)) match++;
+        }
+    cout << match;
+}
