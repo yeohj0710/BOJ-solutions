c2d282483717a8a3f547f1be24faea0dee518fca
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:01:44 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N*5; j++) cout << "@";
+        cout << "\n";
+    }
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) cout << "@";
+        cout << "\n";
+    }
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N*5; j++) cout << "@";
+        cout << "\n";
+    }
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) cout << "@";
+        cout << "\n";
+    }
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N*5; j++) cout << "@";
+        cout << "\n";
+    }
+}
