db57a2a0d04b1876cecf7c3bb887c29517b7138b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:53:16 +0900

    [Bronze III] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int T, a, b;
+    scanf("%d", &T);
+    for(int i=0; i<T; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i+1, a+b);
+    }
+}
