e039a730144275ab9d6e25f3421cd61aefe2825b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 01:16:11 +0900

    [Gold III] Title: 부분 문자열, Time: 80 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Gold/16916. 부분 문자열/README.md
@@ -0,0 +1,12 @@
+# [Gold III] 부분 문자열 - 16916 
+
+[문제 링크](https://www.acmicpc.net/problem/16916) 
+
+### 성능 요약
+
+메모리: 12772 KB, 시간: 80 ms
+
+### 분류
+
+KMP(kmp), 문자열(string)
+
---
+++ 백준/Gold/16916. 부분 문자열/부분 문자열.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> Fail(string Pattern) {
+    vector<int> Pi(Pattern.length());
+    for(int i=1, j=0; i<Pattern.length(); i++) {
+        while(j>0 && Pattern[i] != Pattern[j]) j = Pi[j-1];
+        if(Pattern[i] == Pattern[j]) Pi[i] = ++j;
+    }
+    return Pi;
+}
+
+vector<int> KMP(string Str, string Pattern) {
+    vector<int> Pi = Fail(Pattern), Pos;
+    for(int i=0, j=0; i<Str.length(); i++) {
+        while(j>0 && Str[i] != Pattern[j]) j = Pi[j-1];
+        if(Str[i] == Pattern[j]) {
+            if(j == Pattern.length()-1) {
+                Pos.push_back(i-j);
+                j = Pi[j];
+            }
+            else j++;
+        }
+    }
+    return Pos;
+}
+
+int main() {
+    string Str, Pattern;
+    cin >> Str >> Pattern;
+    vector<int> Pos = KMP(Str, Pattern);
+    cout << !Pos.empty();
+}
