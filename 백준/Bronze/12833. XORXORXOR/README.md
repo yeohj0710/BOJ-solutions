b720780135c24b49f50e700ca1c0bef14d8e190b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:26:04 +0900

    [Bronze II] Title: XORXORXOR, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] XORXORXOR - 12833 
+
+[문제 링크](https://www.acmicpc.net/problem/12833) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+비트마스킹(bitmask), 수학(math)
+
---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+
+    if(c % 2 == 0) cout << a;
+    else cout << int(a ^ b);
+}
