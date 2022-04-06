fb91ddea32781ffa2476834e262ad60a475b1901
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 01:04:56 +0900

    [Platinum IV] Title: 시간 끌기, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Platinum/17402. 시간 끌기/README.md
@@ -0,0 +1,12 @@
+# [Platinum IV] 시간 끌기 - 17402 
+
+[문제 링크](https://www.acmicpc.net/problem/17402) 
+
+### 성능 요약
+
+메모리: 2160 KB, 시간: 4 ms
+
+### 분류
+
+이분 매칭(bipartite_matching)
+
---
+++ 백준/Platinum/17402. 시간 끌기/시간 끌기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 205;
+
+vector<int> Adj[MAX];
+int N, M, K;
+int Left[MAX], Right[MAX];
+bool Visit[MAX];
+
+bool DFS(int from) {
+    Visit[from] = true;
+    for(int i=0; i<Adj[from].size(); i++) {
+        int to = Adj[from][i];
+        if(Right[to] == -1 || (!Visit[Right[to]] && DFS(Right[to]))) {
+            Left[from] = to, Right[to] = from;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> M >> K;
+    while(K--) {
+        int x, y; cin >> x >> y;
+        Adj[x].push_back(y);
+    }
+
+    int match = 0;
+    fill(Left, Left+MAX, -1), fill(Right, Right+MAX, -1);
+    for(int i=1; i<=N; i++) {
+        fill(Visit, Visit+MAX, false);
+        if(DFS(i)) match++;
+    }
+
+    cout << N + M - match;
+}
