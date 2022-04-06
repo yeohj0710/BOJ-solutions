13e22dffe7c7ee5dd6f9b315b87bf3a39c6a3919
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:54:49 +0900

    [Bronze III] Title: Chessboard, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13240. Chessboard/Chessboard.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n, m; cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if((i+j)%2 == 0) cout << "*";
+            else cout << ".";
+        }
+        cout << "\n";
+    }
+}
---
+++ 백준/Bronze/13240. Chessboard/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] Chessboard - 13240 
+
+[문제 링크](https://www.acmicpc.net/problem/13240) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
