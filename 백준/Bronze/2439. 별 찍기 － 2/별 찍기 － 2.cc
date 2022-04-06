90561fa5d1c108c142a480be21c87f182177376a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:34:28 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        for(int j=i+1; j<=n; j++) printf(" ");
+        for(int j=1; j<=i; j++) printf("*");
+        printf("\n");
+    }
+}
