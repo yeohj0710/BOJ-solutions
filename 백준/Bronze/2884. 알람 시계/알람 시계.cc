eae71917a9987ca7119ae74b344d6bdb25b94b69
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 03:40:02 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int H, M;
+    scanf("%d %d", &H, &M);
+    if(M >= 45) printf("%d %d", H, M-45);
+    else if(H >= 1) printf("%d %d", H-1, M+15);
+    else printf("23 %d", M+15);
+}
