6308ab9ca71476eb751bd585f52431f08ccb6b75
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 04:18:21 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (a+b+c+d+e+f)*5;
+}
