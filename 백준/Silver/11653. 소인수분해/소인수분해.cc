ae6b583f0b87f6b03e2ac8c938f8babe56a572fd
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-07 02:42:09 +0900

    [Silver V] Title: 소인수분해, Time: 32 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for(int i=2; i<=N; i++) {
+        while(N%i == 0) {
+            N /= i;
+            printf("%d\n", i);
+        }
+    }
+}
