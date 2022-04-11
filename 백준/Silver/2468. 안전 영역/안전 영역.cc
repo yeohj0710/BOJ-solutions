e23573d1a27108e02a4001058c279d6cd08315a1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-11 19:56:51 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2732 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define MAX 101
+using namespace std;
+
+int N;
+int arr[MAX][MAX];
+bool visited[MAX][MAX];
+
+void DFS(int x, int y, int z) {
+    visited[x][y] = true;
+
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+        if(arr[nx][ny] > z && !visited[nx][ny]) DFS(nx, ny, z);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N;
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++) cin >> arr[i][j];
+
+    int ans = 0;
+    for(int k=0; k<100; k++) {
+        int cnt = 0;
+        memset(visited, false, sizeof(visited));
+
+        for(int i=0; i<N; i++)
+            for(int j=0; j<N; j++)
+                if(arr[i][j] > k && !visited[i][j]) {
+                    DFS(i, j, k);
+                    cnt++;
+                }
+
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+}
