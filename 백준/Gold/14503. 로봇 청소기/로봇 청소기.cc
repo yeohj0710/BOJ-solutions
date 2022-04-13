ae092c7e4a516aaacf48e3cd19dc0da9bf02acdc
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-13 21:39:13 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -3,9 +3,6 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
     int N, M; cin >> N >> M;
     int x, y, d; cin >> x >> y >> d;
 
