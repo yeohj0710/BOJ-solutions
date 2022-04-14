cdea5b3ed53d6064372b9404b3c6a13ccf7d9445
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-14 18:10:07 +0900

    [Silver II] Title: N과 M (11), Time: 120 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> arr, ans;
+
+void DFS(int cnt) {
+    if(cnt == M) {
+        for(int i=0; i<ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i=0; i<arr.size(); i++) {
+        ans.push_back(arr[i]);
+
+        DFS(cnt+1);
+
+        ans.pop_back();
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> M;
+
+    arr.resize(N);
+
+    for(int i=0; i<N; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+
+    DFS(0);
+}
