e56fed7008f19e9ba9435f0621b0bb112e94c4de
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-04-17 19:09:08 +0900

    [Silver III] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N, M; cin >> N >> M;
+
+        while(M--) {
+            int a, b; cin >> a >> b;
+        }
+
+        cout << N-1 << "\n";
+    }
+}
