209242f72105c5e87b47a4e95c4b1536be0e0355
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-03 16:13:44 +0900

    [Bronze III] Title: Test, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8815. Test/Test.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        string ans = "ABCBCDCDADAB";
+
+        int N; cin >> N;
+
+        cout << ans[(N-1)%12] << "\n";
+    }
+}
