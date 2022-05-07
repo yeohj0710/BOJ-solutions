747d4cefe2687a56fd36aaf728365fa794f1da2d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-07 18:01:38 +0900

    [Silver IV] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.cc
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -1,21 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
+main() {
     long long a, b; cin >> a >> b;
-
-    long long x = a, y = b;
-    if(x < y) swap(x, y);
-
-    while(y != 0) {
-        long long temp = x % y;
-        x = y;
-        y = temp;
-    }
-    long long gcd = x;
-
-    cout << (a/gcd) * (b/gcd) * gcd << "\n";
+    cout << a * b / __gcd(a, b);
 }
