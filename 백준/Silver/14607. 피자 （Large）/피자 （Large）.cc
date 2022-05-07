723051a7f54069ef62ec8843bd119818caf5c0fd
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-07 17:06:42 +0900

    [Silver III] Title: 피자 (Large), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    long long N; cin >> N;
+
+    cout << N * (N-1) / 2 << "\n";
+}
