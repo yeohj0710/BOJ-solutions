f3023395d10061a5c2471a07fbd92bcddb0001c2
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-11 15:02:23 +0900

    [Bronze II] Title: Practice: Roll Call, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        string a, b; cin >> a >> b;
+
+        cout << "Case " << t << ": " << b << ", " << a << "\n";
+    }
+}
