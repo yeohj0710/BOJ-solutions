8e36532ba41c1170ce9a2ac60beca0f96c57c2c8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-12 01:40:18 +0900

    [Bronze III] Title: Изгороди, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, a; cin >> N >> a;
+
+    cout << 90 << "\n";
+}
