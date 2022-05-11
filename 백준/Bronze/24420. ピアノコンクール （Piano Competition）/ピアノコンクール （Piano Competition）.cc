4a5b81c23c96fa859ab3de4aafe6ce3384a8d4d7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-12 00:44:17 +0900

    [Bronze II] Title: ピアノコンクール (Piano Competition), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24420. ピアノコンクール （Piano Competition）/ピアノコンクール （Piano Competition）.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> arr(N);
+    for(int i=0; i<N; i++) cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+
+    int sum = 0;
+    for(int i=1; i<N-1; i++) sum += arr[i];
+
+    cout << sum << "\n";
+}
