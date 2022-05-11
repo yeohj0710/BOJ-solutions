2f11a10fd8dc52a93a3429745d4fdb381b4bf7ee
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-12 01:42:56 +0900

    [Bronze II] Title: Greetings!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17548. Greetings！/Greetings！.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+
+    cout << "h";
+    for(int i=0; i<(str.length() - 2)*2; i++) cout << "e";
+    cout << "y\n";
+}
