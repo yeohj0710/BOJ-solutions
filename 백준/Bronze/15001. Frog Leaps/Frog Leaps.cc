41ea8be0fe5ed89dee8511af26ef9719352b544e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-14 18:11:44 +0900

    [Bronze II] Title: Frog Leaps, Time: 100 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Bronze/15001. Frog Leaps/Frog Leaps.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int ans = 0;
+    for(int i=1; i<N; i++) ans += (v[i] - v[i-1]) * (v[i] - v[i-1]);
+
+    cout << ans << "\n";
+}
