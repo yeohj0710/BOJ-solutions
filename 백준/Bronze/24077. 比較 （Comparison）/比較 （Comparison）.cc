0262aaec10042f5399ecb3fc80782b45bfb8a7b7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-15 15:13:57 +0900

    [Bronze II] Title: 比較 (Comparison), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24077. 比較 （Comparison）/比較 （Comparison）.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> u(N), v(M);
+    for(int i=0; i<N; i++) cin >> u[i];
+    for(int i=0; i<M; i++) cin >> v[i];
+
+    int ans = 0;
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++)
+            if(u[i] <= v[j]) ans++;
+
+    cout << ans << "\n";
+}
