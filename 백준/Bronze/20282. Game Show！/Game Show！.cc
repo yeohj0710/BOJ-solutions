b3d29b3d7959dd66f357ca9454cfed5266dfeb9b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-16 17:58:32 +0900

    [Bronze II] Title: Game Show!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20282. Game Show！/Game Show！.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int sum = 100, Max = 100;
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        sum += x;
+
+        Max = max(Max, sum);
+    }
+
+    cout << Max << "\n";
+}
+
