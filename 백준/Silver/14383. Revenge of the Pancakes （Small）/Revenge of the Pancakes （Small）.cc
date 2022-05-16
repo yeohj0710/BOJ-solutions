22532a99769b822c2eaee31e13fb321f1db9a2f7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-17 02:40:39 +0900

    [Silver V] Title: Revenge of the Pancakes (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14383. Revenge of the Pancakes （Small）/Revenge of the Pancakes （Small）.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        string str; cin >> str;
+
+        int ans = 0;
+        for(int i=1; i<str.length(); i++)
+            if(str[i] != str[i-1]) {
+                for(int j=0; j<i; j++) {
+                    if(str[j] == '+') str[j] = '-';
+                    else str[j] = '+';
+                }
+                ans++;
+            }
+
+        if(str[0] == '-') ans++;
+
+        cout << "Case #" << t << ": " << ans << "\n";
+    }
+}
