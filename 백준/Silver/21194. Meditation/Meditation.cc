ce9fbb19f9d120a01949dc9c161d3bca1f5fdfab
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-17 18:11:17 +0900

    [Silver V] Title: Meditation, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/21194. Meditation/Meditation.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, K; cin >> N >> K;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    int ans = 0;
+    for(int i=0; i<K; i++) ans += v[i];
+
+    cout << ans << "\n";
+}
