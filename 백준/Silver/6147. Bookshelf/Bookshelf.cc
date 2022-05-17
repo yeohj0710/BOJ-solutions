38979c14408d12dba45e63570d2147e63e7ed336
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-17 09:38:30 +0900

    [Silver V] Title: Bookshelf, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

---
+++ 백준/Silver/6147. Bookshelf/Bookshelf.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, K; cin >> N >> K;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    int sum = 0;
+    for(int i=0; i<N; i++) {
+        sum += v[i];
+
+        if(sum >= K) {
+            cout << i+1 << "\n";
+            break;
+        }
+    }
+}
