6e8e42999ae30a7c63e38c33de1f67cb91546e2f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-20 01:02:04 +0900

    [Bronze II] Title: Tetrahedral Stacks of Cannonballs, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4482. Tetrahedral Stacks of Cannonballs/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] Tetrahedral Stacks of Cannonballs - 4482 
+
+[문제 링크](https://www.acmicpc.net/problem/4482) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+다이나믹 프로그래밍(dp), 수학(math)
+
---
+++ 백준/Bronze/4482. Tetrahedral Stacks of Cannonballs/Tetrahedral Stacks of Cannonballs.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        int N; cin >> N;
+
+        int ans = 0;
+        for(int i=1; i<=N; i++) ans += i * (i+1) / 2;
+
+        cout << t << ": " << N << " " << ans << "\n";
+    }
+}
