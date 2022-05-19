c8006fcfb1e6e367409de6ed1b219f08a4ea2a04
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-19 17:58:23 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 100 ms, Memory: 8936 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -1,57 +1,61 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
 int N;
-vector<int> Arr, Tree;
+vector<int> v, tree;
 
-void initTree(int Begin, int End, int Node) {
-    if(Begin == End) {
-        Tree[Node] = Begin;
+void init(int b, int e, int n) {
+    if(b == e) {
+        tree[n] = b;
         return;
     }
-    int Mid = (Begin+End)/2;
-    initTree(Begin, Mid, Node*2);
-    initTree(Mid+1, End, Node*2+1);
-    if(Arr[Tree[Node*2]] < Arr[Tree[Node*2+1]]) Tree[Node] = Tree[Node*2];
-    else Tree[Node] = Tree[Node*2+1];
+
+    init(b, (b+e)/2, n*2);
+    init((b+e)/2 + 1, e, n*2 + 1);
+
+    if(v[tree[n*2]] < v[tree[n*2 + 1]]) tree[n] = tree[n*2];
+    else tree[n] = tree[n*2 + 1];
 }
 
-int findMinIdx(int Begin, int End, int Node, int Left, int Right) {
-    if(Left > End || Right < Begin) return -1;
-    if(Left <= Begin && Right >= End) return Tree[Node];
-    int Mid = (Begin+End)/2;
-    int leftMinIdx = findMinIdx(Begin, Mid, Node*2, Left, Right);
-    int rightMinIdx = findMinIdx(Mid+1, End, Node*2+1, Left, Right);
-    if(leftMinIdx < 0) return rightMinIdx;
-    if(rightMinIdx < 0) return leftMinIdx;
-    if(Arr[leftMinIdx] < Arr[rightMinIdx]) return leftMinIdx;
-    else return rightMinIdx;
+int fidx(int b, int e, int n, int l, int r) {
+    if(l > e || r < b) return -1;
+    if(l <= b && r >= e) return tree[n];
+
+    int lidx = fidx(b, (b+e)/2, n*2, l, r);
+    int ridx = fidx((b+e)/2 + 1, e, n*2 + 1, l, r);
+
+    if(lidx < 0) return ridx;
+    if(ridx < 0) return lidx;
+
+    if(v[lidx] < v[ridx]) return lidx;
+    else return ridx;
 }
 
-long long maxArea(int Left, int Right) {
-    int minIdx = findMinIdx(0, N-1, 1, Left, Right);
-    long long Ans = (long long)(Right-Left+1)*(long long)Arr[minIdx];
-    if(minIdx+1 <= Right) {
-        long long Area = maxArea(minIdx+1, Right);
-        if(Area > Ans) Ans = Area;
-    }
-    if(minIdx-1 >= Left) {
-        long long Area = maxArea(Left, minIdx-1);
-        if(Area > Ans) Ans = Area;
-    }
-    return Ans;
+int f(int l, int r) {
+    int idx = fidx(1, N, 1, l, r);
+    int ret = (r-l+1)*v[idx];
+
+    if(idx+1 <= r) ret = max(ret, f(idx+1, r));
+    if(idx-1 >= l) ret = max(ret, f(l, idx-1));
+
+    return ret;
 }
 
-int main() {
-    while(1) {
-        scanf("%d", &N);
-        if(!N) break;
-        Arr.resize(N);
-        for(int i=0; i<N; i++) scanf("%d", &Arr[i]);
-        Tree.resize(N*4);
-        initTree(0, N-1, 1);
-        printf("%lld\n", maxArea(0, N-1));
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        cin >> N;
+        if(N == 0) break;
+
+        v.resize(N+1);
+        for(int i=1; i<=N; i++) cin >> v[i];
+
+        tree.resize(N*4);
+        init(1, N, 1);
+
+        cout << f(1, N) << "\n";
     }
 }
