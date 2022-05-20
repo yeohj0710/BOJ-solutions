7720134cc0dacb312005f1ce9d6600cdd91b261c
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-20 17:14:31 +0900

    [Silver IV] Title: 백대열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14490. 백대열/백대열.cc
+++ 백준/Silver/14490. 백대열/백대열.cc
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
 main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
     int a, b; char c; cin >> a >> c >> b;
 
-    cout << a/__gcd(a, b) << ":" << b/__gcd(a, b) << "\n";
+    cout << a/__gcd(a, b) << ":" << b/__gcd(a, b);
 }
