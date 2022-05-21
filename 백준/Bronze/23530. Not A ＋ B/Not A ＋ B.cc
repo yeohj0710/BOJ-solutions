c2da8482e20baf1940611cd5e306f3083782e261
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-21 22:47:37 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int a, b; cin >> a >> b;
+
+        cout << 1 << "\n";
+    }
+}
