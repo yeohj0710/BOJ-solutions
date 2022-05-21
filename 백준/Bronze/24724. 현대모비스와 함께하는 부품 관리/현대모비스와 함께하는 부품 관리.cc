6d69a48c765850dc01f4ca62a1db15a1bfdfc8a6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-21 17:54:08 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        int N; cin >> N;
+
+        int A, B; cin >> A >> B;
+
+        while(N--) {
+            int u, v; cin >> u >> v;
+        }
+
+        cout << "Material Management " << t << "\n";
+        cout << "Classification ---- End!\n";
+    }
+}
