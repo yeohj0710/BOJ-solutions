8f58681722118e48fc2736676b34596152571d57
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-22 20:45:42 +0900

    [Bronze III] Title: Copying Homework, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/19155. Copying Homework/Copying Homework.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    for(int i=0; i<N; i++) cout << N + 1 - v[i] << " ";
+    cout << "\n";
+}
---
+++ 백준/Bronze/19155. Copying Homework/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] Copying Homework - 19155 
+
+[문제 링크](https://www.acmicpc.net/problem/19155) 
+
+### 성능 요약
+
+메모리: 2804 KB, 시간: 20 ms
+
+### 분류
+
+구성적(constructive)
+
