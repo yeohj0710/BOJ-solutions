dc9e621c8731912f9340963da814b4f3576c847e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-23 06:32:12 +0900

    [Silver III] Title: Hexagonal Tiles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int dp[41] = {0, 1, 2};
+
+    for(int i=3; i<=40; i++) dp[i] = dp[i-1] + dp[i-2];
+
+    while(true) {
+        int N; cin >> N;
+        if(N == 0) break;
+
+        cout << dp[N] << "\n";
+    }
+}
