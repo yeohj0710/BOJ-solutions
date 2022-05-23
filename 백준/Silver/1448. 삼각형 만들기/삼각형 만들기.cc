0979088c5db30f3306e8b2474bc6dfb47dea24c0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-23 10:15:35 +0900

    [Silver III] Title: 삼각형 만들기, Time: 164 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    for(int i=0; i<N-2; i++)
+        if(v[i] < v[i+1] + v[i+2]) {
+            cout << v[i] + v[i+1] + v[i+2] << "\n";
+            return 0;
+        }
+
+    cout << -1 << "\n";
+}
