ae867a0cd9d92a22e1d02c9247e8813871613856
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-23 17:13:02 +0900

    [Silver V] Title: 기상청 인턴 신현수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2435. 기상청 인턴 신현수/기상청 인턴 신현수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, K; cin >> N >> K;
+
+    vector<int> v(N+1);
+    for(int i=1; i<=N; i++) {
+        int x; cin >> x;
+        v[i] = v[i-1] + x;
+    }
+
+    int ans = INT_MIN;
+    for(int i=0; i<=N-K; i++) ans = max(ans, v[i+K] - v[i]);
+
+    cout << ans << "\n";
+}
