e1c7f2d713a9fc1a08ad98565b3c8e775ea9c0ae
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-23 17:53:58 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 8 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int a = 0;
+    for(int i=0; i<N; i++) a += v[i];
+
+    int b = 0;
+    for(int i=0; i<N; i++) b += v[i] * v[i];
+
+    cout << (a * a - b)/2 << "\n";
+}
