7ac6f9a0f47d9cd07ab5fa95faee570570a9df77
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-24 09:51:30 +0900

    [Gold V] Title: 택배 배송, Time: 32 ms, Memory: 6420 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+typedef pair<int, int> p;
+vector<vector<p>> adj;
+vector<int> dist;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    adj.resize(N+1);
+    dist.resize(N+1, INT_MAX);
+
+    while(M--) {
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+        adj[b].push_back({a, c});
+    }
+
+    priority_queue<p, vector<p>, greater<p>> pq; // <dist, node>
+    pq.push({0, 1});
+
+    while(!pq.empty()) {
+        int dist1 = pq.top().first;
+        int curr = pq.top().second;
+        pq.pop();
+
+        if(dist[curr] < dist1) continue;
+
+        for(int i=0; i<adj[curr].size(); i++) {
+            int next = adj[curr][i].first;
+            int dist2 = adj[curr][i].second;
+
+            if(dist1 + dist2 < dist[next]) {
+                dist[next] = dist1 + dist2;
+                pq.push({dist[next], next});
+            }
+        }
+    }
+
+    cout << dist[N] << "\n";
+}
