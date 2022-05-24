19fd4ca2e89511028a8ad318b93575c565a8eafb
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-25 00:40:01 +0900

    [Bronze III] Title: Простая задача, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int a, b; cin >> a >> b;
+
+        if((a <= 2 && b <= 1) || (a <= 1 && b <= 2)) cout << "Yes\n";
+        else cout << "No\n";
+    }
+}
