e206afc00933b4e7b11d1259595964293a546c7d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-25 20:21:01 +0900

    [Bronze II] Title: 이진수 합, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8741. 이진수 합/이진수 합.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    for(int i=0; i<N; i++) cout << 1;
+    for(int i=0; i<N-1; i++) cout << 0;
+    cout << "\n";
+}
