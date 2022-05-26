d494dbcaf10ef5c7a020dd6cbc943468188b2cdd
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-26 17:49:39 +0900

    [Bronze II] Title: 상자 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7482. 상자 만들기/상자 만들기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cout << fixed;
+    cout.precision(10);
+
+    int T; cin >> T;
+
+    while(T--) {
+        double x; cin >> x;
+
+        cout << x/6 << "\n";
+    }
+}
