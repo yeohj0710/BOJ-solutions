a7efdfe6b6d646d562a3cca11a48f8b0c87f6e36
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-27 11:20:14 +0900

    [Bronze II] Title: Euler’s Number, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16515. Euler’s Number/Euler’s Number.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    long double ans = 1, mul = 1;
+
+    for(int i=1; i<=N; i++) {
+        mul = mul / i;
+        ans += mul;
+    }
+
+    cout << fixed;
+    cout.precision(12);
+
+    cout << ans << "\n";
+}
