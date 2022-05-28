71855c5852c2452938dd6c7e1a5921a67b3cb84f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-28 20:14:59 +0900

    [Silver IV] Title: Data Packing (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12234. Data Packing （Small）/Data Packing （Small）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        int N, M; cin >> N >> M;
+
+        vector<int> v(N);
+        for(int i=0; i<N; i++) cin >> v[i];
+
+        sort(v.begin(), v.end());
+
+        int i = 0, j = N-1, ans = 0;
+
+        while(i <= j) {
+            if(i == j) {
+                ans++;
+                break;
+            }
+            else if(v[i] + v[j] <= M) {
+                ans++;
+                i++, j--;
+            }
+            else {
+                ans++;
+                j--;
+            }
+        }
+
+        cout << "Case #" << t << ": " << ans << "\n";
+    }
+}
