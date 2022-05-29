c52ad98813c48d0fe0fd757362788be0d738e6af
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-30 08:50:14 +0900

    [Gold II] Title: 브리징 시그널, Time: 28 ms, Memory: 4036 KB -BaekjoonHub

---
+++ 백준/Gold/3066. 브리징 시그널/README.md
@@ -0,0 +1,12 @@
+# [Gold II] 브리징 시그널 - 3066 
+
+[문제 링크](https://www.acmicpc.net/problem/3066) 
+
+### 성능 요약
+
+메모리: 4036 KB, 시간: 28 ms
+
+### 분류
+
+이분 탐색(binary_search), 가장 긴 증가하는 부분 수열: O(n log n)(lis)
+
---
+++ 백준/Gold/3066. 브리징 시그널/브리징 시그널.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N; cin >> N;
+
+        vector<int> v(N);
+        for(int i=0; i<N; i++) cin >> v[i];
+
+        vector<int> u;
+        u.push_back(v[0]);
+
+        int cnt = 0;
+
+        vector<pair<int, int>> dp(N);
+        dp[0] = {v[0], 0};
+
+        for(int i=1; i<N; i++) {
+            if(v[i] > u.back()) {
+                u.push_back(v[i]);
+                cnt++;
+
+                dp[i] = {v[i], cnt};
+            }
+            else {
+                int x = lower_bound(u.begin(), u.end(), v[i]) - u.begin();
+                u[x] = v[i];
+
+                dp[i] = {v[i], x};
+            }
+        }
+
+        cout << cnt + 1 << "\n";
+    }
+}
