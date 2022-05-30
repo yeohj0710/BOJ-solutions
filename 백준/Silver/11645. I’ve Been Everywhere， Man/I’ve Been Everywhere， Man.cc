d4c0c9bf858f5044b284e18a693eb2248b4d69e9
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-30 09:05:23 +0900

    [Silver V] Title: I’ve Been Everywhere, Man, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/11645. I’ve Been Everywhere， Man/I’ve Been Everywhere， Man.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    map<string, bool> m;
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N; cin >> N;
+
+        int ans = 0;
+
+        while(N--) {
+            string str; cin >> str;
+
+            if(!m[str]) ans++;
+
+            m[str] = true;
+        }
+
+        cout << ans << "\n";
+    }
+}
