8f2f4244f4812c35cae17d0723831f919f31f31e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-30 10:40:38 +0900

    [Platinum V] Title: 볼록 껍질, Time: 32 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
@@ -1,54 +1,52 @@
-#include <cstdio>
-#include <vector>
-#include <stack>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-struct Coor { long long x, y; };
-vector<Coor> Point;
+struct P { int x, y; };
+vector<P> v;
 
-long long CCW(Coor a, Coor b, Coor c) {
-    return a.x*b.y + b.x*c.y + c.x*a.y - a.x*c.y - b.x*a.y - c.x*b.y;
+int ccw(P a, P b, P c) {
+    return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
 }
 
-bool minCCW(Coor a, Coor b) {
-    long long Value = CCW(Point[0], a, b);
-    if(Value) return Value > 0;
+bool cmp(P &a, P &b) {
+    int x = ccw(v[0], a, b);
+    if(x != 0) return x > 0;
     else if(a.y != b.y) return a.y < b.y;
     else return a.x < b.x;
 }
 
-int main() {
-    int N;
-    Coor temp1, temp2;
-    stack<Coor> Convex;
-    scanf("%d", &N);
-    Point.resize(N);
-    for(int i=0; i<N; i++) scanf("%lld %lld", &Point[i].x, &Point[i].y);
-    for(int i=1; i<N; i++) {
-        if(Point[i].y < Point[0].y || (Point[i].y == Point[0].y && Point[i].x < Point[0].x)) {
-            long long temp = Point[0].x;
-            Point[0].x = Point[i].x;
-            Point[i].x = temp;
-            temp = Point[0].y;
-            Point[0].y = Point[i].y;
-            Point[i].y = temp;
-        }
-    }
-    sort(Point.begin()+1, Point.end(), minCCW);
-    Convex.push(Point[0]);
-    Convex.push(Point[1]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    v.resize(N);
+    for(int i=0; i<N; i++) cin >> v[i].x >> v[i].y;
+
+    for(int i=1; i<N; i++)
+        if(v[i].y < v[0].y || (v[i].y == v[0].y && v[i].x < v[0].x)) swap(v[i], v[0]);
+
+    sort(v.begin()+1, v.end(), cmp);
+
+    stack<P> s;
+
+    s.push(v[0]);
+    s.push(v[1]);
+
     for(int i=2; i<N; i++) {
-        while(Convex.size() >= 2) {
-            temp2 = Convex.top();
-            Convex.pop();
-            temp1 = Convex.top();
-            if(CCW(temp1, temp2, Point[i]) > 0) {
-                Convex.push(temp2);
+        while(s.size() >= 2) {
+            P a = s.top(); s.pop();
+            P b = s.top();
+
+            if(ccw(b, a, v[i]) > 0) {
+                s.push(a);
                 break;
             }
         }
-        Convex.push(Point[i]);
+        s.push(v[i]);
     }
-    printf("%d", Convex.size());
+
+    cout << s.size() << "\n";
 }
