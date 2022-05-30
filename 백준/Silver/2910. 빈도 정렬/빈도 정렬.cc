7e6cc38ff21dd4460718ef1127305abfc742314f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-05-30 17:34:15 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+map<int, int> m, idx;
+
+bool cmp(int a, int b) {
+    if(m[a] != m[b]) return m[a] > m[b];
+    else return idx[a] < idx[b];
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, C; cin >> N >> C;
+
+    vector<int> v;
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        if(m[x] == 0) idx[x] = i;
+
+        m[x]++;
+
+        bool check = true;
+        for(int j=0; j<v.size(); j++)
+            if(x == v[j]) check = false;
+
+        if(check) v.push_back(x);
+    }
+
+    sort(v.begin(), v.end(), cmp);
+
+    for(int i=0; i<v.size(); i++)
+        for(int j=0; j<m[v[i]]; j++) cout << v[i] << " ";
+    cout << "\n";
+}
