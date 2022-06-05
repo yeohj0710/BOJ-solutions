c6bfeebe874dfe3d21a6cc7b12eef17ebb9b2c10
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-06 06:59:59 +0900

    [Bronze II] Title: Zwycięzcy, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8638. Zwycięzcy/Zwycięzcy.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int Max = 0;
+    for(int i=0; i<N; i++) Max = max(Max, v[i]);
+
+    for(int i=0; i<N; i++)
+        if(v[i] == Max) cout << char('A' + i);
+    cout << "\n";
+}
