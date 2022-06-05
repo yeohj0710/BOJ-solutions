131ba04fd53e2ec1509eaef330d97dc84e3373f7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-06 08:59:00 +0900

    [Bronze II] Title: The n Days of Christmas, Time: 276 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9488. The n Days of Christmas/The n Days of Christmas.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        int N; cin >> N;
+        if(N == 0) break;
+
+        cout << N * (N + 1) * (N + 2) / 6 << "\n";
+    }
+}
