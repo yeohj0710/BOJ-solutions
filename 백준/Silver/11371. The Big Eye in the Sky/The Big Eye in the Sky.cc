66babad54470a6e1f758ddabd06ed2d8255178f4
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-14 12:52:28 +0900

    [Silver V] Title: The Big Eye in the Sky, Time: 0 ms, Memory: 2140 KB -BaekjoonHub

---
+++ 백준/Silver/11371. The Big Eye in the Sky/The Big Eye in the Sky.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        int a, b; cin >> a >> b;
+        if(a == 0 && b == 0) break;
+
+        cout << round(acos(a/sqrt(a*a + b*b)) * (180.0/M_PI)) << "\n";
+    }
+}
