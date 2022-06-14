c74552545a087041cbdc4cffb3e5530f9f12f2a9
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-14 18:12:10 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    if(prev_permutation(v.begin(), v.end())) {
+        for(int i=0; i<N; i++) cout << v[i] << " ";
+        cout << "\n";
+    }
+    else cout << -1 << "\n";
+}
