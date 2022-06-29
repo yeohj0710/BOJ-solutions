b29b0dbea3b823d00c2c602d800404425bb70330
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-29 12:15:44 +0900

    [Platinum I] Title: 보석 가게, Time: 1028 ms, Memory: 71572 KB -BaekjoonHub

---
+++ 백준/Platinum/13575. 보석 가게/보석 가게.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const double PI = acos(-1);
+typedef complex<double> cpx;
+
+void FFT(vector<cpx> &v, bool inv) {
+    int S = v.size();
+
+    for(int i=1, j=0; i<S; i++) {
+        int bit = S/2;
+
+        while(j >= bit) {
+            j -= bit;
+            bit /= 2;
+        }
+        j += bit;
+
+        if(i < j) swap(v[i], v[j]);
+    }
+
+    for(int k=1; k<S; k*=2) {
+        double angle = (inv ? PI/k : -PI/k);
+        cpx w(cos(angle), sin(angle));
+
+        for(int i=0; i<S; i+=k*2) {
+            cpx z(1, 0);
+
+            for(int j=0; j<k; j++) {
+                cpx even = v[i+j];
+                cpx odd = v[i+j+k];
+
+                v[i+j] = even + z*odd;
+                v[i+j+k] = even - z*odd;
+
+                z *= w;
+            }
+        }
+    }
+
+    if(inv)
+        for(int i=0; i<S; i++) v[i] /= S;
+}
+
+vector<int> mul(vector<int> &v, vector<int> &u) {
+    vector<cpx> vc(v.begin(), v.end());
+    vector<cpx> uc(u.begin(), u.end());
+
+    int S = 2;
+    while(S <= max(v.size(), u.size())) S *= 2;
+
+    vc.resize(S); FFT(vc, false);
+    uc.resize(S); FFT(uc, false);
+
+    for(int i=0; i<S; i++) vc[i] *= uc[i];
+    FFT(vc, true);
+
+    vector<int> w(S);
+    for(int i=0; i<S; i++)
+        if(round(vc[i].real()) != 0) w[i] = 1;
+
+    return w;
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(1024);
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+        v[x] = 1;
+    }
+
+    vector<int> u(1, 1);
+
+    while(M > 0) {
+        if(M % 2 == 1) u = mul(u, v);
+
+        v = mul(v, v);
+        M /= 2;
+    }
+
+    for(int i=0; i<u.size(); i++)
+        if(u[i] != 0) cout << i << " ";
+    cout << "\n";
+}
