c4240b79b066505ee73e5676f89a78edafc91277
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-06-30 13:21:48 +0900

    [Gold III] Title: 행성 X3, Time: 184 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2830. 행성 X3/행성 X3.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int cnt[20] = {};
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        for(int j=0; j<20; j++)
+            if(x & (1 << j)) cnt[j]++;
+    }
+
+    int ans = 0;
+
+    for(int i=0; i<20; i++) {
+        int a = cnt[i] * (N - cnt[i]);
+
+        ans += (a << i);
+    }
+
+    cout << ans << "\n";
+}
