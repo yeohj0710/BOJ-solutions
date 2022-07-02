c5851d25050fa5a3b8b0756d53d5f1073b52c3e3
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 04:39:21 +0900

    [Gold V] Title: 토마토, Time: 112 ms, Memory: 26128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,38 +1,57 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-struct node { int x, y; } queue[1000005];
-int tomato[1005][1005] = {0, }, day[1005][1005] = {0, }, var[2][4] = {{1, -1, 0, 0}, {0, 0, 1, -1}};
-int M, N, count = 0, front = 0, rear = 1;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-int BFS() {
-    int popX, popY;
-    while(++front < rear) {
-        for(int i=0; i<4; i++) {
-            popX = queue[front].x + var[0][i];
-            popY = queue[front].y + var[1][i];
-            if(popX >= 1 && popX <= M && popY >= 1 && popY <= N && tomato[popY][popX] == 0) {
-                tomato[popY][popX] = 1;
-                day[popY][popX] = day[queue[front].y][queue[front].x] + 1;
-                queue[rear].x = popX;
-                queue[rear++].y = popY;
-                count--;
+    int N, M; cin >> M >> N;
+
+    vector<vector<int>> v(N, vector<int>(M));
+    vector<vector<int>> cnt(N, vector<int>(M, -1));
+
+    queue<pair<int, int>> q;
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) {
+            cin >> v[i][j];
+
+            if(v[i][j] == 1) {
+                cnt[i][j] = 0;
+                q.push({i, j});
             }
+        }
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+            if(v[nx][ny] != 0 || cnt[nx][ny] != -1) continue;
+
+            cnt[nx][ny] = cnt[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-    if(!count) return day[queue[rear-1].y][queue[rear-1].x];
-    else return -1;
-}
 
-int main() {
-    scanf("%d %d", &M, &N);
-    for(int i=1; i<=N; i++)
-        for(int j=1; j<=M; j++) {
-            scanf("%d", &tomato[i][j]);
-            if(tomato[i][j] == 0) count++;
-            else if(tomato[i][j] == 1) {
-                queue[rear].x = j;
-                queue[rear++].y = i;
-            }
+    bool check = true;
+    int ans = 0;
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) {
+            ans = max(ans, cnt[i][j]);
+            if(v[i][j] == 0 && cnt[i][j] == -1) check = false;
         }
-    printf("%d", BFS());
+
+    if(check) cout << ans << "\n";
+    else cout << -1 << "\n";
 }
