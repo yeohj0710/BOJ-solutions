a8de077e0f6ce812947de15aa843f7fcadf71cc6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 04:50:16 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2352 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define MAX 101
+#define int long long
 using namespace std;
 
 int N;
-char arr1[MAX][MAX], arr2[MAX][MAX];
-bool visit1[MAX][MAX] = {}, visit2[MAX][MAX] = {};
+vector<vector<char>> v, u;
+vector<vector<bool>> vis;
 
-void DFS(int x, int y, int mode) {
-    if(mode == 1) visit1[x][y] = true;
-    else visit2[x][y] = true;
+void DFS(int x, int y, int m) {
+    vis[x][y] = true;
 
     int dx[4] = {1, -1, 0, 0};
     int dy[4] = {0, 0, 1, -1};
@@ -18,41 +17,54 @@ void DFS(int x, int y, int mode) {
         int ny = y + dy[i];
 
         if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+        if(vis[nx][ny]) continue;
 
-        if(mode == 1 && !visit1[nx][ny] && arr1[nx][ny] == arr1[x][y]) DFS(nx, ny, 1);
-        if(mode == 2 && !visit2[nx][ny] && arr2[nx][ny] == arr2[x][y]) DFS(nx, ny, 2);
+        if(m == 1 && v[nx][ny] != v[x][y]) continue;
+        if(m == 2 && u[nx][ny] != u[x][y]) continue;
+
+        DFS(nx, ny, m);
     }
 }
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     cin >> N;
-    cin.ignore();
 
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            cin >> arr1[i][j];
+    v.resize(N, vector<char>(N));
+    u.resize(N, vector<char>(N));
 
-            if(arr1[i][j] == 'R') arr2[i][j] = 'G';
-            else arr2[i][j] = arr1[i][j];
-        }
-        cin.ignore();
-    }
-
-    int cnt1 = 0, cnt2 = 0;
     for(int i=0; i<N; i++)
         for(int j=0; j<N; j++) {
-            if(!visit1[i][j]) {
-                DFS(i, j, 1);
-                cnt1++;
-            }
-            if(!visit2[i][j]) {
-                DFS(i, j, 2);
-                cnt2++;
-            }
+            cin >> v[i][j];
+
+            if(v[i][j] == 'R') u[i][j] = 'G';
+            else u[i][j] = v[i][j];
         }
 
-    cout << cnt1 << " " << cnt2;
+    int ans = 0;
+    vis.resize(N, vector<bool>(N));
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++)
+            if(!vis[i][j]) {
+                DFS(i, j, 1);
+                ans++;
+            }
+
+    cout << ans << " ";
+
+    ans = 0;
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++) vis[i][j] = false;
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<N; j++)
+            if(!vis[i][j]) {
+                DFS(i, j, 2);
+                ans++;
+            }
+
+    cout << ans << "\n";
 }
