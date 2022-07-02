ec8db54e69b1813975e446a149e40b3af59c256b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-02 17:12:40 +0900

    [Gold III] Title: 선분 교차 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
@@ -2,10 +2,10 @@
 #define int long long
 using namespace std;
 
-struct P { int x1, y1, x2, y2; };
+struct P { int x, y; };
 
-int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-    int val = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+int ccw(P a, P b, P c) {
+    int val = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
 
     if(val == 0) return 0;
     else if(val > 0) return 1;
@@ -16,22 +16,12 @@ main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N; N = 2;
+    vector<P> v(4);
+    for(int i=0; i<4; i++) cin >> v[i].x >> v[i].y;
 
-    vector<P> v(N);
-    for(int i=0; i<N; i++)
-        cin >> v[i].x1 >> v[i].y1 >> v[i].x2 >> v[i].y2;
+    int a = ccw(v[0], v[1], v[2]) * ccw(v[0], v[1], v[3]);
+    int b = ccw(v[2], v[3], v[0]) * ccw(v[2], v[3], v[1]);
 
-    vector<int> u(N);
-    for(int i=0; i<N; i++)
-        for(int j=i+1; j<N; j++) {
-            int a = ccw(v[i].x1, v[i].y1, v[i].x2, v[i].y2, v[j].x1, v[j].y1)
-                    * ccw(v[i].x1, v[i].y1, v[i].x2, v[i].y2, v[j].x2, v[j].y2);
-            int b = ccw(v[j].x1, v[j].y1, v[j].x2, v[j].y2, v[i].x1, v[i].y1)
-                    * ccw(v[j].x1, v[j].y1, v[j].x2, v[j].y2, v[i].x2, v[i].y2);
-
-            if((a < 0) && (b < 0)) u[i]++, u[j]++;
-        }
-
-    cout << u[0] << "\n";
+    if((a < 0) && (b < 0)) cout << 1 << "\n";
+    else cout << 0 << "\n";
 }
