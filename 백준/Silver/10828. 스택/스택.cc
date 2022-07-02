3ca2f47a5ebe4f2b390a3079e5879906c1bdeb51
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 04:27:10 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,31 +1,37 @@
-#include<stdio.h>
-#include<string.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int N, digit, stack[10001] = {0, }, top = 0;
-    char input[20];
-    stack[top] = -1;
-    scanf("%d", &N);
-    for(int i=0; i<N; i++) {
-        scanf("%s", input);
-        if(!strcmp(input, "push")) {
-            scanf("%d", &digit);
-            stack[top] = digit;
-            stack[++top] = -1;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    stack<int> s;
+
+    while(N--) {
+        string str; cin >> str;
+
+        if(str == "push") {
+            int x; cin >> x;
+            s.push(x);
         }
-        else if(!strcmp(input, "pop")) {
-            if(!top) printf("-1\n");
-            else {
-                stack[top] = 0;
-                printf("%d\n", stack[--top]);
-                stack[top] = -1;
+        else if(str == "pop") {
+            if(!s.empty()) {
+                cout << s.top() << "\n";
+                s.pop();
             }
+            else cout << -1 << "\n";
         }
-        else if(!strcmp(input, "size")) printf("%d\n", top);
-        else if(!strcmp(input, "empty")) printf("%d\n", !top);
-        else if(!strcmp(input, "top")) {
-            if(!top) printf("-1\n");
-            else printf("%d\n", stack[top-1]);
+        else if(str == "size") cout << s.size() << "\n";
+        else if(str == "empty") {
+            if(s.empty()) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        }
+        else if(str == "top") {
+            if(!s.empty()) cout << s.top() << "\n";
+            else cout << -1 << "\n";
         }
     }
 }
