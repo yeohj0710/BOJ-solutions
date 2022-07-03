a05847b558ea1a1da2fb74dde33c452b1267a1c6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 16:00:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,11 +1,21 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int dp[100] = {0, 1, 2, 4}, T, digit;
-    for(int i=4; i<=100; i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        scanf("%d", &digit);
-        printf("%d\n", dp[digit]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int dp[11] = {0, 1, 2, 4};
+
+    for(int i=4; i<=10; i++)
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+
+    int T; cin >> T;
+
+    while(T--) {
+        int x; cin >> x;
+
+        cout << dp[x] << "\n";
     }
 }
