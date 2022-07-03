31d9c34e06c35fc038bd1e1e3327115d2ace7eff
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 00:24:11 +0900

    [Bronze I] Title: Triangles (Bronze), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/18786. Triangles （Bronze）/Triangles （Bronze）.cc
+++ 백준/Bronze/18786. Triangles （Bronze）/Triangles （Bronze）.cc
@@ -2,18 +2,16 @@
 #define int long long
 using namespace std;
 
+struct P { int x, y; };
+
 main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N; cin >> N;
 
-    vector<pair<int, int>> v(N);
-    for(int i=0; i<N; i++) {
-        int x, y; cin >> x >> y;
-
-        v[i] = {x, y};
-    }
+    vector<P> v(N);
+    for(int i=0; i<N; i++) cin >> v[i].x >> v[i].y;
 
     int ans = 0;
     for(int i=0; i<N; i++)
@@ -21,8 +19,8 @@ main() {
             for(int k=0; k<N; k++) {
                 if(i == j || j == k || k == i) continue;
 
-                if(v[i].first == v[j].first && v[i].second == v[k].second)
-                    ans = max(ans, abs(v[i].first - v[k].first) * abs(v[i].second - v[j].second));
+                if(v[i].x == v[j].x && v[i].y == v[k].y)
+                    ans = max(ans, abs(v[i].x * (v[j].y - v[k].y) + v[j].x * (v[k].y - v[i].y) + v[k].x * (v[i].y - v[j].y)));
             }
 
     cout << ans << "\n";
