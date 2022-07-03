87c06058e880f55fe0552a088138f89b20dfc16a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 16:37:50 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 5984 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,17 +1,29 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int triangle[505][505] = {0, }, maxSum[505][505] = {0, }, maxVal = 0;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-int max(int a, int b) { return a>b?a:b; }
+    int N; cin >> N;
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++)
-        for(int j=1; j<=i; j++) {
-            scanf("%d", &triangle[i][j]);
-            maxSum[i][j] = max(maxSum[i-1][j-1], maxSum[i-1][j]) + triangle[i][j];
-            if(maxSum[i][j] > maxVal) maxVal = maxSum[i][j];
+    vector<vector<int>> v(N, vector<int>(N));
+    for(int i=0; i<N; i++)
+        for(int j=0; j<=i; j++) cin >> v[i][j];
+
+    vector<vector<int>> dp(N, vector<int>(N));
+    dp[0][0] = v[0][0];
+
+    for(int i=1; i<N; i++)
+        for(int j=0; j<N; j++) {
+            if(j == 0) dp[i][j] = dp[i-1][j] + v[i][j];
+            else if(j == N-1) dp[i][j] = dp[i-1][j-1] + v[i][j];
+            else dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + v[i][j];
         }
-    printf("%d", maxVal);
+
+    int ans = 0;
+    for(int i=0; i<N; i++) ans = max(ans, dp[N-1][i]);
+
+    cout << ans << "\n";
 }
