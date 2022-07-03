ddd9a160421d45b13642eba4cea308b08a25dd9e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 16:56:39 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-bool cmp(pair<int, int> &a, pair<int, int> &b) {
+bool cmp(pair<int, int> a, pair<int, int> b) {
     if(a.second != b.second) return a.second < b.second;
     else return a.first < b.first;
 }
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N; cin >> N;
 
-    vector<pair<int, int>> v(N); // <start time, end time>
+    vector<pair<int, int>> v(N);
     for(int i=0; i<N; i++) cin >> v[i].first >> v[i].second;
 
     sort(v.begin(), v.end(), cmp);
 
     int ans = 0, time = 0;
-    for(int i=0; i<N; i++)
+    for(int i=0; i<N; i++) {
         if(v[i].first >= time) {
             ans++;
             time = v[i].second;
         }
+    }
 
     cout << ans << "\n";
 }
