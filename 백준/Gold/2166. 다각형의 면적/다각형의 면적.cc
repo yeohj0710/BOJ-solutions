6b948235fa15c7ae32652dd1752a98467da236fe
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 15:28:06 +0900

    [Gold V] Title: 다각형의 면적, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,14 +1,26 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
-    int N;
-    double coor[10005][2], sum = 0;
-    scanf("%d" ,&N);
-    for(int i=0; i<N; i++) scanf("%lf %lf", &coor[i][0], &coor[i][1]);
-    for(int i=0; i<N-1; i++) {
-        sum += 0.5 * (coor[0][0]*coor[i][1] + coor[i][0]*coor[i+1][1] + coor[i+1][0]*coor[0][1]
-                        - coor[i][0]*coor[0][1] - coor[i+1][0]*coor[i][1] - coor[0][0]*coor[i+1][1]);
-    }
-    printf("%.1lf", sum>=0?sum:-sum);
+struct P { double x, y; };
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<P> v(N);
+    for(int i=0; i<N; i++) cin >> v[i].x >> v[i].y;
+
+    double ans = 0;
+    for(int i=1; i<N-1; i++)
+        ans += 0.5 * (v[0].x * (v[i].y - v[i+1].y) + v[i].x * (v[i+1].y - v[0].y) + v[i+1].x * (v[0].y - v[i].y));
+
+    ans = abs(ans);
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << ans << "\n";
 }
