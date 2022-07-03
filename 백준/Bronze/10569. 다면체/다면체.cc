4701b85d2271de75942b731afac2796aba83bf80
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 15:30:20 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10569. 다면체/다면체.cc
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n, a, b;
-    scanf("%d", &n);
-    for(int i=0; i<n; i++) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", 2-a+b);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int a, b; cin >> a >> b;
+
+        cout << - a + b + 2 << "\n";
     }
 }
