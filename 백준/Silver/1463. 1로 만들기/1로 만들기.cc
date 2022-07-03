e9b433724e2b4f46d7cfb1792bc06df3ca06c177
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 15:48:19 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,14 +1,22 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int arr[1000005];
-int main() {
-    int N;
-    scanf("%d", &N);
-    arr[1] = 0;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> dp(N+1, INT_MAX);
+    dp[1] = 0;
+
     for(int i=2; i<=N; i++) {
-        arr[i] = arr[i-1] + 1;
-        if(i%2 == 0 && arr[i] > arr[i/2] + 1) arr[i] = arr[i/2] + 1;
-        if(i%3 == 0 && arr[i] > arr[i/3] + 1) arr[i] = arr[i/3] + 1;
+        dp[i] = min(dp[i], dp[i-1] + 1);
+
+        if(i % 2 == 0) dp[i] = min(dp[i], dp[i/2] + 1);
+        if(i % 3 == 0) dp[i] = min(dp[i], dp[i/3] + 1);
     }
-    printf("%d", arr[N]);
+
+    cout << dp[N] << "\n";
 }
