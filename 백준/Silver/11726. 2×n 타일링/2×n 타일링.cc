78dd135049bea24778252e5b95e63839337a8f04
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 16:05:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,8 +1,17 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n, arr[1005] = {0, 1, 2};
-    scanf("%d", &n);
-    for(int i=3; i<=n; i++) arr[i] = (arr[i-1] + arr[i-2])%10007;
-    printf("%d", arr[n]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int dp[1001] = {0, 1, 2};
+
+    int N; cin >> N;
+
+    for(int i=3; i<=N; i++)
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+
+    cout << dp[N] << "\n";
 }
