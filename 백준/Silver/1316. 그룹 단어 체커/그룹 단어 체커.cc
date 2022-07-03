4f93e87a8e50822a283af8b14befa383aa691230
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 16:47:10 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -1,19 +1,30 @@
-#include<stdio.h>
-#include<string.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n, check, count = 0;
-    char word[105], alphabet[26];
-    scanf("%d", &n);
-    for(int i=0; i<n; i++) {
-        check = 0;
-        for(int j=0; j<26; j++) alphabet[j] = 0;
-        scanf("%s", word);
-        for(int j=0; j<strlen(word); j++) {
-            if(alphabet[word[j]-'a'] && (j>0 && word[j] != word[j-1])) check = 1;
-            alphabet[word[j]-'a']++;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    int ans = 0;
+    while(T--) {
+        string str; cin >> str;
+
+        vector<bool> check(26);
+
+        bool check2 = true;
+        for(int i=1; i<str.length(); i++) {
+            if(str[i] != str[i-1]) {
+                if(check[str[i-1] - 'a']) check2 = false;
+                check[str[i-1] - 'a'] = true;
+            }
         }
-        if(!check) count++;
+        if(check[str[str.length()-1] - 'a']) check2 = false;
+
+        if(check2) ans++;
     }
-    printf("%d", count);
+
+    cout << ans << "\n";
 }
