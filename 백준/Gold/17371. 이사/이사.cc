245056786653063133befe8f3db47a481b860093
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 15:44:07 +0900

    [Gold I] Title: 이사, Time: 8 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/17371. 이사/이사.cc
+++ 백준/Gold/17371. 이사/이사.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-struct Point { int x, y; };
+struct P { double x, y; };
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N; cin >> N;
-    vector<Point> point(N);
 
-    for(int i=0; i<N; i++)
-        cin >> point[i].x >> point[i].y;
+    vector<P> v(N);
+    for(int i=0; i<N; i++) cin >> v[i].x >> v[i].y;
 
-    int ans, minDist = INT_MAX;
+    double sum = INT_MAX, ans;
     for(int i=0; i<N; i++) {
-        double maxDist = 0;
+        double Min = INT_MAX, Max = 0;
         for(int j=0; j<N; j++) {
-            double dist = sqrt(pow(point[i].x - point[j].x, 2) + pow(point[i].y - point[j].y, 2));
-            maxDist = max(maxDist, dist);
+            Min = min(Min, sqrt(pow(v[i].x - v[j].x, 2) + pow(v[i].y - v[j].y, 2)));
+            Max = max(Max, sqrt(pow(v[i].x - v[j].x, 2) + pow(v[i].y - v[j].y, 2)));
         }
-        if(maxDist < minDist) {
-            minDist = maxDist;
+
+        if((Min + Max)/2 < sum) {
+            sum = (Min + Max)/2;
             ans = i;
         }
     }
-    cout << point[ans].x << " " << point[ans].y << "\n";
+
+    cout << v[ans].x << " " << v[ans].y << "\n";
 }
