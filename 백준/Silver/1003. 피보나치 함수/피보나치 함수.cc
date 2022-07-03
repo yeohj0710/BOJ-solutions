645ba7df9315503fd8093d476fd4fee22e3acf6a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-03 15:50:57 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,13 +1,24 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int fibo[100], T, N;
-    fibo[0] = 0, fibo[1] = 1;
-    for(int i=2; i<100; i++) fibo[i] = fibo[i-1] + fibo[i-2];
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        scanf("%d", &N);
-        if(N == 0) printf("1 0\n");
-        else printf("%d %d\n", fibo[N-1], fibo[N]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a[41] = {1, 0, 1};
+    int b[41] = {0, 1, 1};
+
+    for(int i=3; i<=40; i++) {
+        a[i] = a[i-1] + a[i-2];
+        b[i] = b[i-1] + b[i-2];
+    }
+
+    int T; cin >> T;
+
+    while(T--) {
+        int x; cin >> x;
+
+        cout << a[x] << " " << b[x] << "\n";
     }
 }
