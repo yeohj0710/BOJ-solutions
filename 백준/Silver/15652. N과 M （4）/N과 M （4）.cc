a50a32d2a47f445a0d276ec04861b36bcf83188f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 17:56:20 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,26 +1,34 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int n, m, check[10] = {0, }, arr[10], confirm;
+int N, M;
+vector<int> v;
 
-void search(int idx) {
-    if(idx <= 0) {
-        confirm = 1;
-        for(int i=1; i<m; i++) if(arr[i] < arr[i-1]) confirm = 0;
-        if(confirm) {
-            for(int i=0; i<m; i++) printf("%d ", arr[i]);
-            printf("\n");
-        }
+void f(int idx) {
+    if(v.size() == M) {
+        bool check = true;
+        for(int i=0; i<v.size()-1; i++)
+            if(v[i] > v[i+1]) check = false;
+        if(!check) return;
+
+        for(int i=0; i<v.size(); i++) cout << v[i] << " ";
+        cout << "\n";
         return;
     }
-    for(int i=1; i<=n; i++) {
-        check[i] = 1;
-        arr[m-idx] = i;
-        search(idx-1);
-        check[i] = 0;
+
+    for(int i=1; i<=N; i++) {
+        v.push_back(i);
+        f(idx+1);
+        v.pop_back();
     }
 }
 
-int main() {
-    scanf("%d %d", &n, &m);
-    search(m);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> M;
+
+    f(1);
 }
