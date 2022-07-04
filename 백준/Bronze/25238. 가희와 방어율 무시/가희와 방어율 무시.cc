b074889f268ca4aab567bbfec774380518ee7429
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 22:33:02 +0900

    [Bronze V] Title: 가희와 방어율 무시, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    double a, b; cin >> a >> b;
+
+    if(a - a*(b/100) < 100) cout << 1 << "\n";
+    else cout << 0 << "\n";
+}
