34bbc81b281998dcb861424d49308623994e030e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 13:49:04 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,36 +1,56 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int field[51][51], visit[51][51], M, N, K, count;
+int N, M, K;
+int v[51][51];
+bool vis[51][51];
 
 void DFS(int x, int y) {
-    visit[x][y] = 1;
-    if(field[x+1][y] && !visit[x+1][y]) DFS(x+1, y);
-    if(field[x-1][y] && !visit[x-1][y]) DFS(x-1, y);
-    if(field[x][y+1] && !visit[x][y+1]) DFS(x, y+1);
-    if(field[x][y-1] && !visit[x][y-1]) DFS(x, y-1);
+    vis[x][y] = true;
+
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+        if(v[nx][ny] != 1 || vis[nx][ny]) continue;
+
+        DFS(nx, ny);
+    }
 }
 
-int main() {
-    int T, x, y;
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        count = 0;
-        scanf("%d %d %d", &M, &N, &K);
-        for(int j=0; j<=M; j++)
-            for(int k=0; k<N; k++) {
-                field[j][k] = 0;
-                visit[j][k] = 0;
-            }
-        for(int j=0; j<K; j++) {
-            scanf("%d %d", &x, &y);
-            field[x][y] = 1;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        cin >> N >> M >> K;
+
+        for(int i=0; i<N; i++)
+            for(int j=0; j<M; j++) v[i][j] = 0;
+
+        while(K--) {
+            int a, b; cin >> a >> b;
+            v[a][b] = 1;
         }
-        for(int j=0; j<M; j++)
-            for(int k=0; k<N; k++)
-                if(field[j][k] && !visit[j][k]) {
-                    DFS(j, k);
-                    count++;
+
+        for(int i=0; i<N; i++)
+            for(int j=0; j<M; j++) vis[i][j] = false;
+
+        int ans = 0;
+        for(int i=0; i<N; i++)
+            for(int j=0; j<M; j++)
+                if(v[i][j] == 1 && !vis[i][j]) {
+                    DFS(i, j);
+                    ans++;
                 }
-        printf("%d\n", count);
+
+        cout << ans << "\n";
     }
 }
