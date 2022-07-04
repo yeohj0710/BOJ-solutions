15c2e23fb590f73210dc8bd451e64949e187a65f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 10:02:28 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N, K; cin >> N >> K;
+    int N, M; cin >> N >> M;
 
-    vector<int> sum(N+1);
-
+    vector<int> v(N+1);
     for(int i=1; i<=N; i++) {
         int x; cin >> x;
 
-        sum[i] = sum[i-1] + x;
+        v[i] = v[i-1] + x;
     }
 
-    int Max = INT_MIN;
-    for(int i=0; i<N-K+1; i++)
-        Max = max(Max, sum[i+K] - sum[i]);
+    int ans = INT_MIN;
+    for(int i=1; i<=N-M+1; i++) ans = max(ans, v[i+M-1] - v[i-1]);
 
-    cout << Max << "\n";
+    cout << ans << "\n";
 }
