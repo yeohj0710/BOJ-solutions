eee9a9c970f597d82adff5b579027e1e4850f4e0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 16:51:18 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    bool winner[1001] = {1, 0, 1, 0, 1}; // SK : 1 / CY : 0
+    int dp[1001] = {0, 0, 1, 0, 1};
+
+    for(int i=5; i<=1000; i++) {
+        if(dp[i-1] && dp[i-3] && dp[i-4]) dp[i] = 0;
+        else dp[i] = 1;
+    }
 
     int N; cin >> N;
 
-    for(int i=5; i<=N; i++)
-        if(winner[i-1] && winner[i-3] && winner[i-4]) winner[i] = 0;
-    else winner[i] = 1;
-
-    if(winner[N]) cout << "SK";
-    else cout << "CY";
+    if(dp[N]) cout << "SK\n";
+    else cout << "CY\n";
 }
