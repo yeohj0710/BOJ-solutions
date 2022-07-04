7b988cf0511c749292466e1996799c064b409a36
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 22:38:39 +0900

    [Bronze IV] Title: The Merchant of Venice, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13496. The Merchant of Venice/The Merchant of Venice.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        int N, a, b; cin >> N >> a >> b;
+
+        int ans = 0;
+        while(N--) {
+            int x, y; cin >> x >> y;
+
+            if(x <= a*b) ans += y;
+        }
+
+        cout << "Data Set " << t << ":\n";
+        cout << ans << "\n\n";
+    }
+}
