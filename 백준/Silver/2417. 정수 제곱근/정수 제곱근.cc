3283ac38b11e03203d8f8ddefb0baeed319ca62d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 23:27:43 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -8,25 +8,15 @@ main() {
 
     int N; cin >> N;
 
-    if(N == 0) {
-        cout << 0 << "\n";
-        return 0;
-    }
-
     int l = 0, r = LLONG_MAX;
-    __int128 ans;
+    while(l <= r) {
+        int m = (l + r)/2;
 
-    while(l+1 < r) {
-        __int128 m = (l + r)/2;
+        unsigned long long mm = m * m;
 
-        if(m*m >= (__int128)N) {
-            ans = m;
-            r = m;
-        }
-        else l = m;
+        if(mm >= N) r = m - 1;
+        else l = m + 1;
     }
 
-    int x = (int)ans;
-
-    cout << x << "\n";
+    cout << l << "\n";
 }
