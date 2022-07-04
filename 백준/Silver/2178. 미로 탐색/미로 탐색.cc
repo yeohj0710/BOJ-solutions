e3d420544f5f6c428030faab5637b477b5441fff
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 17:07:25 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,46 +1,45 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int map[105][105] = {0, }, visit[105][105] = {0, }, dist[105][105], queueX[10005] = {0, }, queueY[10005] = {0, }, N, M;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-void BFS(int X, int Y) {
-    int front = 0, rear = 1, popX, popY;
-    visit[X][Y] = 1;
-    queueX[0] = X, queueY[0] = Y;
-    while(front < rear) {
-        popX = queueX[front], popY = queueY[front++];
-        if(map[popX+1][popY] && !visit[popX+1][popY]) {
-            visit[popX+1][popY] = 1;
-            dist[popX+1][popY] = dist[popX][popY]+1;
-            queueX[rear] = popX+1;
-            queueY[rear++] = popY;
+    int N, M; cin >> N >> M;
+
+    vector<vector<char>> v(N, vector<char>(M));
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) cin >> v[i][j];
+
+    vector<vector<int>> vis(N, vector<int>(M, -1));
+    vis[0][0] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        if(x == N-1 && y == M-1) {
+            cout << vis[x][y] << "\n";
+            break;
         }
-        if(map[popX-1][popY] && !visit[popX-1][popY]) {
-            visit[popX-1][popY] = 1;
-            dist[popX-1][popY] = dist[popX][popY]+1;
-            queueX[rear] = popX-1;
-            queueY[rear++] = popY;
-        }
-        if(map[popX][popY+1] && !visit[popX][popY+1]) {
-            visit[popX][popY+1] = 1;
-            dist[popX][popY+1] = dist[popX][popY]+1;
-            queueX[rear] = popX;
-            queueY[rear++] = popY+1;
-        }
-        if(map[popX][popY-1] && !visit[popX][popY-1]) {
-            visit[popX][popY-1] = 1;
-            dist[popX][popY-1] = dist[popX][popY]+1;
-            queueX[rear] = popX;
-            queueY[rear++] = popY-1;
+
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+            if(v[nx][ny] != '1' || vis[nx][ny] != -1) continue;
+
+            vis[nx][ny] = vis[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-}
-
-int main() {
-    int x, y;
-    scanf("%d %d", &N, &M);
-    for(int i=1; i<=N; i++)
-        for(int j=1; j<=M; j++) scanf("%1d", &map[i][j]);
-    dist[1][1] = 1;
-    BFS(1, 1);
-    printf("%d", dist[N][M]);
 }
