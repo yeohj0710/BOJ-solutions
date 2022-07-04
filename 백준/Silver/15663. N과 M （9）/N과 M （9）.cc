df8155447107a4200c383f67a5c2b9209fe0ac59
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 18:06:21 +0900

    [Silver II] Title: N과 M (9), Time: 20 ms, Memory: 7012 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,33 +1,48 @@
-#include <stdio.h>
-int arr[10], check[10] = {0, }, print_arr[10], n, m, temp;
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-void recursion(int count) {
-    int prev_num = 0;
-    if(!count) {
-        for(int i=0; i<m; i++) printf("%d ", print_arr[i]);
-        printf("\n");
+int N, M;
+vector<int> v, u;
+vector<bool> vis;
+vector<vector<int>> w;
+
+void f(int cnt) {
+    if(u.size() == M) {
+        w.push_back(u);
         return;
     }
-    for(int i=0; i<n; i++) {
-        if(!check[i] && prev_num != arr[i]) {
-            prev_num = arr[i];
-            print_arr[m-count] = arr[i];
-            check[i] = 1;
-            recursion(count-1);
-            check[i] = 0;
+
+    for(int i=0; i<N; i++) {
+        if(!vis[i]) {
+            u.push_back(v[i]);
+            vis[i] = true;
+            f(cnt+1);
+            u.pop_back();
+            vis[i] = false;
         }
     }
 }
 
-int main() {
-    scanf("%d %d", &n, &m);
-    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
-    for(int i=0; i<n; i++)
-        for(int j=i+1; j<n; j++)
-            if(arr[i] > arr[j]) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-    recursion(m);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N >> M;
+
+    v.resize(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    vis.resize(N);
+    f(1);
+
+    sort(w.begin(), w.end());
+    w.erase(unique(w.begin(), w.end()), w.end());
+
+    for(int i=0; i<w.size(); i++) {
+        for(int j=0; j<w[i].size(); j++) cout << w[i][j] << " ";
+        cout << "\n";
+    }
 }
