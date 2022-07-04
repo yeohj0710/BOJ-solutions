a56564accc001907cdbae623513e9cc00697ec38
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 16:00:34 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,13 +1,19 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int T, n;
-    long long int arr[105];
-    arr[1] = 1, arr[2] = 1, arr[3] = 1, arr[4] = 2, arr[5] = 2;
-    for(int i=6; i<=100; i++) arr[i] = arr[i-1] + arr[i-5];
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        scanf("%d", &n);
-        printf("%lld\n", arr[n]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int dp[101] = {0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9};
+    for(int i=11; i<=100; i++) dp[i] = dp[i-1] + dp[i-5];
+
+    int T; cin >> T;
+
+    while(T--) {
+        int x; cin >> x;
+
+        cout << dp[x] << "\n";
     }
 }
