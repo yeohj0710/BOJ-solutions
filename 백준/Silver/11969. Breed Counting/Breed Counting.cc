77ae226256db3eb6ecc7e1fe1fe4d8febdda2617
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 10:55:50 +0900

    [Silver III] Title: Breed Counting, Time: 60 ms, Memory: 5144 KB -BaekjoonHub

--- 백준/Silver/11969. Breed Counting/Breed Counting.cc
+++ 백준/Silver/11969. Breed Counting/Breed Counting.cc
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-const int MAX = 100001;
+int arr[100001][4] = {};
 
-int sum[MAX][4] = {};
-
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N, Q; cin >> N >> Q;
+    int N, M; cin >> N >> M;
 
     for(int i=1; i<=N; i++) {
-        int val; cin >> val;
+        int x; cin >> x;
+
+        arr[i][x] = arr[i-1][x] + 1;
 
         for(int j=1; j<=3; j++) {
-            if(j == val) sum[i][j] = sum[i-1][j] + 1;
-            else sum[i][j] = sum[i-1][j];
+            if(j == x) continue;
+
+            arr[i][j] = arr[i-1][j];
         }
     }
 
-    while(Q--) {
+    while(M--) {
         int a, b; cin >> a >> b;
 
-        for(int i=1; i<=3; i++)
-            cout << sum[b][i] - sum[a-1][i] << " ";
-
+        for(int i=1; i<=3; i++) cout << arr[b][i] - arr[a-1][i] << " ";
         cout << "\n";
     }
 }
