6273f8a19a220214e877b1a0e1b7da7d9d2df695
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 15:10:34 +0900

    [Silver II] Title: 다이나믹이 뭐예요?, Time: 12 ms, Memory: 9892 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
@@ -8,12 +8,13 @@ main() {
 
     int N, M; cin >> N >> M;
 
-    vector<vector<int>> v(N, vector<int>(M));
+    vector<vector<int>> dp(N, vector<int>(M));
+
     for(int i=0; i<N; i++)
         for(int j=0; j<M; j++) {
-            if(i == 0 || j == 0) v[i][j] = 1;
-            else v[i][j] = (v[i-1][j-1] + v[i-1][j] + v[i][j-1]) % (int)(1e9 + 7);
+            if(i == 0 || j == 0) dp[i][j] = 1;
+            else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j] + dp[i][j-1]) % (int)(1e9 + 7);
         }
 
-    cout << v[N-1][M-1] << "\n";
+    cout << dp[N-1][M-1] << "\n";
 }
