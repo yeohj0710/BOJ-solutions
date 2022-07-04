845babd875866af672528e9737d50519228fc0e8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 15:08:36 +0900

    [Silver II] Title: 이동하기, Time: 80 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/README.md
+++ 백준/Silver/11048. 이동하기/README.md
@@ -1,10 +1,10 @@
-# [Silver I] 이동하기 - 11048 
+# [Silver II] 이동하기 - 11048 
 
 [문제 링크](https://www.acmicpc.net/problem/11048) 
 
 ### 성능 요약
 
-메모리: 9848 KB, 시간: 72 ms
+메모리: 17744 KB, 시간: 80 ms
 
 ### 분류
 
--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-const int MAX = 1001;
-
-int arr[MAX][MAX] = {};
-int dp[MAX][MAX] = {};
-
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N, M; cin >> N >> M;
 
-    for(int i=1; i<=N; i++)
-        for(int j=1; j<=M; j++) cin >> arr[i][j];
+    vector<vector<int>> v(N, vector<int>(M));
 
-    for(int i=1; i<=N; i++)
-        for(int j=1; j<=M; j++) {
-            dp[i][j] = max(dp[i-1][j-1] + arr[i][j], dp[i-1][j] + arr[i][j]);
-            dp[i][j] = max(dp[i][j], dp[i][j-1] + arr[i][j]);
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) cin >> v[i][j];
+
+    vector<vector<int>> dp(N, vector<int>(M));
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) {
+            if(i == 0 && j == 0) dp[i][j] = v[i][j];
+            else if(i == 0) dp[i][j] = dp[i][j-1] + v[i][j];
+            else if(j == 0) dp[i][j] = dp[i-1][j] + v[i][j];
+            else dp[i][j] = max(dp[i-1][j-1], max(dp[i-1][j], dp[i][j-1])) + v[i][j];
         }
 
-    cout << dp[N][M] << "\n";
+    cout << dp[N-1][M-1] << "\n";
 }
