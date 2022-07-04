4f7c200268da5204ce8035f29b9469d7ec423b06
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-04 09:59:55 +0900

    [Silver IV] Title: Maximum Subarray, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
@@ -11,15 +11,16 @@ main() {
     while(T--) {
         int N; cin >> N;
 
-        vector<int> v(N);
-        for(int i=0; i<N; i++) cin >> v[i];
+        vector<int> v(N+1);
+        for(int i=1; i<=N; i++) {
+            int x; cin >> x;
 
-        vector<int> sum(N+1);
-        for(int i=1; i<=N; i++) sum[i] = sum[i-1] + v[i-1];
+            v[i] = v[i-1] + x;
+        }
 
         int ans = INT_MIN;
-        for(int i=0; i<=N; i++)
-            for(int j=i+1; j<=N; j++) ans = max(ans, sum[j] - sum[i]);
+        for(int i=1; i<=N; i++)
+            for(int j=i; j<=N; j++) ans = max(ans, v[j] - v[i-1]);
 
         cout << ans << "\n";
     }
