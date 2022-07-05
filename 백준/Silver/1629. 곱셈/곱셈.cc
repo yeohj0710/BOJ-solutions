005dea7f88b23aa6f8a206911b374ab218ed186e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-05 17:36:25 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-long long fastPow(int A, int B, int C) {
-    if(B == 0) return 1;
-    if(B == 1) return A % C;
-
-    if(B % 2 == 0) {
-        long long ret = fastPow(A, B/2, C);
-        return (ret*ret) % C;
-    }
-    else {
-        long long ret = fastPow(A, B-1, C);
-        return (ret*A) % C;
-    }
-}
-
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int A, B, C; cin >> A >> B >> C;
-    cout << fastPow(A, B, C);
+    int a, b, c; cin >> a >> b >> c;
+
+    int ans = 1;
+    while(b > 0) {
+        if(b % 2 == 1) ans = (ans * a) % c;
+
+        a = (a * a) % c;
+        b /= 2;
+    }
+
+    cout << ans << "\n";
 }
