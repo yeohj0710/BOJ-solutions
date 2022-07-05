0d3f3fcae00db634104e3e3a9002d632c4483f66
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-05 20:07:03 +0900

    [Silver V] Title: 학번, Time: 256 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3711. 학번/학번.cc
+++ 백준/Silver/3711. 학번/학번.cc
@@ -21,8 +21,8 @@ main() {
             sort(u.begin(), u.end());
 
             bool check = true;
-            for(int i=0; i<u.size()-1; i++)
-                if(u[i] == u[i+1]) check = false;
+            for(int j=0; j<u.size()-1; j++)
+                if(u[j] == u[j+1]) check = false;
 
             if(check) {
                 cout << i << "\n";
