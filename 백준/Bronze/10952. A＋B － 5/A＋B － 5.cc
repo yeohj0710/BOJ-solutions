20e021b5b2db40790c0c135af891246fc0c3cfb1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-05 14:25:37 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,10 +1,15 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int A, B;
-    while(1) {
-        scanf("%d %d", &A, &B);
-        if(!A && !B) return 0;
-        printf("%d\n", A+B);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        int a, b; cin >> a >> b;
+        if(a == 0 && b == 0) break;
+
+        cout << a + b << "\n";
     }
 }
