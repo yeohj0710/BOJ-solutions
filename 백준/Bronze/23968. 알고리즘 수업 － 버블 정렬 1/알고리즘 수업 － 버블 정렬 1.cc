78ad0f11ac25247a8db20af9ea7ff8229e63a06f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 17:00:47 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 136 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int cnt = 0;
+
+    for(int i=N-1; i>=0; i--)
+        for(int j=0; j<i; j++)
+            if(v[j] > v[j+1]) {
+                swap(v[j], v[j+1]);
+                cnt++;
+
+                if(cnt == M) {
+                    cout << min(v[j], v[j+1]) << " " << max(v[j], v[j+1]) << "\n";
+                    return 0;
+                }
+            }
+
+    cout << -1 << "\n";
+}
