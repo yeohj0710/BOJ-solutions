6fb8bc5ca2ba919f1feab087852a782d4ea30811
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 23:55:27 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,15 +1,22 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int T, N, M, A;
-    long long int ans;
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        ans = 1;
-        scanf("%d %d", &N, &M);
-        A = N > (M-N) ? N : (M-N);
-        for(int j=A+1; j<=M; j++) ans *= j;
-        for(int j=1; j<=(M-A); j++) ans /= j;
-        printf("%d\n", ans);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int v[31][31] = {};
+
+    for(int i=0; i<=30; i++) v[i][0] = 1;
+    for(int i=1; i<=30; i++)
+        for(int j=1; j<=i; j++) v[i][j] = v[i-1][j-1] + v[i-1][j];
+
+    int T; cin >> T;
+
+    while(T--) {
+        int a, b; cin >> a >> b;
+
+        cout << v[b][a] << "\n";
     }
 }
