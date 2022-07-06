a25e72dec5cc3c88856d72a74557a440a33c78ed
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 23:56:47 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -12,7 +12,7 @@ main() {
     for(int i=0; i<N; i++) cin >> v[i];
 
     if(next_permutation(v.begin(), v.end())) {
-        for(int i=0; i<N; i++) cout << v[i] << " " ;
+        for(int i=0; i<N; i++) cout << v[i] << " ";
         cout << "\n";
     }
     else cout << -1 << "\n";
