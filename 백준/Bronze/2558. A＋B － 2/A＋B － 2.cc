bcec4cd46144f427254df7c3a3f32830455801a6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 23:46:35 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a+b);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+
+    cout << a + b << "\n";
 }
