f58e5320ea4fafaf41de9f2fa7aced7edc1080e7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-07 00:02:39 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     string str; cin >> str;
 
-    int L = str.length();
-
-    for(int i=0; i<L; i++) {
+    for(int i=0; i<str.length(); i++) {
         if(str[i] == 'X') {
-            if(i+3 < L && str[i+1] == 'X' && str[i+2] == 'X' && str[i+3] == 'X')
-                str[i] = str[i+1] = str[i+2] = str[i+3] = 'A';
-            else if(i+1 < L && str[i+1] == 'X')
-                str[i] = str[i+1] = 'B';
+            if(i+3 < str.length() && str[i+1] == 'X' && str[i+2] == 'X' && str[i+3] == 'X') {
+                str[i] = 'A', str[i+1] = 'A', str[i+2] = 'A', str[i+3] = 'A';
+            }
+            else if(i+1 < str.length() && str[i+1] == 'X') {
+                str[i] = 'B', str[i+1] = 'B';
+            }
+            else {
+                cout << -1 << "\n";
+                return 0;
+            }
         }
     }
 
-    bool check = true;
-    for(int i=0; i<L; i++)
-        if(str[i] == 'X') check = false;
-
-    if(!check) cout << "-1";
-    else cout << str;
+    cout << str << "\n";
 }
