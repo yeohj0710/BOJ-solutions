50ef43ff49bd812dd8802dec8dab0b1c84387200
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 16:29:18 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 44 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int cnt = 0;
+
+    for(int i=N-1; i>=0; i--) {
+        int Max = INT_MIN, idx;
+        for(int j=0; j<=i; j++) {
+            if(v[j] > Max) {
+                Max = v[j];
+                idx = j;
+            }
+        }
+        if(Max != v[i]) {
+            swap(v[i], v[idx]);
+            cnt++;
+
+            if(cnt == M) {
+                cout << min(v[i], v[idx]) << " " << max(v[i], v[idx]) << "\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << -1 << "\n";
+}
