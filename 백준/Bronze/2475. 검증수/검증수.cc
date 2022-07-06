bded18a1732609262d5f25969ced9623bcdb357a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 23:49:59 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.cc
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int sum = 0, a;
-    for(int i=0; i<5; i++) {
-        scanf("%d", &a);
-        sum += a*a;
-    }
-    printf("%d", sum%10);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    vector<int> v(5);
+    for(int i=0; i<5; i++) cin >> v[i];
+
+    int sum = 0;
+    for(int i=0; i<5; i++) sum += v[i] * v[i];
+
+    cout << sum % 10 << "\n";
 }
