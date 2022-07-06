f65fde2864c0eb4cbfb3f2a202251ab6a8343adc
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-06 17:42:58 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 2, Time: 32 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/24052. 알고리즘 수업 － 삽입 정렬 2/알고리즘 수업 － 삽입 정렬 2.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int cnt = 0;
+
+    for(int i=1; i<N; i++) {
+        int loc = i-1;
+        int x = v[i];
+
+        while(loc >= 0 && x < v[loc]) {
+            v[loc+1] = v[loc];
+            loc--;
+            cnt++;
+
+            if(cnt == M) {
+                for(int i=0; i<N; i++) cout << v[i] << " ";
+                cout << "\n";
+                return 0;
+            }
+        }
+        if(loc + 1 != i) {
+            v[loc+1] = x;
+            cnt++;
+
+            if(cnt == M) {
+                for(int i=0; i<N; i++) cout << v[i] << " ";
+                cout << "\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << -1 << "\n";
+}
