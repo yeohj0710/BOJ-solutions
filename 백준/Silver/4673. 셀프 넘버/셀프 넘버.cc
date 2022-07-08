03e88a311bf960debde04a7529b4c72035e989c7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 15:11:36 +0900

    [Silver V] Title: 셀프 넘버, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -1,15 +1,29 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int j, sum, check[10005] = {0, };
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    vector<bool> v(10001, true);
+
     for(int i=1; i<=10000; i++) {
-        sum = i;
-        j = i;
-        while(j) {
-            sum += j%10;
-            j /= 10;
+        int x = i;
+
+        while(x <= 10000) {
+            int y = x;
+
+            while(x > 0) {
+                y += x % 10;
+                x /= 10;
+            }
+
+            v[y] = false;
+            x = y;
         }
-        if(sum <= 10000) check[sum] = 1;
     }
-    for(int i=1; i<=10000; i++) if(!check[i]) printf("%d\n", i);
+
+    for(int i=1; i<=10000; i++)
+        if(v[i]) cout << i << "\n";
 }
