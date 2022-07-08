8ace856c212f4ae387c646aff344c5103bfdfe38
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 14:41:41 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int max = 0, num, order;
-    for(int i=0; i<9; i++) {
-        scanf("%d", &num);
-        if(num > max) max = num, order = i+1;
-    }
-    printf("%d\n%d", max, order);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    vector<int> v(9);
+
+    for(int i=0; i<9; i++) cin >> v[i];
+
+    int Max = 0;
+    for(int i=0; i<9; i++) Max = max(Max, v[i]);
+
+    for(int i=0; i<9; i++)
+        if(v[i] == Max) {
+            cout << v[i] << "\n";
+            cout << i+1 << "\n";
+        }
 }
