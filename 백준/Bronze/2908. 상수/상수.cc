1bd70f7b337295b22580a67fb7d6d913fdbfefcb
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 15:26:40 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.cc
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -1,11 +1,18 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int reverse(int n) {
-    return (n%10)*100 + (n/10%10)*10 + n/100;
-}
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", reverse(a)>reverse(b)?reverse(a):reverse(b));
+    string a, b; cin >> a >> b;
+
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    int x = stoi(a), y = stoi(b);
+
+    if(x > y) cout << x << "\n";
+    else cout << y << "\n";
 }
