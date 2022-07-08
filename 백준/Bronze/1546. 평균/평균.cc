577a7c812d31d70750f47083ffb74a7b4c8894bb
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 14:45:04 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,12 +1,30 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n, score, max = 0, sum = 0;
-    scanf("%d", &n);
-    for(int i=0; i<n; i++) {
-        scanf("%d", &score);
-        sum += score;
-        if(score > max) max = score;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<double> v(N);
+    double Max = 0;
+
+    for(int i=0; i<N; i++) {
+        cin >> v[i];
+        Max = max(Max, v[i]);
     }
-    printf("%f", (float)sum*100/(max*n));
+
+    for(int i=0; i<N; i++) v[i] = (v[i] / Max) * 100;
+
+    double avg = 0;
+    for(int i=0; i<N; i++) avg += v[i];
+
+    avg /= N;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << avg << "\n";
 }
