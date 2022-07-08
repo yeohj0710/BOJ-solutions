4a0428e0e6c212f74dd7d368ef7e66414e7dc567
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 17:14:22 +0900

    [Bronze II] Title: 분해합, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,17 +1,24 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int arr[1000005] = {0, };
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
-int main() {
-    int n, j, sum;
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++) {
-        sum = j = i;
-        while(j) {
-            sum += j%10;
-            j /= 10;
+    int N; cin >> N;
+
+    for(int i=1; i<=N; i++) {
+        string str = to_string(i);
+
+        int sum = i;
+        for(int j=0; j<str.length(); j++) sum += str[j] - '0';
+
+        if(sum == N) {
+            cout << i << "\n";
+            return 0;
         }
-        if(arr[sum] == 0 || sum < arr[sum]) arr[sum] = i;
     }
-    printf("%d", arr[n]);
+
+    cout << 0 << "\n";
 }
