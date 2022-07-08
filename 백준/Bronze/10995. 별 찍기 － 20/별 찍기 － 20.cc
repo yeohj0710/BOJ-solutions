2f0dbe842bd0f6f1d8ea5772ee3d32cf008f9808
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 17:08:59 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -1,13 +1,21 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=2*n; j++) {
-            if((i+j)%2 == 0) printf("*");
-            else printf(" ");
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    for(int i=1; i<=N; i++) {
+        if(i % 2 == 1) {
+            for(int j=1; j<=N; j++) cout << "* ";
         }
-        printf("\n");
+        else {
+            for(int j=1; j<=N; j++) cout << " *";
+        }
+
+        cout << "\n";
     }
 }
