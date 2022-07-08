522fecec6c82afb86c41cdc3e3a634f381dae9c8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 15:15:18 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.cc
+++ 백준/Silver/1065. 한수/한수.cc
@@ -1,11 +1,29 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int N, sum = 0;
-    scanf("%d", &N);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int ans = 0;
+
     for(int i=1; i<=N; i++) {
-        if(i<100) sum++;
-        else if(i/100 - i%100/10 == i%100/10 - i%10) sum++;
+        if(i < 10) {
+            ans++;
+            continue;
+        }
+
+        string str = to_string(i);
+
+        bool check = true;
+        for(int i=1; i<str.length(); i++)
+            if(str[i] - str[i-1] != str[1] - str[0]) check = false;
+
+        if(check) ans++;
     }
-    printf("%d", sum);
+
+    cout << ans << "\n";
 }
