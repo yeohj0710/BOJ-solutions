e00ded55943f523d3fe75b6bda21e98f54142c3d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-08 14:34:14 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,12 +1,17 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b, c, mul, count[10] = {0, };
-    scanf("%d %d %d", &a, &b, &c);
-    mul = a*b*c;
-    while(mul) {
-        count[mul%10]++;
-        mul /= 10;
-    }
-    for(int i=0; i<10; i++) printf("%d\n", count[i]);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+
+    string str = to_string(a * b * c);
+
+    int cnt[10] = {};
+    for(int i=0; i<str.length(); i++) cnt[str[i] - '0']++;
+
+    for(int i=0; i<10; i++) cout << cnt[i] << "\n";
 }
