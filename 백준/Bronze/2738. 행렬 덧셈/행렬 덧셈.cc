0ae7b84a1c08b55a86e58d5cb543135929124de6
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 16:27:23 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N, M; cin >> N >> M;
 
-    int arr[2][100][100] = {};
-    for(int i=0; i<2; i++)
-        for(int j=0; j<N; j++)
-            for(int k=0; k<M; k++)
-                cin >> arr[i][j][k];
+    vector<vector<int>> v(N, vector<int>(M));
+    vector<vector<int>> u(v);
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) cin >> v[i][j];
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) cin >> u[i][j];
 
     for(int i=0; i<N; i++) {
-        for(int j=0; j<M; j++)
-            cout << arr[0][i][j] + arr[1][i][j] << " ";
+        for(int j=0; j<M; j++) cout << v[i][j] + u[i][j] << " ";
         cout << "\n";
     }
 }
