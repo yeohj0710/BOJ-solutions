0d73e48d029e0947ba6c7108ff114dc797c137d1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 14:31:21 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    cout << "         ,r'\"7\n";
+    cout << "         ,r\'\"7\n";
     cout << "r`-_   ,'  ,/\n";
-    cout << " \\. \". L_r'\n";
+    cout << " \\. \". L_r\'\n";
     cout << "   `~\\/\n";
     cout << "      |\n";
     cout << "      |\n";
+
 }
