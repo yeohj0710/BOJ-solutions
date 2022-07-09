be991da61f953a3105f4a53bed214804c4bf6ff1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 22:47:51 +0900

    [Bronze III] Title: In the Shower, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11176. In the Shower/In the Shower.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int a, b; cin >> a >> b;
+
+        int ans = 0;
+        while(b--) {
+            int x; cin >> x;
+            if(x > a) ans++;
+        }
+
+        cout << ans << "\n";
+    }
+}
