66470e61e55de18933bf28e26950dcf65e2476c4
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 16:25:12 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/8370. Plane/Plane.cc
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    printf("%d", a*b+c*d);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c, d; cin >> a >> b >> c >> d;
+
+    cout << (a * b) + (c * d) << "\n";
 }
