27f7a93185f633115ee9423542fc22ae002f23c8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 14:32:37 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.cc
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -1,8 +1,12 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    if(a > b) printf("%lld", a-b);
-    else printf("%lld", b-a);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+
+    cout << abs(a - b) << "\n";
 }
