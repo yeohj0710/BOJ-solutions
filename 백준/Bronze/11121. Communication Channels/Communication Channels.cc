5119eb9035af6a01ba0e41b150a8cbb51aa24760
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-09 22:59:08 +0900

    [Bronze III] Title: Communication Channels, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11121. Communication Channels/Communication Channels.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        string a, b; cin >> a >> b;
+
+        if(a == b) cout << "OK\n";
+        else cout << "ERROR\n";
+    }
+}
