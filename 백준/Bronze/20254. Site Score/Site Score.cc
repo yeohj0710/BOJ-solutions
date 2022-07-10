3cdfa022732d83fcad375850d5bf715cd9d8ca83
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-10 15:43:41 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20254. Site Score/Site Score.cc
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int a, b, c, d; cin >> a >> b >> c >> d;
-    cout << 56*a + 24*b + 14*c + 6*d;
+
+    int ans = a*56 + b*24 + c*14 + d*6;
+
+    cout << ans << "\n";
 }
