ea1c7cb129eb25f4a26c3441e2ea558e5541e95d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-10 15:50:52 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N; cin >> N;
-    cout << N % 21;
+    int x; cin >> x;
+
+    cout << x % 21 << "\n";
 }
