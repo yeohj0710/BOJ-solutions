64cf0929ce4a0ba45e98b53522b5d0a5870ab8a8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-10 15:51:53 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int a, b; cin >> a >> b;
-    cout << b - a;
+
+    cout << b - a << "\n";
 }
