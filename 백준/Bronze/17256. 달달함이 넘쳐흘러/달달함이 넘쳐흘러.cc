e1a7cceb2cb4f1e664968acf4dc04a2a6e83b72a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-10 15:40:55 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -1,7 +1,21 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b, c, d, e, f;
-    scanf("%d %d %d\n%d %d %d", &a, &b, &c, &d, &e, &f);
-    printf("%d %d %d", d-c, e/b, f-a);
+struct s { int x, y, z; };
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    s a, b, c;
+
+    cin >> a.x >> a.y >> a.z;
+    cin >> c.x >> c.y >> c.z;
+
+    b.x = c.x - a.z;
+    b.y = c.y / a.y;
+    b.z = c.z - a.x;
+
+    cout << b.x << " " << b.y << " " << b.z << "\n";
 }
