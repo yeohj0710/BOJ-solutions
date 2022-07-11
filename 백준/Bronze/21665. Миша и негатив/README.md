448849af55207082d8c0d1c9b230e2fbb7d8054e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 08:48:05 +0900

    [Bronze IV] Title: Миша и негатив, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/21665. Миша и негатив/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] Миша и негатив - 21665 
+
+[문제 링크](https://www.acmicpc.net/problem/21665) 
+
+### 성능 요약
+
+메모리: 2152 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation), 문자열(string)
+
---
+++ 백준/Bronze/21665. Миша и негатив/Миша и негатив.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<vector<char>> v(N, vector<char>(M));
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) cin >> v[i][j];
+
+    int ans = 0;
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++) {
+            char c; cin >> c;
+
+            if(c == v[i][j]) ans++;
+        }
+
+    cout << ans << "\n";
+}
