0fc59d43882b58eed8bcc2c8b7cdd1bc8e179a48
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-11 19:12:59 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -1,8 +1,17 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int sum, a, b, c, d;
-    scanf("%d %d %d\n%d", &a, &b, &c, &d);
-    sum = a * 60 * 60 + b * 60 + c + d;
-    printf("%d %d %d", sum%(24*60*60)/(60*60), sum%(24*60*60)%(60*60)/60, sum%60);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+    int x; cin >> x;
+
+    int sum = a*60*60 + b*60 + c + x;
+
+    if(sum >= 24*60*60) sum %= 24*60*60;
+
+    cout << sum / 3600 << " " << (sum % 3600) / 60 << " " << sum % 60 << "\n";
 }
