1702e25ab38e78e8ad7f9cc3a686788c6b763d32
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 08:56:01 +0900

    [Bronze III] Title: 입실 관리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/5524. 입실 관리/입실 관리.cc
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N; cin >> N;
-    while(N--) {
+    int T; cin >> T;
+
+    while(T--) {
         string str; cin >> str;
+
         for(int i=0; i<str.length(); i++) {
-            if(str[i] >= 'A' && str[i] <= 'Z') cout << (char)(str[i]+'a'-'A');
+            if(str[i] >= 'A' && str[i] <= 'Z')
+                cout << char(str[i] - 'A' + 'a');
             else cout << str[i];
         }
         cout << "\n";
