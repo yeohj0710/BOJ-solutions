6528449d6c0ea50e3af1ca1db91ebc8a44970a93
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-11 19:18:00 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N, M; cin >> N >> M;
-    cin.ignore();
 
-    char arr[10][10] = {};
-
+    vector<vector<char>> v(N, vector<char>(M));
     for(int i=0; i<N; i++)
-        for(int j=0; j<M; j++) cin >> arr[i][j];
+        for(int j=0; j<M; j++) cin >> v[i][j];
 
     for(int i=0; i<N; i++) {
-        for(int j=M-1; j>=0; j--) cout << arr[i][j];
+        for(int j=M-1; j>=0; j--) cout << v[i][j];
         cout << "\n";
     }
 }
