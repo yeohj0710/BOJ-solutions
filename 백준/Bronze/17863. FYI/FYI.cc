7fdd03990e3769b4047cfafac5e5a0a5e3372a8e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 08:45:14 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/17863. FYI/FYI.cc
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -1,8 +1,13 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    if(n/10000 == 555) printf("YES");
-    else printf("NO");
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+
+    if(str.substr(0, 3) == "555") cout << "YES\n";
+    else cout << "NO\n";
 }
--- 백준/Bronze/17863. FYI/README.md
+++ 백준/Bronze/17863. FYI/README.md
@@ -4,9 +4,9 @@
 
 ### 성능 요약
 
-메모리: 1112 KB, 시간: 0 ms
+메모리: 2024 KB, 시간: 0 ms
 
 ### 분류
 
-구현(implementation)
+구현(implementation), 문자열(string)
 
