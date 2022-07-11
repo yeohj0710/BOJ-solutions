ce7e7a3d2853605ad7da9fa53936f5d3780aa1f4
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 08:44:16 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/13985. Equality/Equality.cc
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -1,8 +1,22 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int a, b, c;
-    scanf("%d + %d = %d", &a, &b, &c);
-    if(a+b == c) printf("YES");
-    else printf("NO");
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    double a, b, c;
+    char c1, c2;
+    cin >> a >> c1 >> b >> c2 >> c;
+
+    bool ans = false;
+
+    if(c1 == '+' && a + b == c) ans = true;
+    else if(c1 == '-' && a - b == c) ans = true;
+    else if(c1 == '*' && a * b == c) ans = true;
+    else if(c1 == '/' && a / b == c) ans = true;
+
+    if(ans) cout << "YES\n";
+    else cout << "NO\n";
 }
