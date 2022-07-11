1b19b6ed07076f9d8cee06e0bb49ff4caee87b3e
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 08:59:15 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
@@ -1,10 +1,19 @@
-#include<stdio.h>
-int main() {
-    int n = 0;
-    char chess[10][10];
-    for(int i=0; i<8; i++) scanf("%s", chess[i]);
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    char v[8][8];
+    for(int i=0; i<8; i++)
+        for(int j=0; j<8; j++) cin >> v[i][j];
+
+    int ans = 0;
     for(int i=0; i<8; i++)
         for(int j=0; j<8; j++)
-            if((i+j)%2 == 0 && chess[i][j] == 'F') n++;
-    printf("%d", n);
+            if((i + j) % 2 == 0 && v[i][j] == 'F') ans++;
+
+    cout << ans << "\n";
 }
