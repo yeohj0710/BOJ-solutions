fc5fb1d557dd9a09e74421ef5cc3184102b36ea1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-11 19:09:41 +0900

    [Bronze IV] Title: 전자레인지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.cc
+++ 백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -1,8 +1,22 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    if(n%10 == 0) printf("%d %d %d", n/300, n%300/60, n%300%60/10);
-    else printf("-1");
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int a = N / 300;
+    N -= (N / 300) * 300;
+
+    int b = N / 60;
+    N -= (N / 60) * 60;
+
+    int c = N / 10;
+    N -= (N / 10) * 10;
+
+    if(N == 0) cout << a << " " << b << " " << c << "\n";
+    else cout << -1 << "\n";
 }
