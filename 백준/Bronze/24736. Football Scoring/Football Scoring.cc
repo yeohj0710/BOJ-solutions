4826d141b4c981d1825d064524dfa78f29b99bcb
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-11 18:53:28 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24736. Football Scoring/Football Scoring.cc
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     for(int i=0; i<2; i++) {
-        int a, b, c, d, e;
-        cin >> a >> b >> c >> d >> e;
+        int v[5];
+        for(int j=0; j<5; j++) cin >> v[j];
 
-        int temp = a*6 + b*3 + c*2 + d + e*2;
-        cout << temp << " ";
+        int u[5] = {6, 3, 2, 1, 2};
+
+        int sum = 0;
+        for(int j=0; j<5; j++) sum += v[j]*u[j];
+
+        cout << sum << " ";
     }
+
+    cout << "\n";
 }
