4cdd2fb9a81f79419db9ced0cbf180563492e26b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-12 09:00:10 +0900

    [Bronze III] Title: CAPS, Time: 28 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/15000. CAPS/CAPS.cc
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
@@ -9,4 +10,5 @@ int main() {
 
     for(int i=0; i<str.length(); i++)
         cout << char(str[i] - 'a' + 'A');
+    cout << "\n";
 }
