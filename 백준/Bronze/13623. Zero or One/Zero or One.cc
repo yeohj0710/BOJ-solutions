1c7a3fa598243b1822c2c05dd119dbab6bd88eba
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-13 11:39:13 +0900

    [Bronze IV] Title: Zero or One, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/13623. Zero or One/Zero or One.cc
+++ 백준/Bronze/13623. Zero or One/Zero or One.cc
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int a, b, c; cin >> a >> b >> c;
 
-    if(a == b && b != c) cout << "C";
-    else if(a == c && a != b) cout << "B";
-    else if(b == c && a != b) cout << "A";
-    else cout << "*";
+    if(a == b && b == c) cout << "*\n";
+    else if(a == b) cout << "C\n";
+    else if(b == c) cout << "A\n";
+    else if(c == a) cout << "B\n";
 }
