8831efee385c4b7732afc0ecf7904a76bfd7474d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-13 09:05:48 +0900

    [Bronze IV] Title: 욱 제, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/17356. 욱 제/욱 제.cc
+++ 백준/Bronze/17356. 욱 제/욱 제.cc
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     double a, b; cin >> a >> b;
-    cout << 1/(1+pow(10, (b-a)/400));
+
+    cout << fixed;
+    cout.precision(4);
+
+    cout << 1.0 / (1.0 + pow(10, (b - a)/400)) << "\n";
 }
