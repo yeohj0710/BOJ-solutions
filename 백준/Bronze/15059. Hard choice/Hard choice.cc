bb5607486ac691246d170615be607bca8343ba13
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-13 12:29:51 +0900

    [Bronze IV] Title: Hard choice, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15059. Hard choice/Hard choice.cc
+++ 백준/Bronze/15059. Hard choice/Hard choice.cc
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    int a1, a2, b1, b2, c1, c2, a = 0, b = 0, c = 0;
-    scanf("%d %d %d\n%d %d %d", &a1, &b1, &c1, &a2, &b2, &c2);
-    if(a1 < a2) a = a2 - a1;
-    if(b1 < b2) b = b2 - b1;
-    if(c1 < c2) c = c2 - c1;
-    printf("%d", a+b+c);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a1, b1, c1, a2, b2, c2;
+
+    cin >> a1 >> b1 >> c1;
+    cin >> a2 >> b2 >> c2;
+
+    int ans = max(a2 - a1, 0) + max(b2 - b1, 0) + max(c2 - c1, 0);
+
+    cout << ans << "\n";
 }
