04a74447664754f31f5255ea7b544f7027730331
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-13 09:07:21 +0900

    [Bronze IV] Title: Identifying tea, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11549. Identifying tea/Identifying tea.cc
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.cc
@@ -1,13 +1,19 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int n, a, b, c, d, e, count = 0;
-    scanf("%d", &n);
-    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
-    if(n == a) count++;
-    if(n == b) count++;
-    if(n == c) count++;
-    if(n == d) count++;
-    if(n == e) count++;
-    printf("%d", count);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int x; cin >> x;
+
+    int ans = 0;
+    for(int i=0; i<5; i++) {
+        int y; cin >> y;
+
+        if(x == y) ans++;
+    }
+
+    cout << ans << "\n";
 }
