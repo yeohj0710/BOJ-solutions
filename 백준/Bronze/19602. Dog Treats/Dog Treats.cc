aebcefcc9ae898d1201665a175f3fbf6ae465fd7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-14 09:35:22 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19602. Dog Treats/Dog Treats.cc
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -1,9 +1,13 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
-    int s, m, l;
-    scanf("%d %d %d", &s, &m, &l);
-    if(s+m*2+l*3 >= 10) printf("happy");
-    else printf("sad");
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+
+    if(a + b*2 + c*3 >= 10) cout << "happy\n";
+    else cout << "sad\n";
 }
