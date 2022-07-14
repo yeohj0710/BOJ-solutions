83db83333b1fec24753344389326b416291f37ad
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-14 09:36:19 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/20353. Atrium/Atrium.cc
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -1,9 +1,15 @@
-#include <cstdio>
-#include <cmath>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
-    double a;
-    scanf("%lf", &a);
-    printf("%.7lf", sqrt(a)*4);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    double x; cin >> x;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << sqrt(x) * 4 << "\n";
 }
