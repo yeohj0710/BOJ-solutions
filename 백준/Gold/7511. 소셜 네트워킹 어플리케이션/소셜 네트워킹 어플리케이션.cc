1256b29a637c8e7d0cb7e416bf43a8187e0a3660
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-16 16:53:46 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 136 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+vector<int> v;
+
+int f(int x) {
+    if(x == v[x]) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        cout << "Scenario " << t << ":\n";
+
+        int N; cin >> N;
+
+        v.resize(N);
+        for(int i=0; i<N; i++) v[i] = i;
+
+        int M; cin >> M;
+
+        while(M--) {
+            int a, b; cin >> a >> b;
+
+            if(f(a) != f(b)) v[f(a)] = f(b);
+        }
+
+        int K; cin >> K;
+
+        while(K--) {
+            int a, b; cin >> a >> b;
+
+            if(f(a) == f(b)) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        }
+
+        cout << "\n";
+    }
+}
