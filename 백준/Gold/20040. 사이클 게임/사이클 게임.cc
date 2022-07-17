75d6783fab1219b1d1742c7df9d9339d3d0b4ef7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-18 08:50:49 +0900

    [Gold IV] Title: 사이클 게임, Time: 360 ms, Memory: 11012 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    v.resize(N+1);
+    for(int i=1; i<=N; i++) v[i] = i;
+
+    int ans = 0;
+    for(int i=1; i<=M; i++) {
+        int a, b; cin >> a >> b;
+
+        if(f(a) == f(b) && ans == 0) ans = i;
+
+        v[f(a)] = f(b);
+    }
+
+    cout << ans << "\n";
+}
