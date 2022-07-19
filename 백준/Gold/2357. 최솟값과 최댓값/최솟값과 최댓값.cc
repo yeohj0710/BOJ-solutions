ecfc1df38dff654e22b77f1220b5b7597bab8cf8
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-19 12:52:09 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 132 ms, Memory: 9064 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
@@ -2,42 +2,42 @@
 #define int long long
 using namespace std;
 
-vector<int> v, u, w;
+vector<int> v, u, w; // v : vector, u : min, w : max
 
-int init_u(int b, int e, int n) {
+int initu(int n, int b, int e) {
     if(b == e) return u[n] = v[b];
 
-    int lv = init_u(b, (b+e)/2, n*2);
-    int rv = init_u((b+e)/2 + 1, e, n*2 + 1);
+    int lv = initu(n*2, b, (b+e)/2);
+    int rv = initu(n*2 + 1, (b+e)/2 + 1, e);
 
     return u[n] = min(lv, rv);
 }
 
-int init_w(int b, int e, int n) {
+int initw(int n, int b, int e) {
     if(b == e) return w[n] = v[b];
 
-    int lv = init_w(b, (b+e)/2, n*2);
-    int rv = init_w((b+e)/2 + 1, e, n*2 + 1);
+    int lv = initw(n*2, b, (b+e)/2);
+    int rv = initw(n*2 + 1, (b+e)/2 + 1, e);
 
     return w[n] = max(lv, rv);
 }
 
-int f(int b, int e, int n, int l, int r) {
+int f(int n, int b, int e, int l, int r) {
     if(r < b || e < l) return INT_MAX;
     if(l <= b && e <= r) return u[n];
 
-    int lv = f(b, (b+e)/2, n*2, l, r);
-    int rv = f((b+e)/2 + 1, e, n*2 + 1, l, r);
+    int lv = f(n*2, b, (b+e)/2, l, r);
+    int rv = f(n*2 + 1, (b+e)/2 + 1, e, l, r);
 
     return min(lv, rv);
 }
 
-int g(int b, int e, int n, int l, int r) {
+int g(int n, int b, int e, int l, int r) {
     if(r < b || e < l) return INT_MIN;
     if(l <= b && e <= r) return w[n];
 
-    int lv = g(b, (b+e)/2, n*2, l, r);
-    int rv = g((b+e)/2 + 1, e, n*2 + 1, l, r);
+    int lv = g(n*2, b, (b+e)/2, l, r);
+    int rv = g(n*2 + 1, (b+e)/2 + 1, e, l, r);
 
     return max(lv, rv);
 }
@@ -52,13 +52,14 @@ main() {
     for(int i=1; i<=N; i++) cin >> v[i];
 
     u.resize(N*4);
-    init_u(1, N, 1);
+    initu(1, 1, N);
 
     w.resize(N*4);
-    init_w(1, N, 1);
+    initw(1, 1, N);
 
     while(M--) {
         int a, b; cin >> a >> b;
-        cout << f(1, N, 1, a, b) << " " << g(1, N, 1, a, b) << "\n";
+
+        cout << f(1, 1, N, a, b) << " " << g(1, 1, N, a, b) << "\n";
     }
 }
