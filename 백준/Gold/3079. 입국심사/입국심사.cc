dc4e47c0b5e01b26dcb76b842f5970a873799e34
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-19 22:25:24 +0900

    [Gold V] Title: 입국심사, Time: 76 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int l = 0, r = M * v[N-1], ans = M * v[N-1];
+
+    while(l <= r) {
+        int m = (l + r)/2;
+
+        int sum = 0;
+        for(int i=0; i<N; i++) sum += m / v[i];
+
+        if(sum >= M) {
+            ans = min(ans, m);
+            r = m - 1;
+        }
+        else l = m + 1;
+    }
+
+    cout << ans << "\n";
+}
