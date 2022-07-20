647603adc91260017a26f9cd5e86d7d901746e03
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-20 18:52:41 +0900

    [Silver IV] Title: 해커톤, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/16200. 해커톤/해커톤.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int ans = 0, i = 0;
+
+    while(i < N) {
+        i += v[i];
+        ans++;
+    }
+
+    cout << ans << "\n";
+}
