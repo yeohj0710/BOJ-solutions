312daa86ea4427ae41a6a8f4972e7c7d5a44011b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-20 18:36:56 +0900

    [Bronze II] Title: A+B - 6, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -1,10 +1,34 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
 
-int main() {
-    int T, a, b;
-    scanf("%d", &T);
-    for(int i=0; i<T; i++) {
-        scanf("%d,%d", &a, &b);
-        printf("%d\n", a+b);
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        string str; cin >> str;
+
+        string a = "";
+        int i = 0;
+
+        while(str[i] != ',') {
+            a += str[i];
+            i++;
+        }
+
+        string b = "";
+        i++;
+
+        while(i < str.length()) {
+            b += str[i];
+            i++;
+        }
+
+        int ans = stoi(a) + stoi(b);
+
+        cout << ans << "\n";
     }
 }
