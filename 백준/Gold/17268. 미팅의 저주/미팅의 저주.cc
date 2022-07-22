8548b73698cdc6c83383dce6a51eed53ed1e2d54
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-22 10:46:04 +0900

    [Gold III] Title: 미팅의 저주, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17268. 미팅의 저주/미팅의 저주.cc
+++ 백준/Gold/17268. 미팅의 저주/미팅의 저주.cc
@@ -6,14 +6,11 @@ main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    int N; cin >> N;
-
-    N /= 2;
-
     int dp[5001] = {1, 1};
-
-    for(int i=2; i<=N; i++)
+    for(int i=2; i<=5000; i++)
         for(int j=0; j<i; j++) dp[i] = (dp[i] + dp[j] * dp[i-1-j]) % 987654321;
 
-    cout << dp[N] << "\n";
+    int N; cin >> N;
+
+    cout << dp[N/2] << "\n";
 }
