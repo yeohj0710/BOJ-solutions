929d2f3b0680d02e3a3eb6984c56f3e11a9b247f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-22 17:31:23 +0900

    [Silver III] Title: Opening Ceremony, Time: 12 ms, Memory: 2736 KB -BaekjoonHub

---
+++ 백준/Silver/10263. Opening Ceremony/Opening Ceremony.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    int ans = min(N, v[0]);
+
+    for(int i=1; i<N; i++) {
+        ans = min(ans, i + v[i]);
+    }
+
+    cout << ans << "\n";
+}
