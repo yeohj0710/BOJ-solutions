96e4daf4e9f38c7aafa98e5e8ac812a5471f5bbe
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-22 23:50:53 +0900

    [Silver II] Title: 용돈 관리, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    int Max = 0;
+
+    for(int i=0; i<N; i++) {
+        cin >> v[i];
+        Max = max(Max, v[i]);
+    }
+
+    int l = Max, r = INT_MAX, ans = INT_MAX;
+
+    while(l <= r) {
+        int m = (l + r)/2;
+
+        int cnt = 0, cur = 0;
+
+        for(int i=0; i<N; i++) {
+            while(v[i] > cur) {
+                cur = m;
+                cnt++;
+            }
+
+            cur -= v[i];
+        }
+
+        if(cnt <= M) {
+            ans = min(ans, m);
+            r = m - 1;
+        }
+        else l = m + 1;
+    }
+
+    cout << ans << "\n";
+}
