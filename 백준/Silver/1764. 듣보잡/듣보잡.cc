c8080a0fb2de4c425106be9886b658af4e4b4bc0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-24 23:05:16 +0900

    [Silver IV] Title: 듣보잡, Time: 60 ms, Memory: 12992 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
-#define MAX 101
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     int N, M; cin >> N >> M;
 
-    vector<string> setA(N), setB(M);
+    map<string, bool> m;
 
-    for(int i=0; i<N; i++) cin >> setA[i];
-    for(int i=0; i<M; i++) cin >> setB[i];
+    while(N--) {
+        string str; cin >> str;
 
-    sort(setA.begin(), setA.end());
-    sort(setB.begin(), setB.end());
-
-    int i = 0, j = 0, cnt = 0;
-    vector<string> ansList;
-    while(i < N && j < M) {
-        if(setA[i] == setB[j]) {
-            ansList.push_back(setA[i]);
-            cnt++, i++, j++;
-        }
-        else if(setA[i] < setB[j]) i++;
-        else if(setA[i] > setB[j]) j++;
+        m[str] = true;
     }
 
-    cout << cnt << "\n";
-    for(int i=0; i<ansList.size(); i++) cout << ansList[i] << "\n";
+    vector<string> v;
+
+    while(M--) {
+        string str; cin >> str;
+
+        if(m[str]) v.push_back(str);
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << v.size() << "\n";
+    for(int i=0; i<v.size(); i++) cout << v[i] << "\n";
 }
