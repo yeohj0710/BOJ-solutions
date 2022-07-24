11b4e894fc0924a5f0351ec3acfcd608d7151d32
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-25 08:45:00 +0900

    [Gold III] Title: 괄호, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.cc
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -8,18 +8,15 @@ main() {
 
     int dp[2501] = {1, 1};
     for(int i=2; i<=2500; i++)
-        for(int j=0; j<i; j++) dp[i] = (dp[i] + dp[j] * dp[i-1-j]) % (int)(1e9 + 7);
+        for(int j=0; j<i; j++)
+            dp[i] = (dp[i] + dp[j] * dp[i-1-j]) % (int)(1e9 + 7);
 
     int T; cin >> T;
 
     while(T--) {
         int N; cin >> N;
 
-        if(N % 2 == 1) {
-            cout << 0 << "\n";
-            continue;
-        }
-
-        cout << dp[N/2] << "\n";
+        if(N % 2 == 1) cout << 0 << "\n";
+        else cout << dp[N/2] << "\n";
     }
 }
