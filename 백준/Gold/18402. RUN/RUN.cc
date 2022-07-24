8c83303a5baa4f4d02a7ee8692e3981c8b47d104
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-24 21:10:53 +0900

    [Gold V] Title: RUN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/18402. RUN/RUN.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+typedef pair<int, int> p;
+vector<vector<p>> adj;
+vector<int> dis;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, sour, time, M; cin >> N >> sour >> time >> M;
+
+    adj.resize(N+1);
+    dis.resize(N+1, INT_MAX);
+
+    while(M--) {
+        int a, b, c; cin >> a >> b >> c;
+
+        adj[b].push_back({a, c});
+    }
+
+    dis[sour] = 0;
+
+    priority_queue<p, vector<p>, greater<p>> pq; // <dis, node>
+    pq.push({0, sour});
+
+    while(!pq.empty()) {
+        int dis1 = pq.top().first;
+        int cur = pq.top().second;
+        pq.pop();
+
+        if(dis[cur] < dis1) continue;
+
+        for(int i=0; i<adj[cur].size(); i++) {
+            int nex = adj[cur][i].first;
+            int dis2 = adj[cur][i].second;
+
+            if(dis1 + dis2 < dis[nex]) {
+                dis[nex] = dis1 + dis2;
+                pq.push({dis[nex], nex});
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i=1; i<=N; i++) {
+        if(dis[i] <= time) ans++;
+    }
+
+    cout << ans << "\n";
+}
