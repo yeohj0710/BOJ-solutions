1a8f70931c975f14f37981b042b7c07c0b709565
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-25 23:43:59 +0900

    [Silver I] Title: Knight, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15242. Knight/Knight.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string a, b; cin >> a >> b;
+
+    int bx = a[0] - 'a' + 1, by = a[1] - '0';
+    int ex = b[0] - 'a' + 1, ey = b[1] - '0';
+
+    vector<vector<int>> dis(9, vector<int>(9, -1));
+    dis[bx][by] = 0;
+
+    queue<pair<int, int>> q;
+    q.push({bx, by});
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        if(x == ex && y == ey) {
+            cout << dis[ex][ey] << "\n";
+            break;
+        }
+
+        int dx[8] = {-2, -2, -1, 1, 2, 2, 1, -1};
+        int dy[8] = {-1, 1, 2, 2, 1, -1, -2, -2};
+
+        for(int i=0; i<8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx <= 0 || ny <= 0 || nx > 8 || ny > 8) continue;
+            if(dis[nx][ny] != -1) continue;
+
+            dis[nx][ny] = dis[x][y] + 1;
+            q.push({nx, ny});
+        }
+    }
+}
