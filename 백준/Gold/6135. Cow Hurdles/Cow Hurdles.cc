e023137bbfa7624cc1b6021ec913e486ab8653e0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-30 21:25:56 +0900

    [Gold IV] Title: Cow Hurdles, Time: 44 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Gold/6135. Cow Hurdles/Cow Hurdles.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M, K; cin >> N >> M >> K;
+
+    vector<vector<int>> dis(N+1, vector<int>(N+1, INT_MAX));
+
+    for(int i=1; i<=N; i++) dis[i][i] = 0;
+
+    while(M--) {
+        int a, b, c; cin >> a >> b >> c;
+
+        dis[a][b] = c;
+    }
+
+    for(int k=1; k<=N; k++)
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=N; j++)
+                if(dis[i][k] != INT_MAX && dis[k][j] != INT_MAX) {
+                    if(i == j || i == k || k == j) continue;
+
+                    dis[i][j] = min(dis[i][j], max(dis[i][k], dis[k][j]));
+                }
+
+    while(K--) {
+        int a, b; cin >> a >> b;
+
+        if(dis[a][b] != INT_MAX) cout << dis[a][b] << "\n";
+        else cout << -1 << "\n";
+    }
+}
