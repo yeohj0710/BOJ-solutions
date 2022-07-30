bfc8b86840a2e6c0b31755f08dcb39f958f1009f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-07-30 20:28:36 +0900

    [Gold IV] Title: 보안 시스템 설치, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/2219. 보안 시스템 설치/보안 시스템 설치.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<vector<int>> dis(N+1, vector<int>(N+1, INT_MAX));
+
+    for(int i=1; i<=N; i++) dis[i][i] = 0;
+
+    while(M--) {
+        int a, b, c; cin >> a >> b >> c;
+
+        dis[a][b] = dis[b][a] = c;
+    }
+
+    for(int k=1; k<=N; k++)
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=N; j++)
+                if(dis[i][k] != INT_MAX && dis[k][j] != INT_MAX)
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+
+
+    int ans, Min = INT_MAX;
+
+    for(int i=1; i<=N; i++) {
+        int sum = 0;
+
+        for(int j=1; j<=N; j++) sum += dis[i][j];
+
+        if(sum < Min) {
+            Min = sum;
+            ans = i;
+        }
+    }
+
+    cout << ans << "\n";
+}
