a8ea67aaa9cc938c118fcd31f70737e4fe6ce558
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-01 17:35:58 +0900

    [Silver II] Title: K번째 소수, Time: 188 ms, Memory: 15660 KB -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int Max = 1e7;
+
+    vector<bool> p(Max, true);
+    p[1] = false;
+
+    for(int i=2; i*i<Max; i++)
+        for(int j=2; i*j<Max; j++) p[i*j] = false;
+
+    vector<int> v;
+    for(int i=2; i<Max; i++)
+        if(p[i]) v.push_back(i);
+
+    int N; cin >> N;
+
+    cout << v[N-1] << "\n";
+}
