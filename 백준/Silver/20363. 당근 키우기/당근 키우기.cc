3e3ca8407e5f5722ebe3e44aad1389b4085eb9ed
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-05 16:46:26 +0900

    [Silver IV] Title: 당근 키우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+
+    if(a < b) swap(a, b);
+
+    cout << a + b + b/10 << "\n";
+}
