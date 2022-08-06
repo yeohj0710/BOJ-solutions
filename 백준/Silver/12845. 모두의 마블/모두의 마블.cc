24c8081167577f276311c400fbf00e451601c22b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-06 17:03:44 +0900

    [Silver III] Title: 모두의 마블, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    int ans = v[0] * (N - 1);
+
+    for(int i=1; i<N; i++) ans += v[i];
+
+    cout << ans << "\n";
+}
