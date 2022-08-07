ba32322b31d44efcadf2af1e840b100020b75640
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-07 16:30:25 +0900

    [Silver IV] Title: МАГАЗИН, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/24362. МАГАЗИН/МАГАЗИН.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    int ans = 0;
+
+    for(int i=0; i<N; i++)
+        if(i % M != M-1) ans += v[i];
+
+    cout << ans << "\n";
+}
