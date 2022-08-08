13babe4ad88fe7ad3bcb26edc85a55960d16d991
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-08 12:06:26 +0900

    [Bronze II] Title: 오타맨 고창영, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.cc
+++ 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.cc
@@ -1,16 +1,24 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    int N, idx;
-    cin >> N;
-    while(N--) {
-        string str;
-        cin >> idx >> str;
-        for(int i=0; i<str.length(); i++)
-            if(i+1 != idx) cout << str[i];
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int x; cin >> x;
+
+        string str; cin >> str;
+
+        for(int i=0; i<str.length(); i++) {
+            if(i+1 == x) continue;
+
+            cout << str[i];
+        }
+
         cout << "\n";
     }
 }
