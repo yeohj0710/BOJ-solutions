3ceab78827187956f56e03ed150ef11e9c7588f2
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-12 20:22:46 +0900

    [Gold IV] Title: Bad Cowtractors, Time: 8 ms, Memory: 2496 KB -BaekjoonHub

---
+++ 백준/Gold/7044. Bad Cowtractors/Bad Cowtractors.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int a, b, c; };
+
+bool cmp(s x, s y) { return x.c > y.c; }
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<s> adj(M);
+
+    for(int i=0; i<M; i++)
+        cin >> adj[i].a >> adj[i].b >> adj[i].c;
+
+    sort(adj.begin(), adj.end(), cmp);
+
+    v.resize(N+1);
+    for(int i=1; i<=N; i++) v[i] = i;
+
+    int ans = 0, cnt = 0;
+    bool check = false;
+
+    for(int i=0; i<M; i++) {
+        int a = adj[i].a, b = adj[i].b, c = adj[i].c;
+
+        if(f(a) == f(b)) continue;
+
+        v[f(a)] = f(b);
+
+        ans += c;
+        cnt++;
+
+        if(cnt == N-1) {
+            check = true;
+            break;
+        }
+    }
+
+    if(check) cout << ans << "\n";
+    else cout << -1 << "\n";
+}
