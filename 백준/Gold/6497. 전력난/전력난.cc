461cabd4c0b95252135afa13a45dc5dec54d2cda
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-12 20:13:29 +0900

    [Gold IV] Title: 전력난, Time: 212 ms, Memory: 7044 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int a, b, c; };
+
+bool cmp(s x, s y) { return x.c < y.c; }
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        int N, M; cin >> N >> M;
+        if(N == 0 && M == 0) break;
+
+        vector<s> adj(M);
+        int sum = 0;
+
+        for(int i=0; i<M; i++) {
+            cin >> adj[i].a >> adj[i].b >> adj[i].c;
+
+            sum += adj[i].c;
+        }
+
+        sort(adj.begin(), adj.end(), cmp);
+
+        v.clear();
+        v.resize(N);
+        for(int i=0; i<N; i++) v[i] = i;
+
+        int ans = 0, cnt = 0;
+
+        for(int i=0; i<M; i++) {
+            int a = adj[i].a, b = adj[i].b, c = adj[i].c;
+
+            if(f(a) == f(b)) continue;
+
+            v[f(a)] = f(b);
+
+            ans += c;
+            cnt++;
+
+            if(cnt == N-1) break;
+        }
+
+        ans = sum - ans;
+
+        cout << ans << "\n";
+    }
+}
