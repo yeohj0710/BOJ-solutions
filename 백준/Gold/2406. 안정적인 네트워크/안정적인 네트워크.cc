3bc579b6a79f80e46a9e52663a57dc100df7b344
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-14 10:45:07 +0900

    [Gold III] Title: 안정적인 네트워크, Time: 172 ms, Memory: 20596 KB -BaekjoonHub

---
+++ 백준/Gold/2406. 안정적인 네트워크/안정적인 네트워크.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int a, b, c; };
+
+bool cmp(s x, s y) { return x.c < y.c; }
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<s> adj;
+
+    v.resize(N+1);
+    for(int i=1; i<=N; i++) v[i] = i;
+
+    int cnt = 0;
+
+    while(M--) {
+        int a, b; cin >> a >> b;
+
+        if(f(a) == f(b)) continue;
+
+        v[f(a)] = f(b);
+
+        cnt++;
+    }
+
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=N; j++) {
+            int x; cin >> x;
+
+            if(i == 1 || i >= j) continue;
+
+            adj.push_back({i, j, x});
+        }
+
+    sort(adj.begin(), adj.end(), cmp);
+
+    int ans = 0;
+    vector<pair<int, int>> w;
+
+    for(int i=0; i<adj.size(); i++) {
+        int a = adj[i].a, b = adj[i].b, c = adj[i].c;
+
+        if(f(a) == f(b)) continue;
+
+        v[f(a)] = f(b);
+
+        ans += c;
+        cnt++;
+
+        w.push_back({a, b});
+
+        if(cnt == N-2) break;
+    }
+
+    cout << ans << " " << w.size() << "\n";
+
+    for(int i=0; i<w.size(); i++)
+        cout << w[i].first << " " << w[i].second << "\n";
+}
