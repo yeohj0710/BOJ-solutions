cf0175b6da7db4a6dcf55975eff3ef868bf84cd7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-14 13:29:48 +0900

    [Gold III] Title: 정복자, Time: 12 ms, Memory: 2928 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int a, b, c; };
+
+bool cmp(s x, s y) { return x.c < y.c; }
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M, K; cin >> N >> M >> K;
+
+    vector<s> adj(M);
+
+    for(int i=0; i<M; i++)
+        cin >> adj[i].a >> adj[i].b >> adj[i].c;
+
+    sort(adj.begin(), adj.end(), cmp);
+
+    v.resize(N+1);
+    for(int i=1; i<=N; i++) v[i] = i;
+
+    int ans = 0, cnt = 0, add = 0;
+
+    for(int i=0; i<adj.size(); i++) {
+        int a = adj[i].a, b = adj[i].b, c = adj[i].c;
+
+        if(f(a) == f(b)) continue;
+
+        v[f(a)] = f(b);
+
+        ans += c + add;
+        add += K;
+        cnt++;
+
+        if(cnt == N-1) break;
+    }
+
+    cout << ans << "\n";
+}
