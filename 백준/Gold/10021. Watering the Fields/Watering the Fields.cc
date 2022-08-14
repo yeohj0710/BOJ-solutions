133e01690b53c0eaf7def320ef7d6a2d9163a7e3
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-14 11:47:48 +0900

    [Gold III] Title: Watering the Fields, Time: 336 ms, Memory: 75916 KB -BaekjoonHub

---
+++ 백준/Gold/10021. Watering the Fields/Watering the Fields.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int a, b, c; };
+
+bool cmp(s x, s y) { return x.c < y.c; }
+
+vector<int> v;
+
+int f(int x) {
+    if(v[x] == x) return x;
+    else return v[x] = f(v[x]);
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    vector<pair<double, double>> u(N+1);
+
+    for(int i=1; i<=N; i++)
+        cin >> u[i].first >> u[i].second;
+
+    vector<s> adj;
+
+    for(int i=1; i<=N; i++)
+        for(int j=i+1; j<=N; j++) {
+            int cost = (u[i].first - u[j].first) * (u[i].first - u[j].first)
+                        + (u[i].second - u[j].second) * (u[i].second - u[j].second);
+
+            if(cost >= M) adj.push_back({i, j, cost});
+        }
+
+    sort(adj.begin(), adj.end(), cmp);
+
+    v.resize(N+1);
+    for(int i=1; i<=N; i++) v[i] = i;
+
+    int ans = 0, cnt = 0;
+    bool check = false;
+
+    for(int i=0; i<adj.size(); i++) {
+        int a = adj[i].a, b = adj[i].b, c = adj[i].c;
+
+        if(f(a) == f(b)) continue;
+
+        v[f(a)] = f(b);
+
+        ans += c;
+        cnt++;
+
+        if(cnt == N-1) {
+            check = true;
+            break;
+        }
+    }
+
+    if(check) cout << ans << "\n";
+    else cout << -1 << "\n";
+}
