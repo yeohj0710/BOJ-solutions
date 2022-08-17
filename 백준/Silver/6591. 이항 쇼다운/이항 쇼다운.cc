bfc1fd7432643f5d9860fb8a358aac3bedab5347
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-17 23:18:54 +0900

    [Silver III] Title: 이항 쇼다운, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6591. 이항 쇼다운/이항 쇼다운.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while(true) {
+        int N, M; cin >> N >> M;
+        if(N == 0 && M == 0) break;
+
+        M = min(M, N - M);
+
+        int ans = 1;
+
+        for(int i=1; i<=M; i++) {
+            ans *= N;
+            ans /= i;
+
+            N--;
+        }
+
+        cout << ans << "\n";
+    }
+}
