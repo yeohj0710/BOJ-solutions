e7c4af6dff60720f290938057a71efcdd18077c3
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-19 16:04:39 +0900

    [Silver IV] Title: Sklep, Time: 1932 ms, Memory: 72788 KB -BaekjoonHub

---
+++ 백준/Silver/8641. Sklep/Sklep.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    vector<int> v;
+    map<int, int> m;
+
+    while(N--) {
+        int a, b; cin >> a >> b;
+
+        if(m[a] == 0) v.push_back(a);
+
+        m[a] += b;
+    }
+
+    cout << v.size() << "\n";
+
+    for(int i=0; i<v.size(); i++)
+        cout << v[i] << " " << m[v[i]] << "\n";
+}
