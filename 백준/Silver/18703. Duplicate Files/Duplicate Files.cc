b5c8c7ac6002439655597c46e600fa7e47af6330
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-20 17:17:31 +0900

    [Silver V] Title: Duplicate Files, Time: 1636 ms, Memory: 16112 KB -BaekjoonHub

---
+++ 백준/Silver/18703. Duplicate Files/Duplicate Files.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N; cin >> N;
+
+        map<string, int> m;
+        vector<string> v;
+
+        while(N--) {
+            string str; cin >> str;
+            int x; cin >> x;
+
+            if(m[str] == 0) {
+                m[str] = x;
+
+                v.push_back(str);
+            }
+            else m[str] = min(m[str], x);
+        }
+
+        vector<int> u;
+
+        for(int i=0; i<v.size(); i++) u.push_back(m[v[i]]);
+
+        sort(u.begin(), u.end());
+
+        for(int i=0; i<u.size(); i++) cout << u[i] << " ";
+        cout << "\n";
+    }
+}
