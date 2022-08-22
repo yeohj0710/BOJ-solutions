c7e9cca81012496e4f52030b48f16053f5ca9085
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-22 12:34:30 +0900

    [Silver II] Title: 이진수 더하기, Time: 672 ms, Memory: 8360 KB -BaekjoonHub

---
+++ 백준/Silver/21396. 이진수 더하기/이진수 더하기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N, M; cin >> N >> M;
+
+        map<int, int> m;
+        int ans = 0;
+
+        while(N--) {
+            int x; cin >> x;
+
+            ans += m[x];
+
+            m[x ^ M]++;
+        }
+
+        cout << ans << "\n";
+    }
+}
