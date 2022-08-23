2128e9e0ab53e07be1b077fe947cc81a758e052b
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-23 09:12:18 +0900

    [Bronze II] Title: 문자열 분석, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.cc
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.cc
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     string str;
 
     while(getline(cin, str)) {
+        int a = 0, b = 0, c = 0, d = 0;
 
-        int lower = 0, upper = 0, number = 0, blank = 0;
         for(int i=0; i<str.length(); i++) {
-            if(str[i] >= 'a' && str[i] <= 'z') lower++;
-            else if(str[i] >= 'A' && str[i] <= 'Z') upper++;
-            else if(str[i] >= '0' && str[i] <= '9') number++;
-            else if(str[i] == ' ') blank++;
+            if(str[i] >= 'a' && str[i] <= 'z') a++;
+            else if(str[i] >= 'A' && str[i] <= 'Z') b++;
+            else if(str[i] >= '0' && str[i] <= '9') c++;
+            else if(str[i] == ' ') d++;
         }
 
-        cout << lower << " " << upper << " " << number << " " << blank << "\n";
+        cout << a << " " << b << " " << c << " " << d << "\n";
     }
 }
