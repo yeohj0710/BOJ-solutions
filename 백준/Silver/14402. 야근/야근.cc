16a2b91751d3e4647c612409f81455598658bbc0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-24 16:44:00 +0900

    [Silver IV] Title: 야근, Time: 56 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/14402. 야근/야근.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    map<string, int> m;
+    set<string> s;
+    int ans = 0;
+
+    while(N--) {
+        string str; cin >> str;
+        char c; cin >> c;
+
+        if(c == '+') m[str]++;
+        else {
+            if(m[str] == 0) ans++;
+            else m[str]--;
+        }
+
+        if(s.count(str) == 0) s.insert(str);
+    }
+
+    for(auto i : s) ans += m[i];
+
+    cout << ans << "\n";
+}
