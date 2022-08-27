a4463c74a1f713afa3c05866b97b30b5f7093840
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-27 22:31:09 +0900

    [Gold II] Title: A Bug’s Life, Time: 312 ms, Memory: 34556 KB -BaekjoonHub

---
+++ 백준/Gold/7535. A Bug’s Life/A Bug’s Life.cc
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+vector<vector<int>> adj, scc;
+vector<int> nnum, cnum;
+int ncnt = 0, ccnt = 0;
+
+stack<int> s;
+vector<bool> ch;
+
+int dfs(int x) {
+    nnum[x] = ++ncnt;
+    s.push(x);
+
+    int tmp = nnum[x];
+
+    for(int i=0; i<adj[x].size(); i++) {
+        int y = adj[x][i];
+
+        if(nnum[y] == 0) tmp = min(tmp, dfs(y));
+        else if(!ch[y]) tmp = min(tmp, nnum[y]);
+    }
+
+    if(tmp == nnum[x]) {
+        vector<int> v;
+        ccnt++;
+
+        while(true) {
+            int y = s.top();
+            s.pop();
+
+            v.push_back(y);
+
+            ch[y] = true;
+            cnum[y] = ccnt;
+
+            if(y == x) break;
+        }
+
+        sort(v.begin(), v.end());
+
+        scc.push_back(v);
+    }
+
+    return tmp;
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int T; cin >> T;
+
+    for(int t=1; t<=T; t++) {
+        int N, M; cin >> N >> M;
+
+        adj.clear(); adj.resize(N*2);
+
+        while(M--) {
+            int a, b; cin >> a >> b;
+
+            if(a < 0) a = (-a)*2 - 2;
+            else a = a*2 - 1;
+
+            if(b < 0) b = (-b)*2 - 2;
+            else b = b*2 - 1;
+
+            int na, nb;
+
+            if(a % 2 == 0) na = a + 1;
+            else na = a - 1;
+
+            if(b % 2 == 0) nb = b + 1;
+            else nb = b - 1;
+
+            adj[a].push_back(nb);
+            adj[b].push_back(na);
+
+            adj[na].push_back(b);
+            adj[nb].push_back(a);
+        }
+
+        nnum.clear(); nnum.resize(N*2);
+        cnum.clear(); cnum.resize(N*2);
+        ch.clear();   ch.resize(N*2);
+
+        for(int i=0; i<N*2; i++)
+            if(nnum[i] == 0) dfs(i);
+
+        bool check = true;
+
+        for(int i=0; i<N; i++) {
+            if(cnum[i*2] == cnum[i*2 + 1]) {
+                check = false;
+                break;
+            }
+        }
+
+        cout << "Scenario #" << t << ":\n";
+
+        if(check) cout << "No suspicious bugs found!\n\n";
+        else cout << "Suspicious bugs found!\n\n";
+    }
+}
---
+++ 백준/Gold/7535. A Bug’s Life/README.md
@@ -0,0 +1,26 @@
+# [Gold II] A Bug’s Life - 7535 
+
+[문제 링크](https://www.acmicpc.net/problem/7535) 
+
+### 성능 요약
+
+메모리: 34556 KB, 시간: 312 ms
+
+### 분류
+
+그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
+### 문제 설명
+
+<p>Professor Hopper is researching the sexual behavior of a rare species of bugs. He assumes that they feature two different genders and that they only interact with bugs of the opposite gender. In his experiment, individual bugs and their interactions were easy to identify, because numbers were printed on their backs.</p>
+
+<p>Given a list of bug interactions, decide whether the experiment supports his assumption of two genders with no homosexual bugs or if it contains some bug interactions that falsify it.</p>
+
+### 입력 
+
+ <p>The first line of the input contains the number of scenarios. Each scenario starts with one line giving the number of bugs (at least one, and up to 2000) and the number of interactions (up to 1000000) separated by a single space. In the following lines, each interaction is given in the form of two distinct bug numbers separated by a single space. Bugs are numbered consecutively starting from one.</p>
+
+### 출력 
+
+ <p>The output for every scenario is a line containing “Scenario #i:”, where i is the number of the scenario starting at 1, followed by one line saying either “No suspicious bugs found!” if the experiment is consistent with his assumption about the bugs’ sexual behavior, or ”Suspicious bugs found!” if Professor Hopper’s assumption is definitely wrong.</p>
+
