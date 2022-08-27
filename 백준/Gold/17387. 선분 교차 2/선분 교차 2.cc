d5784a8bfe8a854739306ee71c83fe4c38ab12fb
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-08-28 01:58:41 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct s { int x, y; };
+
+bool operator== (s a, s b) {
+    if(a.x == b.x && a.y == b.y) return true;
+    else return false;
+}
+bool operator>= (s a, s b) {
+    if(a.x > b.x || (a.x == b.x && a.y >= b.y)) return true;
+    else return false;
+}
+bool operator<= (s a, s b) {
+    if(a.x < b.x || (a.x == b.x && a.y <= b.y)) return true;
+    else return false;
+}
+bool operator> (s a, s b) {
+    if(a.x > b.x || (a.x == b.x && a.y > b.y)) return true;
+    else return false;
+}
+bool operator< (s a, s b) {
+    if(a.x < b.x || (a.x == b.x && a.y < b.y)) return true;
+    else return false;
+}
+
+int ccw(s a, s b, s c) {
+    int val = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
+
+    if(val == 0) return 0;
+    else if(val > 0) return 1;
+    else if(val < 0) return -1;
+}
+
+void inter(s a, s b, s c, s d) {
+    int val1 = ccw(a, b, c) * ccw(a, b, d);
+    int val2 = ccw(c, d, a) * ccw(c, d, b);
+
+    if(val1 == 0 && val2 == 0) {
+        if(a > b) swap(a, b);
+        if(c > d) swap(c, d);
+
+        if(a <= d && b >= c) cout << 1 << "\n";
+        else cout << 0 << "\n";
+    }
+    else {
+        if(val1 <= 0 && val2 <= 0) cout << 1 << "\n";
+        else cout << 0 << "\n";
+    }
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    vector<s> p(4);
+    for(int i=0; i<4; i++) cin >> p[i].x >> p[i].y;
+
+    cout << fixed;
+    cout.precision(9);
+
+    inter(p[0], p[1], p[2], p[3]);
+}
