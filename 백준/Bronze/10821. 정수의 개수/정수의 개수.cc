598d773069537dbb2930215f3820ee652fb8ba13
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-09-01 08:58:54 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -1,11 +1,19 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
-    string S; int Count = 0;
-    cin >> S;
-    for(int i=0; i<S.length(); i++)
-        if(S[i] == ',') Count++;
-    cout << Count+1;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+
+    int cnt = 0;
+
+    for(int i=0; i<str.length(); i++)
+        if(str[i] == ',') cnt++;
+
+    int ans = cnt + 1;
+
+    cout << ans << "\n";
 }
