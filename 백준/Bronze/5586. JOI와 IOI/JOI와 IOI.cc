b743389cc7bda6ff0bcc1d7515041646ed513a51
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-09-04 14:59:16 +0900

    [Bronze II] Title: JOI와 IOI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/5586. JOI와 IOI/JOI와 IOI.cc
+++ 백준/Bronze/5586. JOI와 IOI/JOI와 IOI.cc
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
-    string Str;
-    cin >> Str;
-    int JOI = 0, IOI = 0;
-    for(int i=0; i<Str.length()-2; i++) {
-        if(Str[i] == 'J' && Str[i+1] == 'O' && Str[i+2] == 'I') JOI++;
-        if(Str[i] == 'I' && Str[i+1] == 'O' && Str[i+2] == 'I') IOI++;
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string str; cin >> str;
+
+    int a = 0, b = 0;
+
+    for(int i=2; i<str.length(); i++) {
+        if(str[i-2] == 'J' && str[i-1] == 'O' && str[i] == 'I') a++;
+        if(str[i-2] == 'I' && str[i-1] == 'O' && str[i] == 'I') b++;
     }
-    cout << JOI << "\n" << IOI;
+
+    cout << a << "\n" << b << "\n";
 }
