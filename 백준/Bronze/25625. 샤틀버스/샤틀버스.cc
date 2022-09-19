b40f057ef3e5fbf2f7fd9b41d096e0dc159a2ae3
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-09-19 12:44:44 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+
+    int ans = (a + b) % (a * 2);
+
+    cout << ans << "\n";
+}
