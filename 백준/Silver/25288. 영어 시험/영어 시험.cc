0feec2bf98741f6c5a295b804433fa1a8ee9d163
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-09-21 17:44:25 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    string str; cin >> str;
+
+    for(int i=0; i<N; i++) cout << str;
+    cout << "\n";
+}
