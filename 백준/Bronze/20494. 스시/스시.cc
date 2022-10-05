f3144f7f699ffc43e3b3a7aee18342636a1327a4
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-10-05 22:18:50 +0900

    [Bronze II] Title: 스시, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20494. 스시/스시.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int ans = 0;
+
+    while(N--) {
+        string str; cin >> str;
+
+        ans += str.length();
+    }
+
+    cout << ans << "\n";
+}
