57ee9326bc5bd5a403ee2bb69de93c26226ae7ed
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-10-09 21:51:20 +0900

    [Silver I] Title: 나의 행렬곱셈 답사기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12796. 나의 행렬곱셈 답사기/나의 행렬곱셈 답사기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int x; cin >> x;
+
+    cout << 3 << "\n";
+    cout << 1 << " " << 1 << " " << 1 << " " << x+1 << "\n";
+}
