f182022e76c979d599c3038ab3faaafbd344465a
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-10-13 02:38:46 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    for(int i=0; i<N; i++) {
+        int a, b, c; cin >> a >> b >> c;
+    }
+
+    if(N >= 8) cout << "satisfactory\n";
+    else cout << "unsatisfactory\n";
+}
