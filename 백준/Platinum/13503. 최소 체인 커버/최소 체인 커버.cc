4c6b8929ecb9bf84873724d34cc3c116413fbf61
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-10-23 05:58:20 +0900

    [Platinum II] Title: 최소 체인 커버, Time: 64 ms, Memory: 3776 KB -BaekjoonHub

---
+++ 백준/Platinum/13503. 최소 체인 커버/최소 체인 커버.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+vector<vector<int>> adj;
+vector<int> l, r;
+vector<bool> vis;
+
+bool f(int x) {
+    vis[x] = true;
+
+    for(int i=0; i<adj[x].size(); i++) {
+        int y = adj[x][i];
+
+        if(r[y] == -1 || (!vis[r[y]] && f(r[y]))) {
+            l[x] = y, r[y] = x;
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+
+    adj.resize(N+1);
+
+    while(M--) {
+        int x, y; cin >> x >> y;
+
+        adj[x].push_back(y);
+    }
+
+    l.resize(N+1, -1);
+    r.resize(N+1, -1);
+
+    int match = 0;
+
+    for(int i=1; i<=N; i++) {
+        vis.clear();
+        vis.resize(N+1);
+
+        if(f(i)) match++;
+    }
+
+    int ans = N - match;
+
+    cout << ans << "\n";
+}
