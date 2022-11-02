9ab807457dd6d56f871ab1beb6d1d7e3a2e4c0a5
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-11-02 17:37:16 +0900

    [Unrated] Title: 외계인의 침투, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/16490. 외계인의 침투/외계인의 침투.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int a, b; cin >> a >> b;
+
+    cout << a*a - b*b << "\n";
+}
