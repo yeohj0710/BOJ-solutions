8b03540a0e329145c84e42e667d0df49d0f03b80
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-11-04 08:44:58 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    int ans = (int)sqrt(N);
+
+    cout << ans << "\n";
+}
