5509c66feca4005609fcd6775acf00f614c324a1
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-11-27 01:21:02 +0900

    [Platinum III] Title: 지민이의 테러 Season II, Time: 4 ms, Memory: 3768 KB -BaekjoonHub

--- 백준/Platinum/1650. 지민이의 테러 Season II/지민이의 테러 Season II.cc
+++ 백준/Platinum/1650. 지민이의 테러 Season II/지민이의 테러 Season II.cc
@@ -3,73 +3,75 @@
 using namespace std;
 
 struct se {
-    int num, ord, cap, sco;
+    int num, cap, sco, ord;
 
-    se(int num, int ord, int cap, int sco) : num(num), ord(ord), cap(cap), sco(sco) {}
+    se(int num, int cap, int sco, int ord) : num(num), cap(cap), sco(sco), ord(ord) {}
 };
 
 vector<vector<se>> adj;
 int maxf, minc;
 
 void edge(int a, int b, int c, int d) {
-    adj[a].emplace_back(b, adj[b].size(), c, d);
-    adj[b].emplace_back(a, adj[a].size()-1, 0, -d);
+    adj[a].emplace_back(b, c, d, adj[b].size());
+    adj[b].emplace_back(a, 0, -d, adj[a].size()-1);
 }
 
 void mcmf(int sour, int sink) {
-    vector<int> pre(adj.size(), -1), idx(adj.size(), -1);
+    while(true) {
+        vector<int> pre(adj.size(), -1), idx(adj.size(), -1);
 
-    vector<int> tco(adj.size(), INT_MAX);
-    tco[sour] = 0;
+        vector<int> tco(adj.size(), INT_MAX);
+        tco[sour] = 0;
 
-    queue<int> q;
-    q.push(sour);
+        queue<int> q;
+        q.push(sour);
 
-    vector<bool> inq(adj.size());
-    inq[sour] = true;
+        vector<bool> inq(adj.size());
+        inq[sour] = true;
 
-    while(!q.empty()) {
-        int x = q.front();
-        q.pop();
-        inq[x] = false;
+        while(!q.empty()) {
+            int x = q.front();
+            q.pop();
+            inq[x] = false;
 
-        for(int i=0; i<adj[x].size(); i++) {
-            int y = adj[x][i].num;
-            int cap = adj[x][i].cap;
-            int sco = adj[x][i].sco;
+            for(int i=0; i<adj[x].size(); i++) {
+                int y = adj[x][i].num;
+                int cap = adj[x][i].cap;
+                int sco = adj[x][i].sco;
 
-            if(cap <= 0 || tco[x] + sco >= tco[y]) continue;
+                if(cap <= 0 || tco[x] + sco >= tco[y]) continue;
 
-            tco[y] = tco[x] + sco;
-            pre[y] = x;
-            idx[y] = i;
+                tco[y] = tco[x] + sco;
+                pre[y] = x;
+                idx[y] = i;
 
-            if(inq[y]) continue;
+                if(inq[y]) continue;
 
-            q.push(y);
-            inq[y] = true;
+                q.push(y);
+                inq[y] = true;
+            }
         }
+        if(pre[sink] == -1) break;
+
+        int sfl = INT_MAX;
+
+        for(int i=sink; i!=sour; i=pre[i]) {
+            int a = pre[i], b = idx[i];
+
+            sfl = min(sfl, adj[a][b].cap);
+        }
+
+        for(int i=sink; i!=sour; i=pre[i]) {
+            int a = pre[i], b = idx[i];
+
+            adj[a][b].cap -= sfl;
+            adj[i][adj[a][b].ord].cap += sfl;
+
+            minc += sfl * adj[a][b].sco;
+        }
+
+        maxf += sfl;
     }
-    if(pre[sink] == -1) return;
-
-    int sfl = INT_MAX;
-
-    for(int i=sink; i!=sour; i=pre[i]) {
-        int a = pre[i], b = idx[i];
-
-        sfl = min(sfl, adj[a][b].cap);
-    }
-
-    for(int i=sink; i!=sour; i=pre[i]) {
-        int a = pre[i], b = idx[i];
-
-        adj[a][b].cap -= sfl;
-        adj[i][adj[a][b].ord].cap += sfl;
-
-        minc += sfl * adj[a][b].sco;
-    }
-
-    maxf += sfl;
 }
 
 main() {
@@ -79,9 +81,12 @@ main() {
     int N, M; cin >> N >> M;
 
     adj.clear();
-    adj.resize(N*2 + 1);
+    adj.resize(N*2 + 3);
 
-    int sour = 1, sink = N+N;
+    int sour = N*2 + 1, sink = N*2 + 2;
+
+    edge(sour, 1, 2, 0);
+    edge(N+N, sink, 2, 0);
 
     for(int i=1; i<=N; i++) edge(i, N+i, INT_MAX, 0);
 
@@ -89,17 +94,12 @@ main() {
         int a, b, c; cin >> a >> b >> c;
 
         edge(N+a, b, 1, c);
-        edge(N+b, a, 1, c);
+        edge(N+b, a, 1 ,c);
     }
 
-    int ans = 0;
+    maxf = 0, minc = 0;
 
-    for(int i=0; i<2; i++) {
-        maxf = 0, minc = 0;
-        mcmf(sour, sink);
+    mcmf(sour, sink);
 
-        ans += minc;
-    }
-
-    cout << ans << "\n";
+    cout << minc << "\n";
 }
