1db99455e1b70f312ca4a71a014e6fb311f49695
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-11-27 19:40:02 +0900

    [Platinum II] Title: 두부장수 장홍준 2, Time: 4 ms, Memory: 3056 KB -BaekjoonHub

--- 백준/Platinum/11111. 두부장수 장홍준 2/두부장수 장홍준 2.cc
+++ 백준/Platinum/11111. 두부장수 장홍준 2/두부장수 장홍준 2.cc
@@ -2,25 +2,50 @@
 #define int long long
 using namespace std;
 
-struct se {
-    int num, cap, sco, ord;
+struct sm {
+	// referred to the jhnah917's implementation (purpose of study)
 
-    se(int num, int cap, int sco, int ord) : num(num), cap(cap), sco(sco), ord(ord) {}
-};
+    struct se { int num, cap, sco, ord; };
+    vector<vector<se>> adj;
 
-vector<vector<se>> adj;
-int maxf, minc;
+    void edge(int a, int b, int c, int d) {
+        adj[a].push_back({b, c, d, adj[b].size()});
+        adj[b].push_back({a, 0, -d, adj[a].size()-1});
+    }
 
-void edge(int a, int b, int c, int d) {
-    adj[a].emplace_back(b, c, d, adj[b].size());
-    adj[b].emplace_back(a, 0, -d, adj[a].size()-1);
-}
+    int sour, sink;
+    vector<int> tco, idx;
+    vector<bool> chk;
 
-void mcmf(int sour, int sink) {
-    while(true) {
-        vector<int> pre(adj.size(), -1), idx(adj.size(), -1);
+    int dfs(int x, int flo) {
+        chk[x] = true;
 
-        vector<int> tco(adj.size(), INT_MAX);
+        if(x == sink) return flo;
+
+        for(; idx[x] < adj[x].size(); idx[x]++) {
+            se &ad = adj[x][idx[x]];
+
+            if(chk[ad.num] || tco[x] + ad.sco != tco[ad.num] || ad.cap == 0) continue;
+
+            int ret = dfs(ad.num, min(flo, ad.cap));
+
+            if(ret == 0) continue;
+
+            ad.cap -= ret;
+            adj[ad.num][ad.ord].cap += ret;
+
+            return ret;
+        }
+
+        return 0;
+    }
+
+    pair<int, int> mcmf(int sour_, int sink_) {
+        sour = sour_, sink = sink_;
+
+        int minc = 0, maxf = 0;
+
+        tco.resize(adj.size(), INT_MAX);
         tco[sour] = 0;
 
         queue<int> q;
@@ -31,8 +56,7 @@ void mcmf(int sour, int sink) {
 
         while(!q.empty()) {
             int x = q.front();
-            q.pop();
-            inq[x] = false;
+            q.pop(); inq[x] = false;
 
             for(int i=0; i<adj[x].size(); i++) {
                 int y = adj[x][i].num;
@@ -42,8 +66,6 @@ void mcmf(int sour, int sink) {
                 if(cap <= 0 || tco[x] + sco >= tco[y]) continue;
 
                 tco[y] = tco[x] + sco;
-                pre[y] = x;
-                idx[y] = i;
 
                 if(inq[y]) continue;
 
@@ -51,28 +73,44 @@ void mcmf(int sour, int sink) {
                 inq[y] = true;
             }
         }
-        if(pre[sink] == -1) break;
 
-        int sfl = INT_MAX;
+        while(true) {
+            chk.clear(); chk.resize(adj.size());
+            idx.clear(); idx.resize(adj.size());
 
-        for(int i=sink; i!=sour; i=pre[i]) {
-            int a = pre[i], b = idx[i];
+            while(true) {
+                int cur = dfs(sour, INT_MAX);
+                if(cur == 0) break;
 
-            sfl = min(sfl, adj[a][b].cap);
+                minc += tco[sink] * cur;
+                maxf += cur;
+
+                chk.clear(); chk.resize(adj.size());
+            }
+
+            int Min = INT_MAX;
+
+            for(int i=0; i<adj.size(); i++) {
+                if(!chk[i]) continue;
+
+                for(int j=0; j<adj[i].size(); j++) {
+                    int x = adj[i][j].num;
+                    int cap = adj[i][j].cap;
+                    int sco = adj[i][j].sco;
+
+                    if(cap > 0 && !chk[x]) Min = min(Min, tco[i] + sco - tco[x]);
+                }
+            }
+
+            if(Min == INT_MAX) break;
+
+            for(int i=0; i<adj.size(); i++)
+                if(!chk[i]) tco[i] += Min;
         }
 
-        for(int i=sink; i!=sour; i=pre[i]) {
-            int a = pre[i], b = idx[i];
-
-            adj[a][b].cap -= sfl;
-            adj[i][adj[a][b].ord].cap += sfl;
-
-            minc += sfl * adj[a][b].sco;
-        }
-
-        maxf += sfl;
+        return {minc, maxf};
     }
-}
+};
 
 main() {
     ios_base::sync_with_stdio(false);
@@ -85,8 +123,9 @@ main() {
     for(int i=1; i<=N; i++)
         for(int j=1; j<=M; j++) cin >> v[i][j];
 
-    adj.clear();
-    adj.resize(N*M + 3);
+    sm f;
+
+    f.adj.resize(N*M + 3);
 
     int sour = N*M + 1, sink = N*M + 2;
 
@@ -100,8 +139,8 @@ main() {
     for(int i=1; i<=N; i++)
         for(int j=1; j<=M; j++) {
             if((i + j) % 2 == 0) {
-                edge(sour, (i-1)*M + j, 1, 0);
-                edge((i-1)*M + j, sink, 1, 0);
+                f.edge(sour, (i-1)*M + j, 1, 0);
+                f.edge((i-1)*M + j, sink, 1, 0);
 
                 int di[4] = {1, -1, 0, 0};
                 int dj[4] = {0, 0, 1, -1};
@@ -112,14 +151,17 @@ main() {
 
                     if(ni < 1 || nj < 1 || ni > N || nj > M) continue;
 
-                    edge((i-1)*M + j, (ni-1)*M + nj, 1, -cvt[v[i][j]-'A'][v[ni][nj]-'A']);
+                    f.edge((i-1)*M + j, (ni-1)*M + nj, 1, -cvt[v[i][j]-'A'][v[ni][nj]-'A']);
                 }
             }
-            else edge((i-1)*M + j, sink, 1, 0);
+            else f.edge((i-1)*M + j, sink, 1, 0);
         }
 
-    maxf = 0, minc = 0;
-    mcmf(sour, sink);
+
+    pair<int, int> mcmf = f.mcmf(sour, sink);
+
+    int minc = mcmf.first;
+    int maxf = mcmf.second;
 
     cout << -minc << "\n";
 }
