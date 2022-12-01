dfde375b6943dbf502d42dec9f5ce77ef83dbd4d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-01 17:43:12 +0900

    [Silver V] Title: Array Manipulation at Moloco (Easy), Time: 20 ms, Memory: 33416 KB -BaekjoonHub

---
+++ 백준/Silver/15516. Array Manipulation at Moloco （Easy）/Array Manipulation at Moloco （Easy）.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct segmentTree {
+    vector<int> v;
+
+    void upd(int n, int b, int e, int idx, int val) {
+        if(b == e) {
+            v[n] += val;
+            return;
+        }
+
+        if(idx <= (b+e)/2) upd(n*2, b, (b+e)/2, idx, val);
+        else upd(n*2 + 1, (b+e)/2 + 1, e, idx, val);
+
+        v[n] = v[n*2] + v[n*2 + 1];
+    }
+
+    int sum(int n, int b, int e, int l, int r) {
+        if(r < b || e < l) return 0;
+        if(l <= b && e <= r) return v[n];
+
+        return sum(n*2, b, (b+e)/2, l, r)
+                + sum(n*2 + 1, (b+e)/2 + 1, e, l, r);
+    }
+};
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int N; cin >> N;
+
+    segmentTree f; f.v.resize(1e6 * 4);
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    vector<int> u(v);
+
+    sort(u.begin(), u.end());
+    u.erase(unique(u.begin(), u.end()), u.end());
+
+    vector<int> w(N);
+    for(int i=0; i<v.size(); i++)
+        w[i] = lower_bound(u.begin(), u.end(), v[i]) - u.begin();
+
+    for(int i=0; i<N; i++) {
+        cout << f.sum(1, 0, 1e6, 0, w[i]-1) << "\n";
+
+        f.upd(1, 0, 1e6, w[i], 1);
+    }
+}
