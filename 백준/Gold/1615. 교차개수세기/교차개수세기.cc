4df681e00f3bbbccf4663af4e7d74988b1da03d7
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-02 22:48:58 +0900

    [Gold I] Title: 교차개수세기, Time: 752 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Gold/1615. 교차개수세기/교차개수세기.cc
+++ 백준/Gold/1615. 교차개수세기/교차개수세기.cc
@@ -2,30 +2,29 @@
 #define int long long
 using namespace std;
 
-vector<pair<int, int>> v;
-vector<int> u;
+struct segmentTree {
+    vector<int> v;
 
-void upd(int n, int b, int e, int idx) {
-    if(b == e) {
-        u[n] += 1;
-        return;
+    void upd(int n, int b, int e, int idx, int val) {
+        if(b == e) {
+            v[n] += val;
+            return;
+        }
+
+        if(idx <= (b+e)/2) upd(n*2, b, (b+e)/2, idx, val);
+        else upd(n*2 + 1, (b+e)/2 + 1, e, idx, val);
+
+        v[n] = v[n*2] + v[n*2 + 1];
     }
 
-    if(idx <= (b+e)/2) upd(n*2, b, (b+e)/2, idx);
-    else upd(n*2 + 1, (b+e)/2 + 1, e, idx);
+    int sum(int n, int b, int e, int l, int r) {
+        if(r < b || e < l) return 0;
+        if(l <= b && e <= r) return v[n];
 
-    u[n] = u[n*2] + u[n*2 + 1];
-}
-
-int cnt(int n, int b, int e, int l, int r) {
-    if(r < b || e < l) return 0;
-    if(l <= b && e <= r) return u[n];
-
-    int lv = cnt(n*2, b, (b+e)/2, l, r);
-    int rv = cnt(n*2 + 1, (b+e)/2 + 1, e, l, r);
-
-    return lv + rv;
-}
+        return sum(n*2, b, (b+e)/2, l, r)
+                + sum(n*2 + 1, (b+e)/2 + 1, e, l, r);
+    }
+};
 
 main() {
     ios_base::sync_with_stdio(false);
@@ -33,18 +32,20 @@ main() {
 
     int N, M; cin >> N >> M;
 
-    v.resize(M);
+    vector<pair<int, int>> v(M);
 
-    for(int i=0; i<M; i++) cin >> v[i].first >> v[i].second;
+    for(int i=0; i<M; i++)
+        cin >> v[i].first >> v[i].second;
 
     sort(v.begin(), v.end());
 
-    u.resize(N*4);
+    segmentTree f; f.v.resize(N*4);
 
     int ans = 0;
+
     for(int i=0; i<M; i++) {
-        ans += cnt(1, 1, N, v[i].second + 1, N);
-        upd(1, 1, N, v[i].second);
+        ans += f.sum(1, 1, N, v[i].second+1, N);
+        f.upd(1, 1, N, v[i].second, 1);
     }
 
     cout << ans << "\n";
