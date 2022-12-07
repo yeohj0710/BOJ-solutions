e1ed4e695dea2aaf69bf7927d7ceb05b7a319e66
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-07 09:01:50 +0900

    [Bronze II] Title: Cup Covering, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/26173. Cup Covering/Cup Covering.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cout << fixed;
+    cout.precision(9);
+
+    double x; cin >> x;
+
+    cout << sqrt(x / M_PI) * 2 << "\n";
+}
