4a8a810da4b52140323755813d1dba5d45f7f77f
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-09 16:11:59 +0900

    [Gold III] Title: 이진 트리, Time: 176 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Gold/13325. 이진 트리/이진 트리.cc
+++ 백준/Gold/13325. 이진 트리/이진 트리.cc
@@ -5,7 +5,7 @@ using namespace std;
 vector<int> v;
 
 int f(int n) {
-    if(n >= v.size()/2) return 0;
+    if(n*2 >= v.size()) return 0;
 
     int lv = v[n*2] + f(n*2);
     int rv = v[n*2 + 1] + f(n*2 + 1);
@@ -22,13 +22,13 @@ main() {
 
     int N; cin >> N;
 
-    v.resize(1 << (N+2));
-    for(int i=2; i<v.size()/2; i++) cin >> v[i];
+    v.resize(1 << (N+1));
+    for(int i=2; i<v.size(); i++) cin >> v[i];
 
     f(1);
 
     int ans = 0;
-    for(int i=0; i<v.size()/2; i++) ans += v[i];
+    for(int i=0; i<v.size(); i++) ans += v[i];
 
     cout << ans << "\n";
 }
