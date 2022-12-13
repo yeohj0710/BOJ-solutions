2fed5c13b5850ae434a9bf2971fbca70010ead78
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-13 16:37:00 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int a, b; cin >> a >> b;
+
+    cout << (a - 9) * 60 + b << "\n";
+}
