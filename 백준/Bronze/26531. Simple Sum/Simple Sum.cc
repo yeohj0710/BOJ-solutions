34cda575a0d7deade0aeaa1e720d51932c0f1e13
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-18 00:04:50 +0900

    [Bronze IV] Title: Simple Sum, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int a, b, c; char x, y;
+    cin >> a >> x >> b >> y >> c;
+
+    if(a + b == c) cout << "YES\n";
+    else cout << "NO\n";
+}
