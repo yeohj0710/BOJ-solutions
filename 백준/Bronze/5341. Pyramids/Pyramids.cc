903de9ff69ee214e676c8c1b3a6662f6584168ee
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-17 14:03:44 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    while(true) {
+        int N; cin >> N;
+        if(N == 0) break;
+
+        cout << N * (N+1) / 2 << "\n";
+    }
+}
