0ec0d855ff73edba0e5d145141ac1338f030ff29
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-17 10:28:05 +0900

    [Bronze IV] Title: 0123456789, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.cc
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int N; cin >> N;
+
+    if(N == 0) {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if(N == 1) {
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if(N == 2) {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+    }
+    else if(N == 3) {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if(N == 4) {
+        cout << "\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if(N == 5) {
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if(N == 6) {
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if(N == 7) {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "\n";
+    }
+    else if(N == 8) {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if(N == 9) {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+}
