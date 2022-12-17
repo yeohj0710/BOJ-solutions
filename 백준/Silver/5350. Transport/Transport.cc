22b18ecdb5f3b4cf4c130cee54615006d945feb0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-17 15:15:49 +0900

    [Silver II] Title: Transport, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5350. Transport/Transport.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int N, M; cin >> N >> M;
+
+        vector<pair<int, int>> v(N+1); // <weight, value>
+
+        for(int i=1; i<=N; i++)
+            cin >> v[i].first >> v[i].second;
+
+        vector<vector<int>> dp(N+1, vector<int>(M+1));
+
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=M; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(v[i].first <= j)
+                    dp[i][j] = max(dp[i][j], dp[i-1][j - v[i].first] + v[i].second);
+            }
+
+        cout << dp[N][M] << "\n";
+    }
+}
