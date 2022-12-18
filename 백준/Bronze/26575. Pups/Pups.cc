b8e9596830f0f779776a28bbdcfbd2b5c9369736
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-18 18:38:45 +0900

    [Bronze V] Title: Pups, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    cout << fixed << setprecision(2);
+
+    int T; cin >> T;
+
+    while(T--) {
+        double a, b, c; cin >> a >> b >> c;
+
+        cout << "$" << a * b * c << "\n";
+    }
+}
