dd55c1b90f008c4e2ebd8aab253a7a99d3429cb2
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-18 18:37:37 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int T; cin >> T;
+
+    while(T--) {
+        int x; cin >> x;
+
+        cout << x << " " << x << "\n";
+    }
+}
