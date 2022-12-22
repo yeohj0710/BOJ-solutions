f3e0714cfac6bf3ecab18e4b46104c917627fd8c
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-22 12:13:21 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -1,18 +1,14 @@
-#include <bits/stdc++.h>
-#define int long long
-using namespace std;
+#include<stdio.h>
+#include<string.h>
 
-main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
-    string str; cin >> str;
-
-    vector<int> pos(26, -1);
-
-    for(int i=0; i<str.length(); i++)
-        if(pos[str[i] - 'a'] == -1) pos[str[i] - 'a'] = i;
-
-    for(int i=0; i<26; i++) cout << pos[i] << " ";
-    cout << "\n";
+int main() {
+    char word[105], spot[26], check[26] = {0, };
+    for(int i=0; i<26; i++) spot[i] = -1;
+    scanf("%s", word);
+    for(int i=0; i<strlen(word); i++)
+        if(!check[word[i]-'a']) {
+            spot[word[i]-'a'] = i;
+            check[word[i]-'a'] = 1;
+        }
+    for(int i=0; i<26; i++) printf("%d ", spot[i]);
 }
