dbc879094ed1a82d1ba23064f3d591e60d5fa241
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-23 10:47:10 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string sum(string a, string b) {
+    int diff = max(a.length(), b.length()) - min(a.length(), b.length());
+
+    if(a.length() < b.length())
+        for(int i=0; i<diff; i++) a = "0" + a;
+    else if(a.length() > b.length())
+        for(int i=0; i<diff; i++) b = "0" + b;
+
+    vector<int> c;
+    for(int i=0; i<a.length(); i++) c.push_back(a[i] - '0' + b[i] - '0');
+
+    reverse(c.begin(), c.end());
+
+    for(int i=0; i<c.size(); i++) {
+        if(c[i] < 10) continue;
+
+        if(i < c.size()-1) c[i+1] += c[i]/10;
+        else c.push_back(c[i]/10);
+
+        c[i] %= 10;
+    }
+
+    reverse(c.begin(), c.end());
+
+    string ret;
+
+    int i = 0; while(c[i] == 0) i++;
+    if(i >= c.size()) ret.push_back('0');
+
+    while(i < c.size()) {
+        ret.push_back(char(c[i] + '0'));
+        i++;
+    }
+
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string a, b; cin >> a >> b;
+
+    cout << sum(a, b);
+}
