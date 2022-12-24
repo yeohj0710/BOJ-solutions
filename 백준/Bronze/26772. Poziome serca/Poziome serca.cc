2fc61b5e7b32463041589bdff7db45c5e3a93803
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-24 22:35:03 +0900

    [Bronze IV] Title: Poziome serca, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int N; cin >> N;
+
+    for(int i=0; i<N; i++) cout << " @@@   @@@  ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "@   @ @   @ ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "@    @    @ ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "@         @ ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << " @       @  ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "  @     @   ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "   @   @    ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "    @ @     ";
+    cout << "\n";
+
+    for(int i=0; i<N; i++) cout << "     @      ";
+    cout << "\n";
+}
