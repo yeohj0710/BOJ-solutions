ab2b4745577677a6457c5749e6ec87f88187a89d
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-12-24 22:25:10 +0900

    [Bronze V] Title: Serca, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int N; cin >> N;
+
+    while(N--) {
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+}
