6589cb0cd7da13d52abd6e54f54d8ed539cc9213
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2023-01-04 17:17:30 +0900

    [Diamond V] Title: 프로그래밍 대결, Time: 292 ms, Memory: 6504 KB -BaekjoonHub

---
+++ 백준/Diamond/15504. 프로그래밍 대결/프로그래밍 대결.cc
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+struct MCMF {
+    struct se { int num, cap, sco, ord; };
+    vector<vector<se>> adj;
+
+    void edge(int a, int b, int c, int d) {
+        adj[a].push_back({b, c, d, adj[b].size()});
+        adj[b].push_back({a, 0, -d, adj[a].size()-1});
+    }
+
+    int mcmf(int sour, int sink, int amount) {
+        int maxf = 0, minc = 0;
+
+        while(amount--) {
+            vector<int> pre(adj.size(), -1), idx(adj.size(), -1);
+
+            vector<int> tco(adj.size(), INT_MAX);
+            tco[sour] = 0;
+
+            queue<int> q;
+            q.push(sour);
+
+            vector<bool> inq(adj.size());
+            inq[sour] = true;
+
+            while(!q.empty()) {
+                int x = q.front();
+                q.pop();
+                inq[x] = false;
+
+                for(int i=0; i<adj[x].size(); i++) {
+                    int y = adj[x][i].num;
+                    int cap = adj[x][i].cap;
+                    int sco = adj[x][i].sco;
+
+                    if(cap <= 0 || tco[x] + sco >= tco[y]) continue;
+
+                    tco[y] = tco[x] + sco;
+                    pre[y] = x;
+                    idx[y] = i;
+
+                    if(inq[y]) continue;
+
+                    q.push(y);
+                    inq[y] = true;
+                }
+            }
+            if(pre[sink] == -1) break;
+
+            int sfl = INT_MAX;
+
+            for(int i=sink; i!=sour; i=pre[i]) {
+                int a = pre[i], b = idx[i];
+
+                sfl = min(sfl, adj[a][b].cap);
+            }
+
+            for(int i=sink; i!=sour; i=pre[i]) {
+                int a = pre[i], b = idx[i];
+
+                adj[a][b].cap -= sfl;
+                adj[i][adj[a][b].ord].cap += sfl;
+
+                minc += sfl * adj[a][b].sco;
+            }
+
+            maxf += sfl;
+        }
+
+        return minc;
+    }
+};
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    MCMF f; f.adj.resize(N*2 + 3);
+
+    int sour = N*2 + 1, sink = N*2 + 2;
+
+    struct s { int a, b, c; }; vector<s> v(N+1);
+
+    int mx = 0;
+
+    for(int i=1; i<=N; i++) cin >> v[i].a, mx = max(mx, v[i].a);
+    for(int i=1; i<=N; i++) cin >> v[i].b;
+    for(int i=1; i<=N; i++) cin >> v[i].c;
+
+    vector<bool> isMax(N+1);
+
+    for(int i=1; i<=N; i++)
+        if(v[i].a == mx) isMax[i] = true;
+
+    for(int i=1; i<=N; i++) {
+        if(isMax[i]) f.edge(sour, i, v[i].c, v[i].b);
+        else {
+            f.edge(sour, i, v[i].c - 1, v[i].b);
+            f.edge(N+i, sink, 1, v[i].b);
+        }
+
+        for(int j=1; j<=N; j++)
+            if(v[i].a > v[j].a) f.edge(i, N+j, 1, -(v[i].a ^ v[j].a));
+    }
+
+    cout << -f.mcmf(sour, sink, N-1) << "\n";
+}
