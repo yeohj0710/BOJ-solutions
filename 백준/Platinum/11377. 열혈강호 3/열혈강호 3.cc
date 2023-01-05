d24a07216900771c519cdb383d184589b8b3b875
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2023-01-05 23:24:10 +0900

    [Platinum III] Title: 열혈강호 3, Time: 68 ms, Memory: 27808 KB -BaekjoonHub

--- 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
@@ -2,107 +2,105 @@
 #define int long long
 using namespace std;
 
-struct MF {
-    struct se { int num, cap, ord; };
-    vector<vector<se>> adj;
+struct se { int num, cap, ord; };
+vector<vector<se>> adj;
 
-    void edge(int a, int b, int c) {
-        adj[a].push_back({b, c, adj[b].size()});
-        adj[b].push_back({a, 0, adj[a].size()-1});
-    }
+void edge(int a, int b, int c) {
+    adj[a].push_back({b, c, adj[b].size()});
+    adj[b].push_back({a, 0, adj[a].size()-1});
+}
 
-    int sz, sour, sink;
-    vector<int> lv, idx;
+int sz, sour, sink;
+vector<int> lv, idx;
 
-    void init(int sz_) { sz = sz_; adj.resize(sz); }
+void init(int sz_) { sz = sz_; adj.resize(sz); }
 
-    bool bfs() {
-        lv.clear(); lv.resize(sz, -1); lv[sour] = 0;
+bool bfs() {
+    lv.clear(); lv.resize(sz, -1); lv[sour] = 0;
 
-        queue<int> q; q.push(sour);
+    queue<int> q; q.push(sour);
 
-        while(!q.empty()) {
-            int x = q.front(); q.pop();
+    while(!q.empty()) {
+        int x = q.front(); q.pop();
 
-            for(auto e : adj[x]) {
-                int y = e.num, cap = e.cap;
+        for(auto e : adj[x]) {
+            int y = e.num, cap = e.cap;
 
-                if(lv[y] != -1 || cap == 0) continue;
+            if(lv[y] != -1 || cap == 0) continue;
 
-                lv[y] = lv[x] + 1;
-                q.push(y);
-            }
+            lv[y] = lv[x] + 1;
+            q.push(y);
         }
-
-        if(lv[sink] != -1) return true;
-        else return false;
     }
 
-    int dfs(int x, int flo) {
-        if(x == sink) return flo;
+    if(lv[sink] != -1) return true;
+    else return false;
+}
 
-        for(int &i=idx[x]; i<adj[x].size(); i++) {
-            int y = adj[x][i].num, cap = adj[x][i].cap;
+int dfs(int x, int flo) {
+    if(x == sink) return flo;
 
-            if(lv[x] + 1 != lv[y] || cap == 0) continue;
+    for(int &i=idx[x]; i<adj[x].size(); i++) {
+        int y = adj[x][i].num, cap = adj[x][i].cap;
 
-            int sfl = dfs(y, min(flo, cap));
+        if(lv[x] + 1 != lv[y] || cap == 0) continue;
 
-            if(sfl == 0) continue;
+        int sfl = dfs(y, min(flo, cap));
 
-            adj[x][i].cap -= sfl;
-            adj[y][adj[x][i].ord].cap += sfl;
+        if(sfl == 0) continue;
 
-            return sfl;
+        adj[x][i].cap -= sfl;
+        adj[y][adj[x][i].ord].cap += sfl;
+
+        return sfl;
+    }
+
+    return 0;
+}
+
+int mf(int sour_, int sink_) {
+    int maxf = 0; sour = sour_, sink = sink_;
+
+    while(bfs()) {
+        idx.clear(); idx.resize(sz);
+
+        while(true) {
+            int sfl = dfs(sour, INT_MAX);
+
+            if(sfl == 0) break;
+
+            maxf += sfl;
         }
-
-        return 0;
     }
 
-    int mf(int sour_, int sink_) {
-        int maxf = 0; sour = sour_, sink = sink_;
-
-        while(bfs()) {
-            idx.clear(); idx.resize(sz);
-
-            while(true) {
-                int sfl = dfs(sour, INT_MAX);
-
-                if(sfl == 0) break;
-
-                maxf += sfl;
-            }
-        }
-
-        return maxf;
-    }
-};
+    return maxf;
+}
 
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int N, M, K; cin >> N >> M >> K;
 
-    MF f; f.init(N+M+4);
+    init(N+M+4);
 
     int sour = N+M+1, sink = N+M+2, ext = N+M+3;
 
-    f.edge(sour, ext, K);
+    edge(sour, ext, K);
 
     for(int i=1; i<=N; i++) {
-        f.edge(sour, i, 1);
-        f.edge(ext, i, 1);
+        edge(sour, i, 1);
+        edge(ext, i, 1);
 
         int L; cin >> L;
 
         while(L--) {
             int x; cin >> x;
 
-            f.edge(i, N+x, 1);
+            edge(i, N+x, 1);
         }
     }
 
-    for(int i=1; i<=M; i++) f.edge(N+i, sink, 1);
+    for(int i=1; i<=M; i++) edge(N+i, sink, 1);
 
-    cout << f.mf(sour, sink) << "\n";
+    cout << mf(sour, sink) << "\n";
 }
