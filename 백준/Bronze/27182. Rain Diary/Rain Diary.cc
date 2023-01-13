0ddf7693daa4b828c87054fae17e5eae7d6eaafb
yeohj07 <93759367+yeohj07@users.noreply.github.com>
2023-01-13 22:58:56 +0900

    [Bronze IV] Title: Rain Diary, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b; cin >> a >> b;
+
+    if(a - 7 >= 1) cout << a - 7 << "\n";
+    else cout << b + 7 << "\n";
+}
