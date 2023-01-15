6002852e95e7d617c29cb9316a16324a8cd36980
yeohj07 <93759367+yeohj07@users.noreply.github.com>
2023-01-16 01:05:12 +0900

    [Bronze III] Title: Пары, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27272. Пары/Пары.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    vector<int> v(4);
+    for(int i=0; i<4; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    cout << v[0] * v[1] + v[2] * v[3] << "\n";
+}
