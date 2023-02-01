f65011eb0b2140c58eb15674876de97e62699591
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2023-02-01 11:13:50 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string str; cin >> str;
+
+    if(str[0] == str[1]) cout << 1 << "\n";
+    else cout << 0 << "\n";
+}
