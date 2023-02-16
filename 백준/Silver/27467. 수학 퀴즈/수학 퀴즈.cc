b6a488b766575956c1e19e99aea0b385f7f1f16c
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2023-02-16 10:39:30 +0900

    [Silver I] Title: 수학 퀴즈, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/27467. 수학 퀴즈/수학 퀴즈.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    vector<int> v(3);
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        v[x % 3]++;
+    }
+
+    v[0] -= v[2], v[1] -= v[2];
+
+    cout << v[1] << " " << v[0] << "\n";
+}
