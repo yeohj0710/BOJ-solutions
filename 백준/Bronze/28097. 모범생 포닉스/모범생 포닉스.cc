68ac1499737a68927c815167054130a7f5aee483
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-05-23 01:32:03 +0900

    [Bronze IV] Title: 모범생 포닉스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28097. 모범생 포닉스/모범생 포닉스.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    int sum = 0;
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        sum += x;
+    }
+
+    sum += (N - 1) * 8;
+
+    cout << sum / 24 << " " << sum % 24 << "\n";
+}
