21838139b834df7eac270ef7eb47b70b57a17d65
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-08-15 22:07:53 +0900

    [Bronze IV] Title: Счастье Мистера Бина, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29163. Счастье Мистера Бина/Счастье Мистера Бина.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    int cnt = 0;
+
+    for(int i=0; i<N; i++) {
+        int x; cin >> x;
+
+        if(x % 2 == 0) cnt++;
+    }
+
+    if(cnt > N - cnt) cout << "Happy\n";
+    else cout << "Sad\n";
+}
