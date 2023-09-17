e0c05a74c0d91746aa6753ac48bb55f36c99a236
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-09-17 18:22:51 +0900

    [Bronze III] Title: 최장 스트릭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29752. 최장 스트릭/최장 스트릭.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    int ans = 0, cnt = 0;
+
+    for(int i=0; i<N; i++) {
+        if(v[i] > 0) cnt++;
+        else {
+            ans = max(ans, cnt);
+            cnt = 0;
+        }
+    }
+    ans = max(ans, cnt);
+
+    cout << ans << "\n";
+}
