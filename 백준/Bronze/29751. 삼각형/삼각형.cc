7819508d77d4818f37735755e5d040cb6b2b7f79
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-09-17 16:47:41 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    double a, b; cin >> a >> b;
+
+    cout << fixed << setprecision(1);
+
+    cout << a * b / 2 << "\n";
+}
