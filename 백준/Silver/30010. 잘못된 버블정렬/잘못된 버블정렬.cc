db0bc6c676dcb1198c9442d4b606741fc53dad06
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-09-23 22:30:25 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    for(int i=3; i<=N; i++)
+        cout << i << " ";
+
+    cout << 1 << " " << 2 << " \n";
+}
+
