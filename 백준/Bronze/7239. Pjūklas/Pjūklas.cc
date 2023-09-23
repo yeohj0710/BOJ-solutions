7c9a2da1f95e92ee7b04743e5c076ff6efb916b9
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-09-23 11:49:14 +0900

    [Bronze II] Title: Pjūklas, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7239. Pjūklas/Pjūklas.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int i = (N - 1) / 2, j = (N - 1) / 2 + 1, cnt = 0;
+
+    while(true) {
+        if(cnt % 2 == 0) {
+            if(i < 0) break;
+
+            cout << v[i] << " ";
+            i--;
+        }
+        else {
+            if(j >= N) break;
+
+            cout << v[j] << " ";
+            j++;
+        }
+        cnt++;
+    }
+    cout << "\n";
+}
+
