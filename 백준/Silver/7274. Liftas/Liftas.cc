a2e8af3354fa8d50154885af700066f22172958c
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-09-28 16:10:24 +0900

    [Silver V] Title: Liftas, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/7274. Liftas/Liftas.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N, M; cin >> N >> M;
+
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int ans = 0;
+
+    for(int i=0; i<N-M; i++) ans += v[i];
+
+    cout << ans << "\n";
+}
