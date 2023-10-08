8515b87e2af70be8f7eca1f1f16855b488964608
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-10-09 00:19:25 +0900

    [Bronze IV] Title: Advance to Taoyuan Regional, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30319. Advance to Taoyuan Regional/Advance to Taoyuan Regional.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string str; cin >> str;
+
+    if(str <= "2023-09-16") cout << "GOOD\n";
+    else cout << "TOO LATE\n";
+}
