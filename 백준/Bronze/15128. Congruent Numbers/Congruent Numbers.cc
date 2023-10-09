47478c2ebaae4d831a3590d3247b01397b63ce7c
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-10-09 16:14:40 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b, c, d; cin >> a >> b >> c >> d;
+
+    if((a * c) % (2 * b * d) == 0) cout << 1 << "\n";
+    else cout << 0 << "\n";
+}
