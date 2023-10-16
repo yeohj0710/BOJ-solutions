c33ced51a0e8a7c6bd084e2beed95a91d0175a31
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-10-16 22:36:57 +0900

    [Bronze III] Title: Roller Coaster, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30366. Roller Coaster/Roller Coaster.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N, M; cin >> N >> M;
+
+    int ans = 0, cur = 0;
+
+    while(N--) {
+        int x; cin >> x;
+
+        if(cur + x <= M) {
+            cur += x;
+            cout << ans << "\n";
+        }
+        else {
+            cur = x;
+            ans++;
+            cout << ans << "\n";
+        }
+    }
+}
