d4a97b8f99bcdc693040f8b258cfb1bf883d98ab
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-11-07 01:52:11 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    if(N % 2 == 1) cout << "Goose\n";
+    else cout << "Duck\n";
+}
