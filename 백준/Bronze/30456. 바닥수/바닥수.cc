a37a1e9df77e2aadef83c8f0691d844d32a1fdcf
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-11-07 01:50:33 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N, M; cin >> N >> M;
+
+    for(int i=0; i<M-1; i++) cout << 1;
+
+    cout << N << "\n";
+}
