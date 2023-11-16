0da5a0e2a87745efd7becc8e366d432b1ce2d1da
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-11-16 18:11:36 +0900

    [Gold V] Title: Divisibility Trick, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/30599. Divisibility Trick/Divisibility Trick.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    for(int i=0; i<N; i++) cout << N;
+    cout << "\n";
+}
