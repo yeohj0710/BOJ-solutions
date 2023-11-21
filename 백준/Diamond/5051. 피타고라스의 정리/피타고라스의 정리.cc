90586c3eaa84874102f9b98e78f7a8d858f23491
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-11-21 12:17:46 +0900

    [Diamond V] Title: 피타고라스의 정리, Time: 280 ms, Memory: 46684 KB -BaekjoonHub

---
+++ 백준/Diamond/5051. 피타고라스의 정리/피타고라스의 정리.cc
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const double PI = acos(-1);
+typedef complex<double> cpx;
+
+void FFT(vector<cpx> &v, bool inv) {
+    int S = v.size();
+
+    for(int i=1, j=0; i<S; i++) {
+        int bit = S/2;
+
+        while(j >= bit) {
+            j -= bit;
+            bit /= 2;
+        }
+        j += bit;
+
+        if(i < j) swap(v[i], v[j]);
+    }
+
+    for(int k=1; k<S; k*=2) {
+        double angle = (inv ? PI/k : -PI/k);
+        cpx w(cos(angle), sin(angle));
+
+        for(int i=0; i<S; i+=k*2) {
+            cpx z(1, 0);
+
+            for(int j=0; j<k; j++) {
+                cpx even = v[i+j];
+                cpx odd = v[i+j+k];
+
+                v[i+j] = even + z*odd;
+                v[i+j+k] = even - z*odd;
+
+                z *= w;
+            }
+        }
+    }
+
+    if(inv)
+        for(int i=0; i<S; i++) v[i] /= S;
+}
+
+vector<int> mul(vector<int> &v, vector<int> &u) {
+    vector<cpx> vc(v.begin(), v.end());
+    vector<cpx> uc(u.begin(), u.end());
+
+    int S = 2;
+    while(S < v.size() + u.size()) S *= 2;
+
+    vc.resize(S); FFT(vc, false);
+    uc.resize(S); FFT(uc, false);
+
+    for(int i=0; i<S; i++) vc[i] *= uc[i];
+    FFT(vc, true);
+
+    vector<int> w(v.size() + u.size() - 1);
+    for(int i=0; i<w.size(); i++) w[i] = round(vc[i].real());
+
+    return w;
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    vector<int> v(N);
+
+    for(int i=1; i<N; i++) v[(i * i) % N]++;
+
+    v = mul(v, v);
+
+    for(int i=1; i<N; i++) v[(i * i * 2) % N]++;
+
+    int ans = 0;
+
+    for(int i=1; i<N; i++)
+        for(int j=(i*i)%N; j<v.size(); j+=N) ans += v[j];
+
+    ans /= 2;
+
+    cout << ans << "\n";
+}
