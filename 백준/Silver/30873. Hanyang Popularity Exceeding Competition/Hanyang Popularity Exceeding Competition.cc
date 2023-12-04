b92ec58a86d64579b394d9f7e19efaf354917d30
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-12-04 18:23:05 +0900

    [Silver V] Title: Hanyang Popularity Exceeding Competition, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30873. Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    int x = 0;
+
+    while(N--) {
+        int a, b; cin >> a >> b;
+
+        if(abs(a - x) <= b) x++;
+    }
+
+    cout << x << "\n";
+}
