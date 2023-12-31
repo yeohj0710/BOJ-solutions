44efe377f41c275284a484b62805020c95af9de9
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2023-12-31 21:07:00 +0900

    [Bronze II] Title: Разделение амулета, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28973. Разделение амулета/Разделение амулета.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    double a, b; cin >> a >> b;
+
+    cout << fixed << setprecision(6);
+    cout << a * (1 - 1.0 / sqrt(2)) << "\n";
+}
