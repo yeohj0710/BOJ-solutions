cb34a3b2e23bfb2ef8bed821094439a554976363
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-01-18 21:34:41 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 96 ms, Memory: 22056 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -2,28 +2,32 @@
 #define int long long
 using namespace std;
 
-main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
 
     int N; cin >> N;
 
+    unordered_map<string, bool> m;
     vector<string> v;
-    map<string, bool> m;
 
     while(N--) {
         string a, b; cin >> a >> b;
 
-        v.push_back(a);
-
         if(b == "enter") m[a] = true;
         else m[a] = false;
-    }
 
-    sort(v.begin(), v.end());
+        v.push_back(a);
+    }
 
     v.erase(unique(v.begin(), v.end()), v.end());
 
-    for(int i=v.size()-1; i>=0; i--)
-        if(m[v[i]] == true) cout << v[i] << "\n";
+    vector<string> u;
+
+    for(int i=0; i<v.size(); i++)
+        if(m[v[i]]) u.push_back(v[i]);
+
+    sort(u.begin(), u.end(), greater<string>());
+    u.erase(unique(u.begin(), u.end()), u.end());
+
+    for(int i=0; i<u.size(); i++) cout << u[i] << "\n";
 }
