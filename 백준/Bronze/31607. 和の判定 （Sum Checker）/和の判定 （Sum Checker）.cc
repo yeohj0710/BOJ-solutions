8c23a9c192bb52fbf021cec7fb7fcac3f3665c69
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-03-21 15:29:43 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b, c; cin >> a >> b >> c;
+
+    if(a + b == c || b + c == a || c + a == b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+}
