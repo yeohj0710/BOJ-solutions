c4acc74ea556da82e8df154f420a6b821ad8d7d3
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-03-21 15:33:32 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int x; cin >> x;
+
+    if(x % 7 == 2) cout << 1 << "\n";
+    else cout << 0 << "\n";
+}
