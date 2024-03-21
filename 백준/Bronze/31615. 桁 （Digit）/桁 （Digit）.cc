073ef8438e99dd34750f20be9d851e04fa4ebb72
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-03-21 15:38:54 +0900

    [Bronze IV] Title: 桁 (Digit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31615. 桁 （Digit）/桁 （Digit）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b; cin >> a >> b;
+
+    string s = to_string(a + b);
+
+    cout << s.length() << "\n";
+}
