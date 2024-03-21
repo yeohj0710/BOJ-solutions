847b71bd5036a63b43deebc3726cb0155a99073c
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-03-21 15:28:54 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b; cin >> a >> b;
+
+    cout << a + b + 3 << "\n";
+}
