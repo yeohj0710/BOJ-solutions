d1beecb4d1f2595ce2e7a815bb31ce9131e3ac86
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-04-05 11:42:47 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+    
+    if(N % 2 == 0) cout << "SciComLove\n";
+    else cout << "evoLmoCicS\n";
+}
