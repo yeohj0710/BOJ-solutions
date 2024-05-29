ef3513bbe2eeecd7e49d13f7c0948c39f16af75d
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-05-29 15:09:56 +0900

    [Unrated] Title: BASE32 디코딩, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Unrated/10939. BASE32 디코딩/BASE32 디코딩.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string a; cin >> a;
+
+    string b = "";
+
+    for(int i=0; i<a.length(); i++) {
+        if(a[i] == '=') continue;
+
+        int x = 0;
+
+        if(a[i] >= 'A' && a[i] <= 'Z') x = a[i] - 'A';
+        else if(a[i] >= '2' && a[i] <= '9') x = a[i] - '2' + 26;
+
+        string c = "";
+
+        for(int j=0; j<5; j++) {
+            c = char('0' + x % 2) + c;
+
+            x /= 2;
+        }
+
+        b += c;
+    }
+
+    int y = b.length();
+
+    while(y % 8 != 0) y--;
+
+    b = b.substr(0, y);
+
+    string ans = "";
+
+    for(int i=0; i<b.length(); i+=8) {
+        int x = 0;
+
+        for(int j=0; j<8; j++) x = x * 2 + (b[i+j] - '0');
+
+        ans += char(x);
+    }
+
+    cout << ans << "\n";
+}
