8c718f0b35a0e8caed45ffbbc0a0acd493f9a6ed
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-10-07 15:24:55 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+
+    while(t--) {
+        int n; cin >> n;
+
+        cout << (n - 1) * 2 + 1 << "\n";
+    }
+}
