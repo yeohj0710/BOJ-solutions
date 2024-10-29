ca23fb7f97529b995c598bccaad5f54dd982de1b
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-10-29 15:02:56 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n; cin >> n;
+
+    if(n == 2) {
+        cout << 3 << "\n";
+        return 0;
+    }
+
+    cout << n << "\n";
+}
