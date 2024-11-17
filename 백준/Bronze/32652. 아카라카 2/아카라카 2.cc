0d0fe0127b273693ae2b40ef09f2b3dd8fa7b284
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-11-18 00:06:35 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n; cin >> n;
+
+    cout << "AKARAKA";
+
+    n--;
+
+    while(n--) cout << "RAKA";
+
+    cout << "\n";
+}
