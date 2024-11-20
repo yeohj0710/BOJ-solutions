f595ed64ec4d90815ec4bc980d7973353bda995b
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-11-20 19:20:11 +0900

    [Bronze III] Title: МЕД, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24349. МЕД/МЕД.cc
+++ 백준/Bronze/24349. МЕД/МЕД.cc
@@ -2,23 +2,46 @@
 #define int long long
 using namespace std;
 
-main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N, a, b, c; cin >> N >> a >> b >> c;
+    int n, a, b, c; cin >> n >> a >> b >> c;
 
-    int ans;
+    int x = 0, ans = 0;
 
-    if(a <= b && a <= c) ans = (N - 1) * a;
-    else if(b <= a && b <= c) ans = (N - 1) * b;
-    else {
-        if(N == 1) {
-            if(a <= b) ans = a;
-            else ans = b;
+    n--;
+
+    while(n--) {
+        if(x == 0) {
+            if(a < b) {
+                x = 1;
+                ans += a;
+            }
+            else {
+                x = 2;
+                ans += b;
+            }
         }
-        else if(a <= b) ans = a + (N - 2) * c;
-        else ans = b + (N - 2) * c;
+        else if(x == 1) {
+            if(a < c) {
+                x = 0;
+                ans += a;
+            }
+            else {
+                x = 2;
+                ans += c;
+            }
+        }
+        else {
+            if(b < c) {
+                x = 0;
+                ans += b;
+            }
+            else {
+                x = 1;
+                ans += c;
+            }
+        }
     }
 
     cout << ans / 100 << " " << ans % 100 << "\n";
