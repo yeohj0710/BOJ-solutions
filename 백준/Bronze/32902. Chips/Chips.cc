7bec0477cd0552549f039b18142d83ee2acfd2bd
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-12-12 23:23:37 +0900

    [Bronze IV] Title: Chips, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32902. Chips/Chips.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, m; cin >> n >> m;
+
+    cout << m + 1 << " " << n * m + 1 << "\n";
+}
