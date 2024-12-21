9c0235710671345176b9cde44c5b1c08cf8978ec
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-12-22 02:51:28 +0900

    [Bronze III] Title: Consolidating Windows, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/32888. Consolidating Windows/Consolidating Windows.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    double a, b; cin >> a >> b;
+
+    cout << fixed << setprecision(8);
+    cout << sqrt(pow(a, 2) + pow(b, 2)) << "\n";
+}
