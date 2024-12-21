06d45b0451df7746c3af3dbc2f75f4256857707b
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2024-12-22 02:57:35 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b; cin >> a >> b;
+
+    if(a > b) cout << "flight\n";
+    else cout << "high speed rail\n";
+}
