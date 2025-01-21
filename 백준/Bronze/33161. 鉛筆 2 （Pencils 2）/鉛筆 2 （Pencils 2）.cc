6883c8088e3361cbbae863904fc1e8bcdae40057
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2025-01-22 03:05:32 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int x; cin >> x;
+
+    cout << x / 5 << "\n";
+}
