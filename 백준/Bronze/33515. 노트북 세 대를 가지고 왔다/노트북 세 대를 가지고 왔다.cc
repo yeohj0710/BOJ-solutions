ddce573f9fc4d363fff362b7904c48a9f6e5dd84
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2025-03-05 16:17:55 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a, b; cin >> a >> b;
+
+    cout << min(a, b) << "\n";
+}
