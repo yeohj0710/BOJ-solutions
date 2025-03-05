f35e508e47136c7b9db419bd7260a3c18193daf9
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2025-03-05 16:21:02 +0900

    [Bronze II] Title: 아름다운 수열, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/33488. 아름다운 수열/아름다운 수열.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+
+    while(t--) {
+        int n; cin >> n;
+
+        cout << "YES\n";
+
+        for(int i=1; i<=n; i++) cout << i << " ";
+        cout << "\n";
+    }
+}
