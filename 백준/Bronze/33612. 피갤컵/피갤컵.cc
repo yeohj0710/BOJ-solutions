a99af295ced25555457026f9a44e099ebd5d7e78
yeohj0710 <93759367+yeohj0710@users.noreply.github.com>
2025-05-16 17:00:15 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n; cin >> n;
+
+    vector<int> v = {2024, 2025, 2025, 2026, 2026};
+    vector<int> u = {8, 3, 10, 5, 12};
+
+    cout << v[n-1] << " " << u[n-1] << "\n";
+}
